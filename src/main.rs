f0357df61f364754deec5aa70b06195588a0fbf5
Fredrik Norén <fredrik.jw.noren@gmail.com>
2022-12-21 12:25:59 +0100

    Bump clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use clap::{Parser, Subcommand};
 use elements_app::App;
 use elements_ecs::World;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Builds and runs the project at the designated path
+    Run { path: Option<String> },
+}
 
 fn init(world: &mut World) {}
 
