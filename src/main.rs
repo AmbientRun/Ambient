2bd6f92bc32a2a400573ad46b657b2cc9984b679
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-01-05 11:08:18 +0100

    Restructure elements runtime cli

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,26 @@ use elements_std::asset_cache::AssetCache;
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
 struct Cli {
+    /// Set the path to the project. Defaults to the current directory
+    #[arg(short, long)]
+    project_path: Option<String>,
+
     #[command(subcommand)]
     command: Commands,
 }
 
 #[derive(Subcommand, Clone)]
 enum Commands {
-    /// Builds and runs the project at the designated path
-    Run { path: Option<String> },
-    /// Builds the project at the designated path
-    Build { path: Option<String> },
+    /// Builds and runs the project
+    Run,
+    /// Builds the project
+    Build,
 }
 impl Commands {
-    fn get_build(&self) -> Option<PathBuf> {
-        let cur_dir = std::env::current_dir().unwrap();
-        match self.clone() {
-            Commands::Run { path } => Some(path.map(|x| x.into()).unwrap_or(cur_dir)),
-            Commands::Build { path } => Some(path.map(|x| x.into()).unwrap_or(cur_dir)),
+    fn should_build(&self) -> bool {
+        match self {
+            Commands::Run => true,
+            Commands::Build => true,
         }
     }
 }
@@ -39,11 +42,12 @@ fn main() {
     let assets = AssetCache::new(runtime.handle().clone());
 
     let cli = Cli::parse();
-    if let Some(path) = cli.command.get_build() {
-        runtime.block_on(elements_build::build(&assets, path));
+    let project_path = cli.project_path.map(|x| x.into()).unwrap_or_else(|| std::env::current_dir().unwrap());
+    if cli.command.should_build() {
+        runtime.block_on(elements_build::build(&assets, project_path));
     }
 
-    if let Commands::Run { path } = cli.command {
+    if let Commands::Run = cli.command {
         AppBuilder::simple().install_component_registry(false).with_runtime(runtime).with_asset_cache(assets).run(|app, runtime| {
             runtime.spawn(async move {});
         });
