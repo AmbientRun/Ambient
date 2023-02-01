7c6031d4ebaa9e5c835122c72cd92031d1cee297
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-02-01 18:22:10 +0100

    Add skinmesh example

--- crates/animation/src/retargeting.rs
+++ crates/animation/src/retargeting.rs
@@ -6,7 +6,7 @@ use elements_core::transform::{rotation, translation};
 use elements_editor_derive::ElementEditor;
 use elements_model::{Model, ModelDef};
 use elements_std::{
-    asset_cache::{AssetCache, AssetKeepalive, AsyncAssetKey, AsyncAssetKeyExt}, asset_url::{AnimationAssetType, ModelAssetType, TypedAssetUrl}, download_asset::AssetError
+    asset_cache::{AssetCache, AssetKeepalive, AsyncAssetKey, AsyncAssetKeyExt, SyncAssetKeyExt}, asset_url::{AnimationAssetType, ModelAssetType, ServerBaseUrlKey, TypedAssetUrl}, download_asset::AssetError
 };
 use serde::{Deserialize, Serialize};
 
@@ -47,9 +47,11 @@ impl AsyncAssetKey<Result<Arc<AnimationClip>, AssetError>> for AnimationClipReta
         AssetKeepalive::Forever
     }
     async fn load(self, assets: AssetCache) -> Result<Arc<AnimationClip>, AssetError> {
+        let base_url = ServerBaseUrlKey.get(&assets);
+        let clip_url: TypedAssetUrl<AnimationAssetType> = self.clip.resolve(&base_url).context("Failed to resolve clip url")?.into();
         let anim_model =
-            ModelDef(self.clip.model_crate().context("Invalid clip url")?.model()).get(&assets).await.context("Failed to load model")?;
-        let clip = AnimationClipFromUrl::new(self.clip.unwrap_abs(), true).get(&assets).await.context("No such clip")?;
+            ModelDef(clip_url.model_crate().context("Invalid clip url")?.model()).get(&assets).await.context("Failed to load model")?;
+        let clip = AnimationClipFromUrl::new(clip_url.unwrap_abs(), true).get(&assets).await.context("No such clip")?;
         match self.translation_retargeting {
             AnimationRetargeting::None => Ok(clip),
             AnimationRetargeting::Skeleton => {
@@ -58,10 +60,12 @@ impl AsyncAssetKey<Result<Arc<AnimationClip>, AssetError>> for AnimationClipReta
                 Ok(Arc::new(clip))
             }
             AnimationRetargeting::AnimationScaled { normalize_hip } => {
-                let retarget_model = ModelDef(self.retarget_model.context("No retarget_model specified")?)
-                    .get(&assets)
-                    .await
-                    .context("Failed to load retarget model")?;
+                let retarget_model_url = self
+                    .retarget_model
+                    .context("No retarget_model specified")?
+                    .resolve(&base_url)
+                    .context("Failed to resolve retarget url")?;
+                let retarget_model = ModelDef(retarget_model_url.into()).get(&assets).await.context("Failed to load retarget model")?;
                 let mut clip = (*clip).clone();
                 let anim_root = anim_model.roots()[0];
                 let _retarget_root = retarget_model.roots()[0];
---
+++ examples/skinmesh/scripts/main/src/lib.rs
@@ -0,0 +1,82 @@
+use components::core::{
+    app::{main_scene, name}, camera::{
+        active_camera, aspect_ratio, aspect_ratio_from_window, fovy, near, perspective_infinite_reverse, projection, projection_view
+    }, ecs::dont_store, game_objects::player_camera, player::{player, user_id}, primitives::{cube, quad}, rendering::{color, outline, transparency_group}, transform::{inv_local_to_world, local_to_world, lookat_center, lookat_up, rotation, scale, translation}
+};
+use tilt_runtime_scripting_interface::{
+    entity::{AnimationAction, AnimationController}, player::{get_raw_input, KeyCode}, *
+};
+
+pub mod components;
+pub mod params;
+
+#[main]
+pub async fn main() -> EventResult {
+    entity::game_object_base()
+        .with_default(main_scene())
+        .with(active_camera(), 0.)
+        .with_default(dont_store())
+        .with(translation(), vec3(5.0, 5.0, 9.0))
+        .with_default(rotation())
+        .with(lookat_up(), vec3(0., 0., 1.))
+        .with(lookat_center(), vec3(0., 0., 0.))
+        .with_default(local_to_world())
+        .with_default(inv_local_to_world())
+        .with(near(), 0.1)
+        .with(fovy(), 1.0)
+        .with(perspective_infinite_reverse(), ())
+        .with(aspect_ratio(), 1.)
+        .with(aspect_ratio_from_window(), ())
+        .with_default(projection())
+        .with_default(projection_view())
+        .spawn(false);
+
+    let unit_ref = ObjectRef::new("assets/Peasant Man.fbx/objects/main.json");
+    let unit_uid = entity::spawn_template(&unit_ref, Vec3::new(0.0, 0.0, 1.0), None, None, false);
+    let unit_entity = entity::wait_for_spawn(&unit_uid).await;
+
+    entity::set_animation_controller(
+        unit_entity,
+        AnimationController {
+            actions: &[AnimationAction { clip_url: "assets/Capoeira.fbx/animations/mixamo.com.anim", looping: true, weight: 1. }],
+            apply_base_pose: false,
+        },
+    );
+
+    on(event::FRAME, move |_| {
+        for player in player::get_all() {
+            let Some((delta, _)) = player::get_raw_input_delta(player) else { continue; };
+
+            if delta.keys.contains(&KeyCode::Key1) {
+                entity::set_animation_controller(
+                    unit_entity,
+                    AnimationController {
+                        actions: &[AnimationAction {
+                            clip_url: "assets/Robot Hip Hop Dance.fbx/animations/mixamo.com.anim",
+                            looping: true,
+                            weight: 1.,
+                        }],
+                        apply_base_pose: false,
+                    },
+                );
+            }
+
+            if delta.keys.contains(&KeyCode::Key2) {
+                entity::set_animation_controller(
+                    unit_entity,
+                    AnimationController {
+                        actions: &[AnimationAction {
+                            clip_url: "assets/Capoeira.fbx/animations/mixamo.com.anim",
+                            looping: true,
+                            weight: 1.,
+                        }],
+                        apply_base_pose: false,
+                    },
+                );
+            }
+        }
+        EventOk
+    });
+
+    EventOk
+}
