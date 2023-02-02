081d088d3613c0c23c87b7ca69d084ae3d74caf2
Philpax <me@philpax.me>
2023-02-02 12:11:52 +0100

    feat: introduce a crate for project manifests

--- app/src/components.rs
+++ app/src/components.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use elements_ecs::{ComponentRegistry, PrimitiveComponentType};
+use elements_project::{ComponentType, Manifest};
 use serde::Deserialize;
 
 pub(crate) fn init() -> anyhow::Result<()> {
@@ -16,67 +17,38 @@ pub(crate) fn init() -> anyhow::Result<()> {
     crate::player::init_all_components();
 
     // Temporary: this information should move to the ECS through attributes
-    load_from_toml(include_str!("../tilt.toml"))?;
+    load_from_toml(&Manifest::from_str(include_str!("../tilt.toml"))?)?;
 
     Ok(())
 }
 
-fn load_from_toml(manifest: &str) -> anyhow::Result<()> {
-    #[derive(Deserialize, Debug)]
-    #[serde(untagged)]
-    enum ComponentTypeToml {
-        String(String),
-        ContainerType {
-            #[serde(rename = "type")]
-            type_: String,
-            #[serde(skip_serializing_if = "Option::is_none")]
-            element_type: Option<String>,
-        },
-    }
-    impl TryFrom<ComponentTypeToml> for PrimitiveComponentType {
-        type Error = &'static str;
-
-        fn try_from(value: ComponentTypeToml) -> Result<Self, Self::Error> {
-            match value {
-                ComponentTypeToml::String(ty) => PrimitiveComponentType::try_from(ty.as_str()),
-                ComponentTypeToml::ContainerType { type_, element_type } => {
-                    let element_ty = element_type.as_deref().map(PrimitiveComponentType::try_from).transpose()?.map(Box::new);
-                    match element_ty {
-                        Some(element_ty) => match type_.as_str() {
-                            "Vec" => Ok(PrimitiveComponentType::Vec { variants: element_ty }),
-                            "Option" => Ok(PrimitiveComponentType::Option { variants: element_ty }),
-                            _ => Err("invalid container type"),
-                        },
-                        None => PrimitiveComponentType::try_from(type_.as_str()),
-                    }
-                }
-            }
-        }
-    }
-
-    #[derive(Deserialize, Debug)]
-    #[allow(dead_code)]
-    struct ComponentToml {
-        name: String,
-        description: String,
-        #[serde(rename = "type")]
-        type_: ComponentTypeToml,
-    }
-
-    #[derive(Deserialize, Debug)]
-    struct Manifest {
-        components: HashMap<String, ComponentToml>,
-    }
-
-    let manifest: Manifest = toml::from_str(&manifest)?;
+fn load_from_toml(manifest: &Manifest) -> anyhow::Result<()> {
     ComponentRegistry::get_mut().add_external_from_iterator(
         manifest
             .components
-            .into_iter()
-            .map(|(id, component)| Ok((id, PrimitiveComponentType::try_from(component.type_).map_err(|e| anyhow::anyhow!("{e}"))?)))
-            .collect::<anyhow::Result<Vec<_>>>()?
+            .iter()
+            .map(|(id, component)| Ok((id.to_owned(), convert_manifest_type_to_primitive_type(&component.type_)?)))
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(|e: &str| anyhow::anyhow!("{e}"))?
             .into_iter(),
     );
 
     Ok(())
+}
+
+fn convert_manifest_type_to_primitive_type(ty: &ComponentType) -> Result<PrimitiveComponentType, &'static str> {
+    match ty {
+        ComponentType::String(ty) => PrimitiveComponentType::try_from(ty.as_str()),
+        ComponentType::ContainerType { type_, element_type } => {
+            let element_ty = element_type.as_deref().map(PrimitiveComponentType::try_from).transpose()?.map(Box::new);
+            match element_ty {
+                Some(element_ty) => match type_.as_str() {
+                    "Vec" => Ok(PrimitiveComponentType::Vec { variants: element_ty }),
+                    "Option" => Ok(PrimitiveComponentType::Option { variants: element_ty }),
+                    _ => Err("invalid container type"),
+                },
+                None => PrimitiveComponentType::try_from(type_.as_str()),
+            }
+        }
+    }
 }
---
+++ crates/project/src/lib.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct Manifest {
+    pub project: Project,
+    pub components: HashMap<String, Component>,
+}
+impl Manifest {
+    pub fn from_str(manifest: &str) -> Result<Self, toml::de::Error> {
+        toml::from_str(manifest)
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Project {
+    pub name: String,
+    pub version: String,
+    pub description: Option<String>,
+    #[serde(default)]
+    pub authors: Vec<String>,
+    pub organization: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Component {
+    pub name: String,
+    pub description: String,
+    #[serde(rename = "type")]
+    pub type_: ComponentType,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum ComponentType {
+    String(String),
+    ContainerType {
+        #[serde(rename = "type")]
+        type_: String,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        element_type: Option<String>,
+    },
+}
