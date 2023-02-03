bf3b6e29fd3bf6e01606f57ba4b44d0807b26e21
Philpax <me@philpax.me>
2023-02-03 17:46:27 +0100

    feat(scripting): validate name matches project name

--- app/crates/scripting/host/src/server/mod.rs
+++ app/crates/scripting/host/src/server/mod.rs
@@ -125,7 +125,21 @@ pub async fn initialize(
     )
     .await?;
 
-    if project_path.join("Cargo.toml").exists() {
+    let cargo_toml_path = project_path.join("Cargo.toml");
+    if cargo_toml_path.exists() {
+        let toml = cargo_toml::Manifest::from_str(&std::fs::read_to_string(&cargo_toml_path)?)?;
+
+        match toml.package {
+            Some(package) if package.name == manifest.project.name => {}
+            Some(package) => {
+                anyhow::bail!("The name of the package in the Cargo.toml ({}) does not match the project's name ({})", package.name, manifest.project.name);
+            }
+            None => anyhow::bail!(
+                "No [package] present in Cargo.toml for project {}",
+                manifest.project.name
+            ),
+        }
+
         spawn_script(
             world,
             &manifest.project.name,
