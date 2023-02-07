5d3e98550e6ec939d149f0e9d648d75342ffcf67
Philpax <me@philpax.me>
2023-02-07 14:03:24 +0100

    fix: make multiplayer example more consistent

--- examples/multiplayer/src/lib.rs
+++ examples/multiplayer/src/lib.rs
@@ -5,24 +5,31 @@ use tilt_runtime_scripting_interface::*;
 
 #[main]
 pub async fn main() -> EventResult {
+    const CAMERA_POSITION: Vec3 = vec3(CUBE_REGION, CUBE_REGION, CUBE_REGION);
+    const CUBE_REGION: f32 = 5.0;
+    const CUBE_SIZE: f32 = 0.3;
+
     entity::game_object_base()
         .with_default(main_scene())
         .with(active_camera(), 0.)
-        .with(translation(), vec3(5.0, 5.0, 4.0))
+        .with(translation(), CAMERA_POSITION)
         .with(lookat_center(), vec3(0., 0., 0.))
         .with(perspective_infinite_reverse(), ())
         .with(aspect_ratio_from_window(), ())
         .spawn(false);
 
-    spawn_query((player(), user_id())).bind(move |players| {
+    spawn_query(player()).bind(move |players| {
         // For each player joining, spawn a random colored box somewhere
-        for (_, _) in players {
+        for _ in players {
+            let cube_position = rand::random::<Vec3>() * CUBE_REGION - vec3(0.0, 2.0 * CUBE_SIZE, 0.0);
             entity::game_object_base()
                 .with_default(cube())
-                .with(scale(), Vec3::ONE * 0.3)
-                .with(translation(), rand::random::<Vec3>() * 5.)
+                .with(scale(), Vec3::ONE * CUBE_SIZE)
+                .with(translation(), cube_position)
                 .with(color(), rand::random::<Vec3>().extend(1.))
                 .spawn(false);
+
+            println!("Cube created at {cube_position}");
         }
     });
 
