63697728f4779ebdfc7829bb9b42ced1116b688c
Philpax <me@philpax.me>
2023-02-07 19:29:26 +0100

    feat(examples): hello_world -> async

---
+++ examples/async/src/lib.rs
@@ -0,0 +1,15 @@
+use elements_runtime_scripting_interface::*;
+
+#[main]
+pub async fn main() -> EventResult {
+    loop {
+        println!("Hello, world! It is {}", time());
+        run_async(async {
+            sleep(0.25).await;
+            println!("And hello from here! It is {}", time());
+
+            EventOk
+        });
+        sleep(0.5).await;
+    }
+}
