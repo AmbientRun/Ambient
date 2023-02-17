a04d3f42506e77cc376ee8e44489210027d51d85
Philpax <me@philpax.me>
2023-02-17 16:19:38 +0100

    fix(release): tag behaviour

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,6 @@ set -e
 
 BASEDIR=$(dirname $(dirname $(realpath "$0")))
 cd $BASEDIR
-cargo release --no-publish --execute --no-push "$1"
+cargo release --no-publish --execute --no-push --no-tag "$1"
 cd $BASEDIR/guest/rust
-cargo release --no-publish --execute "$1"
+cargo release --no-publish --execute --tag-name "v{{version}}" "$1"
