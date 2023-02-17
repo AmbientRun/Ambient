107c016d42928aeeee945d445d38d9ff25124de2
Philpax <me@philpax.me>
2023-02-17 16:32:42 +0100

    feat(release): update script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 # Helper script to cut a release for both the host and the guest.
+#
+# At time of writing, this does not publish to crates.io for either host or guest.
+#  - Host: blocked on dependencies
+#  - Guest: blocked on API finalization
 set -e
 
 BASEDIR=$(dirname $(dirname $(realpath "$0")))
-cd $BASEDIR
-cargo release --no-publish --execute --no-push --no-tag "$1"
+
 cd $BASEDIR/guest/rust
-cargo release --no-publish --execute --tag-name "v{{version}}" "$1"
+cargo release version --no-confirm --quiet --execute "$1"
+cargo release replace --no-confirm --quiet --execute
+
+cd $BASEDIR
+cargo release version --no-confirm --quiet --execute "$1"
+cargo release commit --no-confirm --quiet --execute
+cargo release tag --no-confirm --quiet --execute --tag-name "v{{version}}"
+cargo release push --quiet --execute
