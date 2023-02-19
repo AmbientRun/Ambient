f808d05e1b11f1593dea044aeee31caa1a50a5a8
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-02-19 18:48:12 +0100

    Change http content endpoint

--- app/src/server/mod.rs
+++ app/src/server/mod.rs
@@ -1,31 +1,20 @@
 use std::{
-    collections::HashMap,
-    net::SocketAddr,
-    path::{Path, PathBuf},
-    sync::Arc,
-    time::SystemTime,
+    collections::HashMap, net::SocketAddr, path::{Path, PathBuf}, sync::Arc, time::SystemTime
 };
 
 use anyhow::Context;
 use axum::{
-    http::{Method, StatusCode},
-    response::IntoResponse,
-    routing::{get, get_service},
-    Router,
+    http::{Method, StatusCode}, response::IntoResponse, routing::{get, get_service}, Router
 };
 use kiwi_core::{app_start_time, asset_cache, dtime, no_sync, time};
 use kiwi_ecs::{ComponentDesc, ComponentRegistry, EntityData, Networked, SystemGroup, World, WorldStreamCompEvent};
 use kiwi_network::{
-    bi_stream_handlers,
-    client::GameRpcArgs,
-    datagram_handlers,
-    server::{ForkingEvent, GameServer, ShutdownEvent},
+    bi_stream_handlers, client::GameRpcArgs, datagram_handlers, server::{ForkingEvent, GameServer, ShutdownEvent}
 };
 use kiwi_object::ObjectFromUrl;
 use kiwi_rpc::RpcRegistry;
 use kiwi_std::{
-    asset_cache::{AssetCache, AsyncAssetKeyExt, SyncAssetKeyExt},
-    asset_url::{AbsAssetUrl, ServerBaseUrlKey},
+    asset_cache::{AssetCache, AsyncAssetKeyExt, SyncAssetKeyExt}, asset_url::{AbsAssetUrl, ServerBaseUrlKey}
 };
 use tower_http::{cors::CorsLayer, services::ServeDir};
 
@@ -98,7 +87,7 @@ pub const QUIC_INTERFACE_PORT: u16 = 9000;
 fn start_http_interface(runtime: &tokio::runtime::Runtime, project_path: &Path) {
     let router = Router::new()
         .route("/ping", get(|| async move { "ok" }))
-        .nest_service("/assets", get_service(ServeDir::new(project_path.join("target"))).handle_error(handle_error))
+        .nest_service("/content", get_service(ServeDir::new(project_path.join("target"))).handle_error(handle_error))
         .layer(CorsLayer::new().allow_origin(tower_http::cors::Any).allow_methods(vec![Method::GET]).allow_headers(tower_http::cors::Any));
 
     runtime.spawn(async move {
@@ -131,7 +120,7 @@ pub(crate) fn start_server(
     let public_host =
         cli.public_host.or_else(|| local_ip_address::local_ip().ok().map(|x| x.to_string())).unwrap_or("localhost".to_string());
     log::info!("Created server, running at {public_host}:{port}");
-    ServerBaseUrlKey.insert(&assets, AbsAssetUrl::parse(format!("http://{public_host}:{HTTP_INTERFACE_PORT}/assets/")).unwrap());
+    ServerBaseUrlKey.insert(&assets, AbsAssetUrl::parse(format!("http://{public_host}:{HTTP_INTERFACE_PORT}/content/")).unwrap());
 
     start_http_interface(runtime, &project_path);
 
