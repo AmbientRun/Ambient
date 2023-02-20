ec4559b811747a304391a7d72fcb9355166e7880
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-02-20 11:00:51 +0100

    Simplify multiplayer example

--- guest/rust/examples/multiplayer/src/lib.rs
+++ guest/rust/examples/multiplayer/src/lib.rs
@@ -4,7 +4,7 @@ use kiwi_api::{
         player::player,
         primitives::cube,
         rendering::color,
-        transform::{lookat_center, scale, translation},
+        transform::{lookat_center, translation},
     },
     concepts::{make_perspective_infinite_reverse_camera, make_transformable},
     prelude::*,
@@ -12,28 +12,20 @@ use kiwi_api::{
 
 #[main]
 pub async fn main() -> EventResult {
-    const CAMERA_POSITION: Vec3 = vec3(CUBE_REGION, CUBE_REGION, CUBE_REGION);
-    const CUBE_REGION: f32 = 5.;
-    const CUBE_SIZE: f32 = 0.6;
-
     make_perspective_infinite_reverse_camera()
         .with_default(player_camera())
-        .with(translation(), CAMERA_POSITION)
+        .with(translation(), Vec3::ONE * 5.)
         .with(lookat_center(), vec3(0., 0., 0.))
         .spawn();
 
     spawn_query(player()).bind(move |players| {
         // For each player joining, spawn a random colored box somewhere
         for _ in players {
-            let cube_position = rand::random::<Vec3>() * CUBE_REGION - vec3(0., 2. * CUBE_SIZE, 0.);
             make_transformable()
                 .with_default(cube())
-                .with(scale(), Vec3::ONE * CUBE_SIZE)
-                .with(translation(), cube_position)
-                .with(color(), rand::random::<Vec3>().extend(1.))
+                .with(translation(), rand::random())
+                .with(color(), rand::random())
                 .spawn();
-
-            println!("Cube created at {cube_position}");
         }
     });
 
