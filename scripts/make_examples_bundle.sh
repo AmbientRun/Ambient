70714cc875520968197c5db894fc591afbba6d58
Philpax <me@philpax.me>
2023-02-22 18:07:32 +0100

    v0.1.1-rc8
    macOS cmd divergences are no good

--- scripts/make_examples_bundle.sh
+++ scripts/make_examples_bundle.sh
@@ -6,8 +6,8 @@ mkdir -p tmp
 cp -r guest/rust/examples tmp/examples
 
 NEW_VERSION=`grep '^version = ' Cargo.toml | tr -d '\n'`
-sed -i '' "s/version = { workspace = true }/$NEW_VERSION/" tmp/examples/*/Cargo.toml
-sed -i '' "s/path = \"..\/..\/api\"/$NEW_VERSION/" tmp/examples/*/Cargo.toml
+sed -i "s/version = { workspace = true }/$NEW_VERSION/" tmp/examples/*/Cargo.toml
+sed -i "s/path = \"..\/..\/api\"/$NEW_VERSION/" tmp/examples/*/Cargo.toml
 
 cd tmp
 zip -r -q examples.zip examples
