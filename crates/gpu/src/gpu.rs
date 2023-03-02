6d7ed3ed953dd91deb0ffb9c819c1d0d37a36e9b
Tei Roberts <ten3roberts@gmail.com>
2023-03-02 16:41:02 +0100

    fix: gpu adapter power preference

--- crates/gpu/src/gpu.rs
+++ crates/gpu/src/gpu.rs
@@ -60,7 +60,7 @@ impl Gpu {
         log::info!("Requesting adapter");
         let adapter = instance
             .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::default(),
+                power_preference: wgpu::PowerPreference::HighPerformance,
                 compatible_surface: surface.as_ref(),
                 force_fallback_adapter: false,
             })
