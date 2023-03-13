a20194172f79ac22c31907f3caef89b1cd2b0d73
Philpax <me@philpax.me>
2023-03-13 13:25:37 +0100

    fix(app): update new project template

--- app/src/cli/new_project_template/src/lib.rs
+++ app/src/cli/new_project_template/src/lib.rs
@@ -1,6 +1,7 @@
 use ambient_api::{
     components::core::{
-        game_objects::player_camera,
+        app::main_scene,
+        camera::aspect_ratio_from_window,
         primitives::quad,
         transform::{lookat_center, translation},
     },
@@ -12,7 +13,8 @@ use ambient_api::{
 pub async fn main() -> EventResult {
     Entity::new()
         .with_merge(make_perspective_infinite_reverse_camera())
-        .with_default(player_camera())
+        .with(aspect_ratio_from_window(), EntityId::resources())
+        .with_default(main_scene())
         .with(translation(), Vec3::ONE * 5.)
         .with(lookat_center(), vec3(0., 0., 0.))
         .spawn();
