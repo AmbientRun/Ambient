e9360fb2b08e7802d771f3aa34bdd0dd20472a62
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-03-17 17:24:26 +0100

    Enable remaining editors in example

--- guest/rust/examples/ui/input/src/client.rs
+++ guest/rust/examples/ui/input/src/client.rs
@@ -1,6 +1,7 @@
 use ambient_api::{
     components::core::app::ui_scene, concepts::make_orthographic_camera, prelude::*,
 };
+use ambient_cb::cb;
 use ambient_element::{element_component, Element, ElementComponentExt, Hooks};
 use ambient_guest_bridge::{
     components::{
@@ -13,48 +14,67 @@ use ambient_guest_bridge::{
 };
 use ambient_ui_components::{
     default_theme::STREET,
-    editor::TextEditor,
+    editor::{Editor, ListEditor, MinimalListEditor, TextEditor},
     layout::{FlowColumn, FlowRow},
+    select::DropdownSelect,
     text::Text,
     FocusRoot, UIExt,
 };
+use indexmap::IndexMap;
 
 #[element_component]
 fn App(hooks: &mut Hooks) -> Element {
     let (text, set_text) = hooks.use_state("Enter some text".to_string());
-    // let (vector3, set_vector3) = hooks.use_state(Vec3::ZERO);
-    // let (index_map, set_index_map) =
-    //     hooks.use_state(vec![("First".to_string(), "Second".to_string())].into_iter().collect::<IndexMap<String, String>>());
-    // let (list, set_list) = hooks.use_state(vec!["First".to_string(), "Second".to_string()]);
-    // let (minimal_list, set_minimal_list) = hooks.use_state(vec!["First".to_string(), "Second".to_string()]);
+    let (vector3, set_vector3) = hooks.use_state(Vec3::ZERO);
+    let (index_map, set_index_map) = hooks.use_state(
+        vec![("First".to_string(), "Second".to_string())]
+            .into_iter()
+            .collect::<IndexMap<String, String>>(),
+    );
+    let (list, set_list) = hooks.use_state(vec!["First".to_string(), "Second".to_string()]);
+    let (minimal_list, set_minimal_list) =
+        hooks.use_state(vec!["First".to_string(), "Second".to_string()]);
     let row = |name, editor| FlowRow(vec![Text::el(name).set(min_width(), 110.), editor]).el();
     FocusRoot(vec![FlowColumn(vec![
         row("TextEditor", TextEditor::new(text, set_text).el()),
         // Button::new("Focus test", |_| {}).hotkey(winit::event::VirtualKeyCode::Back).el(),
-        // row(
-        //     "DropDownSelect",
-        //     DropdownSelect {
-        //         content: Text::el("Select"),
-        //         on_select: cb(|_| {}),
-        //         items: vec![Text::el("First"), Text::el("Second")],
-        //         inline: false,
-        //     }
-        //     .el(),
-        // ),
-        // row("Vec3", Vec3::editor(vector3, set_vector3, Default::default())),
-        // row("IndexMap", IndexMap::editor(index_map, set_index_map, Default::default())),
-        // row("ListEditor", ListEditor { value: list, on_change: Some(set_list) }.el()),
-        // row(
-        //     "MinimalListEditor",
-        //     MinimalListEditor {
-        //         value: minimal_list,
-        //         on_change: Some(set_minimal_list),
-        //         item_opts: Default::default(),
-        //         add_presets: None,
-        //         add_title: "Add".to_string(),
-        //     }
-        //     .el(),
-        // ),
+        row(
+            "DropDownSelect",
+            DropdownSelect {
+                content: Text::el("Select"),
+                on_select: cb(|_| {}),
+                items: vec![Text::el("First"), Text::el("Second")],
+                inline: false,
+            }
+            .el(),
+        ),
+        row(
+            "Vec3",
+            Vec3::editor(vector3, set_vector3, Default::default()),
+        ),
+        row(
+            "IndexMap",
+            IndexMap::editor(index_map, set_index_map, Default::default()),
+        ),
+        row(
+            "ListEditor",
+            ListEditor {
+                value: list,
+                on_change: Some(set_list),
+            }
+            .el(),
+        ),
+        row(
+            "MinimalListEditor",
+            MinimalListEditor {
+                value: minimal_list,
+                on_change: Some(set_minimal_list),
+                item_opts: Default::default(),
+                add_presets: None,
+                add_title: "Add".to_string(),
+            }
+            .el(),
+        ),
     ])
     .el()
     .set(translation(), vec3(200., 200., 0.))
