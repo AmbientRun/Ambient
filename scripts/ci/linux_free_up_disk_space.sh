06ce976a04a2e870da475b22765d12884af6d994
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-03-23 11:31:25 +0100

    Give up on shell scripts

--- scripts/ci/linux_free_up_disk_space.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "HELLO WORLD"
-sudo rm -rf /usr/share/dotnet
-sudo rm -rf /opt/ghc
-sudo rm -rf "/usr/local/share/boost"
-sudo rm -rf "$AGENT_TOOLSDIRECTORY"
--- scripts/ci/linux_install_headless_dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo add-apt-repository ppa:oibaf/graphics-drivers -y
-sudo apt-get update
-sudo apt install -y libxcb-xfixes0-dev mesa-vulkan-drivers
