0cdd4435be731e03260ff5fafca4e6b71fc704e0
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-03-29 14:03:38 +0200

    Add multiplayer todo example

---
+++ guest/rust/examples/ui/todo/src/server.rs
@@ -0,0 +1,12 @@
+use ambient_api::{message::server::MessageExt, prelude::*};
+use components::todo_item;
+
+#[main]
+pub async fn main() {
+    messages::NewItem::subscribe(|_source, data| {
+        Entity::new().with(todo_item(), data.description).spawn();
+    });
+    messages::DeleteItem::subscribe(|_source, data| {
+        entity::despawn(data.id);
+    });
+}
