371d6083d9c016fbc439747091112d4bbe72e6a2
chaosprint <chaosprint@gmail.com>
2023-04-05 20:28:32 +0200

    asset url is working

--- crates/wasm/src/shared/implementation/asset.rs
+++ crates/wasm/src/shared/implementation/asset.rs
@@ -2,14 +2,14 @@ use ambient_std::{
     asset_url::{AbsAssetUrl, AssetUrl}
 };
 
-use ambient_network::abs_asset_url;
+use ambient_network::{ServerWorldExt, abs_asset_url};
 use ambient_ecs::World;
 
 pub(crate) fn url(
-    world: &mut World,
+    world: &World,
     path: String,
 ) -> anyhow::Result<Option<String>> {
-    let url = world.resource(abs_asset_url());
+    let url = world.synced_resource(abs_asset_url()).unwrap();
     let base_url = &AbsAssetUrl::parse(url)?;
     let asset_url = AssetUrl::parse(path)?.resolve(&base_url)?;
     Ok(Some(asset_url.to_string()))
