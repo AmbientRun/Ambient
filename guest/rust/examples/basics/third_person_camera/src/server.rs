31c0b9e5ec57aaed41549b2c7834440c5c709504
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-04-05 09:52:31 +0200

    Move camera in third person camera to client

--- guest/rust/examples/basics/third_person_camera/src/server.rs
+++ guest/rust/examples/basics/third_person_camera/src/server.rs
@@ -1,23 +1,21 @@
 use ambient_api::{
     components::core::{
-        app::main_scene,
-        camera::aspect_ratio_from_window,
         physics::{
             character_controller_height, character_controller_radius, physics_controlled,
             plane_collider, sphere_collider, visualizing,
         },
-        player::{player, user_id},
+        player::player,
         primitives::{cube, quad},
         rendering::color,
-        transform::{lookat_center, rotation, scale, translation},
+        transform::{rotation, scale, translation},
     },
-    concepts::{make_perspective_infinite_reverse_camera, make_sphere, make_transformable},
+    concepts::{make_sphere, make_transformable},
     message::server::{MessageExt, Source},
     prelude::*,
     rand,
 };
 
-use components::{player_camera_ref, player_mouse_delta_x, player_movement_direction};
+use components::{player_mouse_delta_x, player_movement_direction};
 
 #[main]
 pub fn main() {
@@ -37,23 +35,13 @@ pub fn main() {
         .with_default(visualizing())
         .spawn();
 
-    spawn_query((player(), user_id())).bind(move |players| {
-        for (id, (_, user)) in players {
-            let camera = Entity::new()
-                .with_merge(make_perspective_infinite_reverse_camera())
-                .with(aspect_ratio_from_window(), EntityId::resources())
-                .with_default(main_scene())
-                .with(user_id(), user)
-                .with(translation(), Vec3::ONE * 5.)
-                .with(lookat_center(), vec3(0., 0., 0.))
-                .spawn();
-
+    spawn_query(player()).bind(move |players| {
+        for (id, _) in players {
             entity::add_components(
                 id,
                 Entity::new()
                     .with_merge(make_transformable())
                     .with_default(cube())
-                    .with(player_camera_ref(), camera)
                     .with(color(), rand::random::<Vec3>().extend(1.0))
                     .with(character_controller_height(), 2.)
                     .with(character_controller_radius(), 0.5)
@@ -73,27 +61,21 @@ pub fn main() {
 
     query((
         player(),
-        player_camera_ref(),
         player_movement_direction(),
         player_mouse_delta_x(),
         rotation(),
     ))
     .each_frame(move |players| {
-        for (player_id, (_, camera_id, direction, mouse_delta_x, rot)) in players {
+        for (player_id, (_, direction, mouse_delta_x, rot)) in players {
             let speed = 0.1;
 
             let displace = rot * (direction.normalize_or_zero() * speed).extend(-0.1);
             physics::move_character(player_id, displace, 0.01, frametime());
 
-            let rot = entity::mutate_component(player_id, rotation(), |x| {
+            entity::mutate_component(player_id, rotation(), |x| {
                 *x *= Quat::from_rotation_z(mouse_delta_x * 0.01)
             })
             .unwrap_or_default();
-
-            let pos = entity::get_component(player_id, translation()).unwrap();
-            let forward = rot * Vec3::X;
-            entity::set_component(camera_id, lookat_center(), pos);
-            entity::set_component(camera_id, translation(), pos - forward * 4. + Vec3::Z * 2.);
         }
     });
 }
