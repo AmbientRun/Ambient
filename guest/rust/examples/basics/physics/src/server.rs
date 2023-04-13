a3ff50315c8ba3facc8f68ab5d6dbbad4672b8ce
chaosprint <chaosprint@gmail.com>
2023-04-13 11:12:00 +0200

    send bonk pos in physic example with msg

--- guest/rust/examples/basics/physics/src/server.rs
+++ guest/rust/examples/basics/physics/src/server.rs
@@ -46,10 +46,9 @@ pub async fn main() {
         .with(prefab_from_url(), asset::url("assets/Shape.glb").unwrap())
         .spawn();
 
-    ambient_api::messages::Collision::subscribe(|msg| {
-        messages::Bonk::new(
-            format!("Bonk! {:?} collided", msg.ids)
-        ).send_client_broadcast_reliable();
+    ambient_api::messages::Collision::subscribe(move |msg| {
+        let pos = entity::get_component(cube, translation()).unwrap();
+        messages::Bonk::new(pos, "bonk detected").send_client_broadcast_reliable();
     });
 
     ambient_api::messages::Frame::subscribe(move |_| {
