8b3eff30e85a9fa24a8f6495826def28c125dc35
Qichao Lan <35621141+chaosprint@users.noreply.github.com>
2023-04-14 10:35:51 +0200

    quick fix physics example msg fmt

--- guest/rust/examples/basics/physics/src/client.rs
+++ guest/rust/examples/basics/physics/src/client.rs
@@ -12,7 +12,7 @@ pub fn main() {
 
     let mut bonk = audio::load(asset::url("assets/bonk.ogg").unwrap());
 
-    messages::Bonk::subscribe(move |source, data| {
+    messages::Bonk::subscribe(move |_source, data| {
         let cube = data.cube;
         let vel = entity::get_component(cube, linear_velocity()).unwrap();
         let mut amp = (vel.x.abs() / 5.0).powf(2.0)
