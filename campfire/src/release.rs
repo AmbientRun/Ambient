8ee8c662efa34ed52bd3e13bad7ff9be47c06381
Philpax <me@philpax.me>
2023-04-14 15:19:00 +0200

    feat(campfire): add release update-msrv

--- campfire/src/release.rs
+++ campfire/src/release.rs
@@ -2,8 +2,13 @@ use clap::Parser;
 
 #[derive(Parser, Clone)]
 pub enum Release {
-    /// Changes the version of all crates to match the given version
+    /// Changes the Ambient version across all crates and documentation to match the given version
     UpdateVersion {
+        #[clap()]
+        new_version: String,
+    },
+    /// Changes the minimum supported Rust version across all crates and documentation to match the given version
+    UpdateMsrv {
         #[clap()]
         new_version: String,
     },
@@ -12,31 +17,31 @@ pub enum Release {
 pub(crate) fn main(args: &Release) -> anyhow::Result<()> {
     match args {
         Release::UpdateVersion { new_version } => update_version(new_version),
+        Release::UpdateMsrv { new_version } => update_msrv(new_version),
     }
 }
 
+const DOCKERFILE: &str = "Dockerfile";
+const AMBIENT_MANIFEST: &str = "ambient.toml";
+const ROOT_CARGO: &str = "Cargo.toml";
+const WEB_CARGO: &str = "web/Cargo.toml";
+const GUEST_RUST_CARGO: &str = "guest/rust/Cargo.toml";
+const INSTALLING_DOCS: &str = "docs/src/user/installing.md";
+
 fn update_version(new_version: &str) -> anyhow::Result<()> {
-    fn edit_toml(path: &str, f: impl Fn(&mut toml_edit::Document)) -> anyhow::Result<()> {
-        let mut toml = std::fs::read_to_string(path)?.parse::<toml_edit::Document>()?;
-        f(&mut toml);
-        std::fs::write(path, toml.to_string())?;
-
-        Ok(())
-    }
-
-    edit_toml("ambient.toml", |toml| {
+    edit_toml(AMBIENT_MANIFEST, |toml| {
         toml["project"]["version"] = toml_edit::value(new_version);
     })?;
 
-    edit_toml("Cargo.toml", |toml| {
+    edit_toml(ROOT_CARGO, |toml| {
         toml["workspace"]["package"]["version"] = toml_edit::value(new_version);
     })?;
 
-    edit_toml("web/Cargo.toml", |toml| {
+    edit_toml(WEB_CARGO, |toml| {
         toml["workspace"]["package"]["version"] = toml_edit::value(new_version);
     })?;
 
-    edit_toml("guest/rust/Cargo.toml", |toml| {
+    edit_toml(GUEST_RUST_CARGO, |toml| {
         toml["workspace"]["package"]["version"] = toml_edit::value(new_version);
 
         for (key, value) in toml["workspace"]["dependencies"]
@@ -53,12 +58,9 @@ fn update_version(new_version: &str) -> anyhow::Result<()> {
         }
     })?;
 
-    // Update installing.md
-    {
+    edit_file(INSTALLING_DOCS, |document| {
         const PREFIX: &str = "cargo install --git https://github.com/AmbientRun/Ambient.git --tag";
-
-        let path = "docs/src/user/installing.md";
-        let document = std::fs::read_to_string(path)?
+        document
             .lines()
             .map(|l| {
                 if l.starts_with(PREFIX) {
@@ -70,10 +72,65 @@ fn update_version(new_version: &str) -> anyhow::Result<()> {
             // newline at the end
             .chain(std::iter::once("".to_string()))
             .collect::<Vec<String>>()
-            .join("\n");
-
-        std::fs::write(path, document)?;
-    }
+            .join("\n")
+    })?;
 
     Ok(())
+}
+
+fn update_msrv(new_version: &str) -> anyhow::Result<()> {
+    edit_toml(ROOT_CARGO, |toml| {
+        toml["workspace"]["package"]["rust-version"] = toml_edit::value(new_version);
+    })?;
+
+    edit_toml(WEB_CARGO, |toml| {
+        toml["workspace"]["package"]["rust-version"] = toml_edit::value(new_version);
+    })?;
+
+    edit_toml(GUEST_RUST_CARGO, |toml| {
+        toml["workspace"]["package"]["rust-version"] = toml_edit::value(new_version);
+    })?;
+
+    edit_file(DOCKERFILE, |document: String| {
+        const PREFIX: &str = "FROM rust:";
+        document
+            .lines()
+            .map(|l| {
+                if l.starts_with(PREFIX) {
+                    format!("{PREFIX}{new_version}")
+                } else {
+                    l.to_string()
+                }
+            })
+            .collect::<Vec<_>>()
+            .join("\n")
+    })?;
+
+    edit_file(INSTALLING_DOCS, |mut document| {
+        let begin = "<!-- rust-version-begin !-->";
+        let end = "<!-- rust-version-end !-->";
+        let begin_index = document.find(begin).expect("no begin") + begin.len();
+        let end_index = document.find(end).expect("no end");
+
+        document.replace_range(begin_index..end_index, new_version);
+        document
+    })?;
+
+    Ok(())
+}
+
+fn edit_file(path: &str, f: impl Fn(String) -> String) -> anyhow::Result<()> {
+    let input = std::fs::read_to_string(path)?;
+    let output = f(input);
+    std::fs::write(path, output)?;
+
+    Ok(())
+}
+
+fn edit_toml(path: &str, f: impl Fn(&mut toml_edit::Document)) -> anyhow::Result<()> {
+    edit_file(path, |input| {
+        let mut toml = input.parse::<toml_edit::Document>().unwrap();
+        f(&mut toml);
+        toml.to_string()
+    })
 }
