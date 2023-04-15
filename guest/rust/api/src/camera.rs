e443231eb674c2403b8e6cd5895956fdced72c7d
Owen Palmer <speedcubegeek@gmail.com>
2023-04-15 10:01:56 -0700

    document screen_ray

--- guest/rust/api/src/camera.rs
+++ guest/rust/api/src/camera.rs
@@ -6,8 +6,8 @@ use crate::{
     },
 };
 
-// #[cfg(feature = "client")]
-#[allow(missing_docs)]
+/// Converts normalized mouse coordinate to Ray in world space 
+#[cfg(feature = "client")]
 pub fn screen_ray(camera: EntityId, screen_space_pos: Vec2) -> Ray {
     wit::camera::screen_ray(camera.into_bindgen(), screen_space_pos.into_bindgen()).from_bindgen()
 }
