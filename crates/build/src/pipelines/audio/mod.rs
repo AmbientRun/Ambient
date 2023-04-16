aecd6413862ba622f04df7d6f4020c711dc3a737
marceline-cramer <cramermarceline@gmail.com>
2023-04-16 15:12:41 -0400

    feat: WIP replace ffmpeg with symphonia + vorbis_rs

--- crates/build/src/pipelines/audio/mod.rs
+++ crates/build/src/pipelines/audio/mod.rs
@@ -30,7 +30,7 @@ pub async fn pipeline(ctx: &PipelineCtx) -> Vec<OutAsset> {
                     tracing::info!("Processing {ext:?} file");
                     // Make sure to take the contents, to avoid having both the input and output in
                     // memory at once
-                    let contents = ffmpeg_convert(std::io::Cursor::new(contents)).await?;
+                    let contents = symphonia_convert(std::io::Cursor::new(contents)).await?;
                     ctx.write_file(rel_path.with_extension("ogg"), contents).await
                 }
                 other => anyhow::bail!("Audio filetype {:?} is not yet supported", other.unwrap_or_default()),
@@ -113,5 +113,80 @@ where
 
     tracing::info!("Converted to vorbis of {} kb", output.len() as f32 / 1000.0);
 
+    Ok(output)
+}
+
+#[tracing::instrument(level = "info", skip(input))]
+async fn symphonia_convert<A>(input: A) -> anyhow::Result<Vec<u8>>
+where
+    A: 'static + Send + AsyncRead,
+{
+    use std::num::{NonZeroU32, NonZeroU8};
+
+    use symphonia::core::{
+        codecs::{DecoderOptions, CODEC_TYPE_NULL},
+        errors::Error,
+        formats::FormatOptions,
+        io::MediaSourceStream,
+        meta::MetadataOptions,
+        probe::Hint,
+    };
+
+    use vorbis_rs::{VorbisBitrateManagementStrategy, VorbisEncoder};
+
+    let mut buf = Vec::new();
+    let mut input = Box::pin(input);
+    input.read_to_end(&mut buf).await.unwrap();
+    let mss = MediaSourceStream::new(Box::new(std::io::Cursor::new(buf)), Default::default());
+    let meta_opts = MetadataOptions::default();
+    let fmt_opts = FormatOptions::default();
+    let hint = Hint::default();
+    let probed = symphonia::default::get_probe().format(&hint, mss, &fmt_opts, &meta_opts).expect("unsupported format");
+    let mut format = probed.format;
+    let track = format.tracks().iter().find(|t| t.codec_params.codec != CODEC_TYPE_NULL).expect("no supported audio trackes");
+    let dec_opts = DecoderOptions::default();
+    let mut decoder = symphonia::default::get_codecs().make(&track.codec_params, &dec_opts).expect("unsupported codec");
+
+    let sampling_rate = decoder.codec_params().sample_rate.unwrap();
+    let channels: u8 = decoder.codec_params().channels.unwrap().bits().try_into().unwrap();
+    let bitrate = VorbisBitrateManagementStrategy::QualityVbr { target_quality: 0.9 };
+
+    let mut encoder = VorbisEncoder::new(
+        0,             // TODO randomly generate this
+        [("", ""); 0], // no tags,
+        NonZeroU32::new(sampling_rate).unwrap(),
+        NonZeroU8::new(channels).unwrap(),
+        bitrate,
+        None,
+        Vec::new(),
+    )?;
+
+    let result = loop {
+        let packet = match format.next_packet() {
+            Ok(packet) => packet,
+            Err(err) => break err,
+        };
+
+        let decoded = match decoder.decode(&packet) {
+            Ok(buf) => buf,
+            Err(err) => break err,
+        };
+
+        let mut block = decoded.make_equivalent::<f32>();
+        decoded.convert(&mut block);
+        let planes = block.planes();
+        let plane_samples = planes.planes();
+        encoder.encode_audio_block(plane_samples)?;
+    };
+
+    match result {
+        // "end of stream" is non-fatal, ignore it
+        Error::IoError(err) if err.kind() == std::io::ErrorKind::UnexpectedEof && err.to_string() == "end of stream" => {}
+        // return every other kind of error
+        err => return Err(err.into()),
+    }
+
+    let output = encoder.finish()?;
+    tracing::info!("Decoded {} samples", output.len());
     Ok(output)
 }
