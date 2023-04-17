8173ee0c6c8a980d9bd3beeccc608a24a180f8ba
chaosprint <chaosprint@gmail.com>
2023-04-17 08:22:29 +0200

    rotate the camera without right click

--- guest/rust/examples/games/minigolf/src/client.rs
+++ guest/rust/examples/games/minigolf/src/client.rs
@@ -5,11 +5,7 @@ fn main() {
     ambient_api::messages::Frame::subscribe(move |_| {
         let (delta, input) = player::get_raw_input_delta();
 
-        let camera_rotation = if input.mouse_buttons.contains(&MouseButton::Right) {
-            delta.mouse_position
-        } else {
-            Vec2::ZERO
-        };
+        let camera_rotation = delta.mouse_position;
 
         let camera_zoom = delta.mouse_wheel;
         let shoot = delta.mouse_buttons.contains(&MouseButton::Left);
