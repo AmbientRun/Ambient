9448a0026f19c3d4126e2875c944ef3c4813bdf3
chaosprint <chaosprint@gmail.com>
2023-04-19 11:37:36 +0200

    add music sequencer

--- guest/rust/examples/ui/music_sequencer/src/client.rs
+++ guest/rust/examples/ui/music_sequencer/src/client.rs
@@ -4,7 +4,7 @@ use components::{cursor, note_selection};
 
 #[element_component]
 fn App(hooks: &mut Hooks) -> Element {
-    let card_inner = |selected, highlight| {
+    let card_inner = |selected, highlight, _row| {
         FlowRow(vec![Text::el("")])
             .el()
             .with_background(match highlight {
@@ -16,32 +16,67 @@ fn App(hooks: &mut Hooks) -> Element {
     let items = hooks.use_query((note_selection(), cursor()));
 
     let (_id, (_note_selection, _cursor)) = &items[0];
-    FlowRow::el(
-        _note_selection
-            .iter()
-            .enumerate()
-            .map(|(index, &selected)| {
-                let is_on_cursor = *_cursor == index as u8;
-                FlowRow::el([Button::new(card_inner(selected, is_on_cursor), move |_| {
-                    messages::Click::new(index as u8).send_server_reliable();
-                })
-                .toggled(selected)
-                .style(ButtonStyle::Card)
-                .el()])
-            }),
-    )
-    .with_background(vec4(0.1, 0.1, 0.1, 1.))
-    .with_default(fit_vertical_children())
-    .with_default(fit_horizontal_children())
-    .with_padding_even(10.)
-    .with(space_between_items(), 50.)
+
+    FlowColumn::el(vec![
+        FlowRow::el(
+            _note_selection[0..16]
+                .iter()
+                .enumerate()
+                .map(|(index, &selected)| {
+                    let is_on_cursor = *_cursor == index as u8;
+                    let row = 0;
+                    FlowRow::el([
+                        Button::new(card_inner(selected, is_on_cursor, row), move |_| {
+                            messages::Click::new(index as u8).send_server_reliable();
+                        })
+                        .toggled(selected)
+                        .style(ButtonStyle::Card)
+                        .el(),
+                    ])
+                }),
+        )
+        .with_background(vec4(0.1, 0.1, 0.1, 1.))
+        .with_default(fit_vertical_children())
+        .with_default(fit_horizontal_children())
+        .with_padding_even(10.)
+        .with(space_between_items(), 2.),
+        FlowRow::el(
+            _note_selection[16..32]
+                .iter()
+                .enumerate()
+                .map(|(index, &selected)| {
+                    let is_on_cursor = *_cursor == index as u8;
+                    let row = 1;
+                    FlowRow::el([
+                        Button::new(card_inner(selected, is_on_cursor, row), move |_| {
+                            messages::Click::new(index as u8 + 16).send_server_reliable();
+                        })
+                        .toggled(selected)
+                        .style(ButtonStyle::Card)
+                        .el(),
+                    ])
+                }),
+        )
+        .with_background(vec4(0.1, 0.1, 0.1, 1.))
+        .with_default(fit_vertical_children())
+        .with_default(fit_horizontal_children())
+        .with_padding_even(10.)
+        .with(space_between_items(), 2.),
+    ])
 }
 
 #[main]
 pub fn main() {
     App.el().spawn_interactive();
-    let mut ping = audio::load(asset::url("assets/ping.ogg").unwrap());
-    messages::Play::subscribe(move |_source, data| {
-        ping.play();
+    let mut bd = audio::load(asset::url("assets/BD2500.ogg").unwrap());
+    let mut sd = audio::load(asset::url("assets/SD7550.ogg").unwrap());
+    messages::Play::subscribe(move |_source, data| match data.index {
+        0 => {
+            bd.play();
+        }
+        1 => {
+            sd.play();
+        }
+        _ => {}
     });
 }
--- guest/rust/examples/ui/music_sequencer/src/server.rs
+++ guest/rust/examples/ui/music_sequencer/src/server.rs
@@ -5,7 +5,7 @@ use components::{note_selection, cursor};
 pub async fn main() {
     let mut value = 0_u8;
     let mother = Entity::new()
-    .with(note_selection(), vec![false; 4])
+    .with(note_selection(), vec![false; 32])
     .with(cursor(), 0)
     .spawn();
 
@@ -15,12 +15,15 @@ pub async fn main() {
         entity::set_component(mother, note_selection(), v);
     });
     loop {
-        value = (value + 1) % 4;
+        value = (value + 1) % 16;
         entity::set_component(mother, cursor(), value);
         let mut v = entity::get_component(mother, note_selection()).unwrap();
         if v[value as usize] {
             messages::Play::new(0_u8).send_client_broadcast_reliable();
         };
-        sleep(0.5).await;
+        if v[value as usize + 16] {
+            messages::Play::new(1_u8).send_client_broadcast_reliable();
+        };
+        sleep(0.125).await;
     }
 }
