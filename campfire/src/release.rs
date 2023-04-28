abf679dff2cc396f1c972d4172ed02a4bff950bf
Philpax <me@philpax.me>
2023-04-28 16:04:22 +0200

    feat(campfire): switch to guppy for dep resolution

--- campfire/src/release.rs
+++ campfire/src/release.rs
@@ -5,8 +5,6 @@ use std::{
 
 use anyhow::{ensure, Context};
 use clap::Parser;
-use petgraph::visit::EdgeRef;
-
 use serde::Deserialize;
 use std::str;
 
@@ -241,39 +239,25 @@ fn publish(execute: bool) -> anyhow::Result<()> {
     //
     // Once this is done, we topologically sort the graph and publish in that order.
 
-    use cargo_lock::Dependency;
+    use guppy::graph::DependencyDirection;
 
-    let mut graph = petgraph::Graph::<Dependency, ()>::new();
-    let mut nodes_by_name = HashMap::<String, petgraph::graph::NodeIndex>::new();
-    {
-        let mut nodes = HashMap::<Dependency, petgraph::graph::NodeIndex>::new();
-        let api_lockfile = cargo_lock::Lockfile::load("guest/rust/Cargo.lock")?;
-        let root_lockfile = cargo_lock::Lockfile::load("Cargo.lock")?;
+    let graph = guppy::MetadataCommand::new()
+        .manifest_path(ROOT_CARGO)
+        .build_graph()?;
+    let package_id = graph
+        .resolve_package_name("ambient")
+        .package_ids(DependencyDirection::Forward)
+        .next()
+        .unwrap();
+    let mut packages = graph
+        .query_forward([package_id])?
+        .resolve()
+        .package_ids(DependencyDirection::Forward)
+        .collect::<Vec<_>>();
+    packages.reverse();
 
-        let mut populate_graph = |lockfile: &cargo_lock::Lockfile| {
-            for package in &lockfile.packages {
-                let node_index = graph.add_node(package.into());
-                nodes.insert(package.into(), node_index);
-                nodes_by_name.insert(package.name.to_string(), node_index);
-            }
-
-            for package in &lockfile.packages {
-                let Some(&parent_index) = nodes.get(&package.into()) else { continue; };
-                for dependency in &package.dependencies {
-                    if let Some(&child_index) = nodes.get(&dependency) {
-                        graph.add_edge(parent_index, child_index, ());
-                    }
-                }
-            }
-        };
-
-        populate_graph(&api_lockfile);
-        populate_graph(&root_lockfile);
-    }
-    let nodes_by_name = nodes_by_name;
-
-    fn find_crate_manifest(name: &str) -> Option<(PathBuf, cargo_toml::Manifest)> {
-        let stripped = name.strip_prefix("ambient_")?;
+    fn does_manifest_exist(name: &str) -> bool {
+        let Some(stripped) = name.strip_prefix("ambient_") else { return false; };
         [
             Path::new("crates").join(stripped).join("Cargo.toml"),
             Path::new("libs").join(stripped).join("Cargo.toml"),
@@ -284,88 +268,21 @@ fn publish(execute: bool) -> anyhow::Result<()> {
         ]
         .into_iter()
         .filter(|p| p.exists())
-        .find_map(|p| {
+        .any(|p| {
             let toml_contents = std::fs::read_to_string(&p).unwrap();
             let manifest = &cargo_toml::Manifest::from_str(&toml_contents).unwrap();
 
-            if manifest.package().name == name {
-                Some((p, manifest.clone()))
-            } else {
-                None
-            }
+            manifest.package().name == name
         })
     }
 
-    let ambient_crates: HashMap<String, (PathBuf, cargo_toml::Manifest)> = nodes_by_name
-        .keys()
-        .filter(|k| k.starts_with("ambient_"))
-        .cloned()
-        .filter_map(|k| Some((k.clone(), find_crate_manifest(&k)?)))
-        .collect();
+    let packages = packages
+        .iter()
+        .map(|p| p.repr().split_ascii_whitespace().next().unwrap())
+        .filter(|p| does_manifest_exist(p))
+        .collect::<Vec<_>>();
 
-    graph.retain_edges(|g, e| {
-        let (source, target) = g.edge_endpoints(e).unwrap();
-        let source_name = g.node_weight(source).unwrap().name.to_string();
-        let target_name = g.node_weight(target).unwrap().name.to_string();
-
-        if source_name.starts_with("ambient_") && target_name.starts_with("ambient_") {
-            if let Some((_, manifest)) = ambient_crates.get(&source_name) {
-                if manifest.dev_dependencies.keys().any(|d| *d == target_name) {
-                    return false;
-                }
-            }
-        }
-
-        true
-    });
-
-    let mut bfs = petgraph::visit::Bfs::new(&graph, nodes_by_name["ambient_api"]);
-    let mut names = vec![];
-    while let Some(nx) = bfs.next(&graph) {
-        let name = graph.node_weight(nx).unwrap().name.as_str();
-        if !ambient_crates.contains_key(name) {
-            continue;
-        }
-        names.push(name);
-    }
-    names.reverse();
-    dbg!(names);
-
-    let mut subgraph = petgraph::Graph::<Dependency, ()>::new();
-    petgraph::visit::depth_first_search(&graph, [nodes_by_name["ambient_api"]], |event| {
-        use petgraph::visit::DfsEvent;
-
-        match event {
-            DfsEvent::TreeEdge(n1, n2)
-            | DfsEvent::BackEdge(n1, n2)
-            | DfsEvent::CrossForwardEdge(n1, n2) => {
-                subgraph.add_edge(n1, n2, ());
-            }
-            DfsEvent::Discover(_, _) | DfsEvent::Finish(_, _) => {}
-        }
-    });
-
-    let toposort: Vec<String> = petgraph::algo::toposort(&subgraph, None)
-        .map_err(|c| anyhow::anyhow!("{c:?}"))?
-        .into_iter()
-        .filter_map(|n| {
-            let dependency = graph.node_weight(n)?;
-            Some(format!("{} {}", dependency.name, dependency.version))
-        })
-        .filter(|n| n.starts_with("ambient_"))
-        .collect();
-    dbg!(toposort);
-
-    // dbg!(graph
-    //     .edges_directed(
-    //         nodes_by_name["ambient_element"],
-    //         petgraph::Direction::Outgoing
-    //     )
-    //     .map(|e| (
-    //         graph.node_weight(e.source()).unwrap().name.as_str(),
-    //         graph.node_weight(e.target()).unwrap().name.as_str()
-    //     ))
-    //     .collect::<Vec<_>>());
+    dbg!(packages);
 
     Ok(())
 }
