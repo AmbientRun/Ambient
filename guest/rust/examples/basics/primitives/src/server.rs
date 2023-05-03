2b407343c162052c5a03d438bb3c9f34756e7909
Vinh Truong <vinh@ambient.run>
2023-05-03 17:52:46 +0200

    tests: Update primitives test

--- guest/rust/examples/basics/primitives/src/server.rs
+++ guest/rust/examples/basics/primitives/src/server.rs
@@ -2,11 +2,16 @@ use ambient_api::{
     components::core::{
         app::main_scene,
         camera::aspect_ratio_from_window,
-        primitives::{cube, quad, sphere_radius, sphere_sectors, sphere_stacks},
+        primitives::{
+            capsule_half_height, capsule_latitudes, capsule_longitudes, capsule_radius,
+            capsule_rings, cube, quad, sphere_radius, sphere_sectors, sphere_stacks,
+        },
         rendering::color,
         transform::{lookat_target, scale, translation},
     },
-    concepts::{make_perspective_infinite_reverse_camera, make_sphere, make_transformable},
+    concepts::{
+        make_capsule, make_perspective_infinite_reverse_camera, make_sphere, make_transformable,
+    },
     prelude::*,
 };
 
@@ -30,23 +35,44 @@ pub fn main() {
     Entity::new()
         .with_merge(make_transformable())
         .with_default(cube())
-        .with(translation(), vec3(0., 0., 1.))
-        .with(scale(), Vec3::ONE * 2.)
+        .with(translation(), vec3(2., 0., 0.5))
+        .with(scale(), Vec3::ONE)
         .with(color(), vec4(0., 1., 0., 1.))
         .spawn();
 
     Entity::new()
+        .with_merge(make_transformable())
         .with_merge(make_sphere())
         .with(sphere_radius(), 1.)
         .with(sphere_sectors(), 12)
         .with(sphere_stacks(), 6)
-        .with(translation(), vec3(0., 0., 3.))
+        .with(translation(), vec3(0., 2., 0.5))
         .with(color(), vec4(0., 0., 1., 1.))
         .spawn();
 
     Entity::new()
+        .with_merge(make_transformable())
         .with_merge(make_sphere())
-        .with(translation(), vec3(0., 0., 4.5))
+        .with(translation(), vec3(2., 2., 0.25))
         .with(color(), vec4(1., 1., 0., 1.))
+        .spawn();
+
+    Entity::new()
+        .with_merge(make_transformable())
+        .with_merge(make_capsule())
+        .with(translation(), vec3(-2.0, 2.0, 1.0))
+        .with(color(), vec4(1.0, 0.25, 0.0, 1.0))
+        .spawn();
+
+    Entity::new()
+        .with_merge(make_transformable())
+        .with_merge(make_capsule())
+        .with(capsule_radius(), 0.25)
+        .with(capsule_half_height(), 0.25)
+        .with(capsule_rings(), 0)
+        .with(capsule_latitudes(), 16)
+        .with(capsule_longitudes(), 32)
+        .with(translation(), vec3(-2.0, 0.0, 0.5))
+        .with(color(), vec4(1.0, 0.0, 0.25, 1.0))
         .spawn();
 }
