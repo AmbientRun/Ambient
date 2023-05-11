a836f02633253ab6c0161576d75cdcab5ad063cb
chaosprint <chaosprint@gmail.com>
2023-05-11 13:07:45 +0200

    support ambient audio in browser with sab

--- web/www/bootstrap.js
+++
@@ -1,9 +0,0 @@
-// A dependency graph that contains any wasm must all be imported
-// asynchronously. This `bootstrap.js` file does the single async import, so
-// that no one else needs to worry about it again.
-import("ambient_web")
-    .catch((e) => console.error("Error importing `ambient`:", e))
-    .then((ambient) => {
-        ambient.init_ambient(true, true);
-        ambient.start();
-    });
