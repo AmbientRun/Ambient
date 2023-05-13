501245e0dbf233d0c6386a3c59f8ae6283c31dbe
chaosprint <chaosprint@gmail.com>
2023-05-13 13:31:38 +0200

    dynamic updating components

--- crates/wasm/src/shared/implementation/script.rs
+++ crates/wasm/src/shared/implementation/script.rs
@@ -1,21 +1,30 @@
 
-use ambient_ecs::{World, Entity};
-use ambient_primitives::{cube};
-use ambient_core::transform::translation;
+use ambient_ecs::{World, Entity, EntityId};
+use ambient_primitives::{cube, quad};
+use ambient_core::transform::{translation, scale};
+use ambient_renderer::color;
 use std::sync::Arc;
 use parking_lot::Mutex;
 use ambient_core::{
     async_ecs::async_run,
     runtime,
 };
-use glam::{vec3};
-use anyhow::Context;
-use crate::shared::wit;
+use glam::{Vec3, Vec4};
+// use anyhow::Context;
+// use crate::shared::wit;
 
-async fn fetch_url(url: &str) -> anyhow::Result<String> {
-    let response = reqwest::get(url).await?;
-    let text = response.text().await?;
-    Ok(text)
+// async fn fetch_url(url: &str) -> anyhow::Result<String> {
+//     let response = reqwest::get(url).await?;
+//     let text = response.text().await?;
+//     Ok(text)
+// }
+
+#[derive(Clone, Debug, Default, PartialEq, PartialOrd)]
+struct RhaiEntityInfo {
+    shape: String,
+    translation: [f32; 3],
+    scale: [f32; 3],
+    color: [f32; 4],
 }
 
 pub(crate) fn watch(
@@ -36,32 +45,69 @@ pub(crate) fn watch(
 
     runtime.spawn(async move {
         let mut last_content = String::new();
+        let created_entities = Arc::new(Mutex::new(Vec::<EntityId>::new()));
         loop {
             tokio::time::sleep(std::time::Duration::from_secs(1)).await;
             let content = reqwest::get(url.clone()).await.unwrap().text().await.unwrap();
             if content != last_content {
                 last_content = content.clone();
                 let world_arc_clone = Arc::clone(&world_arc_clone);
+                for entity_id in created_entities.lock().iter() {
+                    let mut world = world_arc_clone.lock();
+                    world.despawn(*entity_id);
+                }
+                created_entities.lock().clear();
+                let created_entities_clone = Arc::clone(&created_entities);
                 async_run.run(move |_world| {
                     let mut engine = rhai::Engine::new();
                     let world_arc_1 = Arc::clone(&world_arc_clone);
-                    engine.register_fn("entities", move || {
+
+                    engine.register_fn("new_entity", move |info: rhai::Map| {
+                        println!("entity: {:?}", info);
+                        let mut entity = Entity::new();
                         let world = Arc::clone(&world_arc_1);
-                        let world = world.lock();
-                        println!("{:?}", world.entities());
-                    });
-                    let world_arc_2 = Arc::clone(&world_arc_clone);
-                    engine.register_fn("cube", move |x: f32, y: f32, z: f32| {
-                        let world = Arc::clone(&world_arc_2);
+                        if let Some(shape) = info.get("shape") {
+                            println!("shape: {:?}", shape);
+                            let shape_str = shape.clone().into_string().unwrap();
+                            match shape_str.as_str() {
+                                "cube" => {
+                                    entity = entity.with_default(cube());
+                                },
+                                "quad" => {
+                                    entity = entity.with_default(quad());
+                                },
+                                // "ball" => {
+                                //     entity = entity.with_default(ball());
+                                // },
+                                _ => {}
+                            }
+                        }
+                        if let Some(_translation) = info.get("translation") {
+                            println!("translation: {:?}", _translation);
+                            let pos = _translation.clone().into_typed_array::<f32>().unwrap();
+                            entity = entity.with(translation(), Vec3::from_slice(&pos));
+                        }
+                        if let Some(_scale) = info.get("scale") {
+                            println!("scale: {:?}", _scale);
+                            let s = _scale.clone().into_typed_array::<f32>().unwrap();
+                            entity = entity.with(scale(), Vec3::from_slice(&s));
+                        }
+                        if let Some(_color) = info.get("color") {
+                            println!("color: {:?}", _color);
+                            let c = _color.clone().into_typed_array::<f32>().unwrap();
+                            entity = entity.with(color(), Vec4::from_slice(&c));
+                        }
                         let mut world = world.lock();
-                        let entity = Entity::new().with_default(cube())
-                        .with(translation(), vec3(x, y, z))
-                        .spawn(&mut *world);
-                        // world.add_component(entity_id, component, value);
-                        // println!("{:?}", world.entities());
+                        let id = entity.spawn(&mut *world);
+                        created_entities_clone.lock().push(id);
                     });
 
-                    engine.run(&content).unwrap();
+                    match engine.run(&content) {
+                        Ok(_) => {}
+                        Err(e) => {
+                            println!("error: {:?}", e);
+                        }
+                    }
                 });
             }
         }
