45bd91ce27f61a865abdfaa8b85046859dd63d1a
Tei Roberts <ten3roberts@gmail.com>
2023-05-17 17:41:23 +0200

    feat: webtransport session establishing

--- crates/network/src/native/webtransport.rs
+++ crates/network/src/native/webtransport.rs
@@ -1,10 +1,13 @@
 use ambient_ecs::WorldStreamFilter;
 use ambient_std::asset_url::AbsAssetUrl;
+use anyhow::Context;
 use bytes::Bytes;
+use h3_webtransport::server::WebTransportSession;
 use http::Method;
 
 use crate::server::SharedServerState;
 
+#[tracing::instrument(level = "info", skip_all)]
 pub async fn handle_h3_connection(
     conn: quinn::Connection,
     state: SharedServerState,
@@ -19,7 +22,6 @@ pub async fn handle_h3_connection(
         .enable_connect(true)
         .enable_datagram(true)
         .max_webtransport_sessions(1)
-        .send_grease(true)
         .build(h3_quinn::Connection::new(conn))
         .await
         .unwrap();
@@ -29,11 +31,37 @@ pub async fn handle_h3_connection(
 
         if let Some((req, resp)) = req {
             match req.method() {
-                &Method::CONNECT => {}
+                &Method::CONNECT => {
+                    let session = WebTransportSession::accept(req, resp, conn)
+                        .await
+                        .context("Failed to accept webtransport session")?;
+                    tracing::info!("Accepted webtransport session");
+
+                    return handle_webtransport_session(
+                        session,
+                        state,
+                        world_stream_filter,
+                        content_base_url,
+                    )
+                    .await;
+                }
                 method => {
                     tracing::info!(?method, "Received other HTTP/3 request")
                 }
             }
         }
+    }
+}
+
+#[tracing::instrument(level = "info", skip_all)]
+async fn handle_webtransport_session(
+    session: WebTransportSession<h3_quinn::Connection, Bytes>,
+    state: SharedServerState,
+    world_stream_filter: WorldStreamFilter,
+    content_base_url: AbsAssetUrl,
+) -> anyhow::Result<()> {
+    loop {
+        let datagram = session.accept_datagram().await?;
+        tracing::info!("Received datagram: {datagram:?}");
     }
 }
