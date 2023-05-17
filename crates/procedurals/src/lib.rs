814438e7f75fd5b028be74964dca7063756d76e2
Vinh Truong <vinh@ambient.run>
2023-05-17 13:19:24 +0300

    New crate: `ambient_procedurals`

---
+++ crates/procedurals/src/lib.rs
@@ -0,0 +1,10 @@
+// struct ProceduralStorage {
+//     meshes: HashMap<Ulid, GpuMesh>,
+//     textures: HashMap<Ulid, Texture>,
+//     materials: HashMap<Ulid, PbrMaterial>,
+//     samplers: HashMap<Ulid, Sampler>,
+// }
+
+pub fn foo() {
+    println!("hello");
+}
