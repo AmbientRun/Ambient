AmbientRun/Ambient

commit 3ec6b1d15e15b4494ba8c1b13a3eaba7502354c6

Simple procedural resource generation (#391)
V1 Feature set:

- All APIs client-only! Creating procedural resources on `server.rs` is not supported.
- Procedural meshes
    - Position, texcoord, normal, tangent, must specify all
    - Indexed triangle lists only
- Procedural textures
    - Width, height, 2D only, no mipmaps
    - Supports almost all linear formats (no depth/stencil/compressed ones)
- Procedural samplers
    - Clamp to edge and repeat wrap modes
    - Nearest and linear filter modes
- Procedural materials
    - Pbr material only
    - Base color map (rgb + alpha), normal map, metallic roughness map
    - Transparency on/off

crates/renderer/examples/pbr.rs
@@ -1,7 +1,7 @@
 use ambient_app::{App, AppBuilder};
 use ambient_core::{asset_cache, camera::active_camera, main_scene, transform::*};
 use ambient_ecs::{query_mut, Entity};
-use ambient_gpu::std_assets::{DefaultNormalMapViewKey, PixelTextureViewKey};
+use ambient_gpu::std_assets::{DefaultNormalMapViewKey, DefaultSamplerKey, PixelTextureViewKey};
 use ambient_meshes::{CubeMeshKey, SphereMeshKey};
 use ambient_renderer::{
     color, gpu_primitives_lod, gpu_primitives_mesh,
@@ -36,9 +36,10 @@ async fn init(app: &mut App) {
                         ),
                     }
                     .get(&assets),
-                    transparent: None,
-                    double_sided: None,
-                    depth_write_enabled: None,
+                    sampler: DefaultSamplerKey.get(&assets),
+                    transparent: false,
+                    double_sided: false,
+                    depth_write_enabled: false,
                 },
             ));
 

crates/renderer/examples/texture.rs
@@ -4,7 +4,7 @@ use ambient_app::{gpu, App, AppBuilder};
 use ambient_core::{asset_cache, camera::active_camera, main_scene, transform::*};
 use ambient_ecs::Entity;
 use ambient_gpu::{
-    std_assets::{DefaultNormalMapViewKey, PixelTextureViewKey},
+    std_assets::{DefaultNormalMapViewKey, DefaultSamplerKey, PixelTextureViewKey},
     texture::Texture,
 };
 use ambient_meshes::CubeMeshKey;
@@ -38,9 +38,10 @@ async fn init(app: &mut App) {
             base_color: texture,
             normalmap: DefaultNormalMapViewKey.get(&assets),
             metallic_roughness: PixelTextureViewKey::white().get(&assets),
-            transparent: None,
-            double_sided: None,
-            depth_write_enabled: None,
+            sampler: DefaultSamplerKey.get(&assets),
+            transparent: false,
+            double_sided: false,
+            depth_write_enabled: false,
         },
     ));
 

crates/renderer/src/materials/pbr_material.rs
@@ -123,8 +123,8 @@ impl Default for PbrMaterialParams {
             base_color_factor: Vec4::ONE,
             emissive_factor: Vec4::ZERO,
             alpha_cutoff: 0.5,
-            metallic: 1.,
-            roughness: 1.,
+            metallic: 1.0,
+            roughness: 1.0,
             _padding: Default::default(),
         }
     }
@@ -136,12 +136,12 @@ pub struct PbrMaterialConfig {
     pub params: PbrMaterialParams,
     pub base_color: Arc<TextureView>,
     pub normalmap: Arc<TextureView>,
-    /// r: Metallic
-    /// g: Roughness
+    /// r: Metallic, g: Roughness
     pub metallic_roughness: Arc<TextureView>,
-    pub transparent: Option<bool>,
-    pub double_sided: Option<bool>,
-    pub depth_write_enabled: Option<bool>,
+    pub sampler: Arc<wgpu::Sampler>,
+    pub transparent: bool,
+    pub double_sided: bool,
+    pub depth_write_enabled: bool,
 }
 
 pub struct PbrMaterial {
@@ -165,8 +165,6 @@ impl PbrMaterial {
                 contents: bytemuck::cast_slice(&[config.params]),
             });
 
-        let sampler = DefaultSamplerKey.get(assets);
-
         Self {
             id: friendly_id(),
             bind_group: gpu.device.create_bind_group(&wgpu::BindGroupDescriptor {
@@ -178,7 +176,7 @@ impl PbrMaterial {
                     },
                     wgpu::BindGroupEntry {
                         binding: 1,
-                        resource: wgpu::BindingResource::Sampler(&sampler),
+                        resource: wgpu::BindingResource::Sampler(&config.sampler),
                     },
                     wgpu::BindGroupEntry {
                         binding: 2,
@@ -220,9 +218,10 @@ impl PbrMaterial {
                 base_color: texture,
                 normalmap: DefaultNormalMapViewKey.get(assets),
                 metallic_roughness: PixelTextureViewKey::white().get(assets),
-                transparent: None,
-                double_sided: None,
-                depth_write_enabled: None,
+                sampler: DefaultSamplerKey.get(assets),
+                transparent: false,
+                double_sided: false,
+                depth_write_enabled: false,
             },
         )
     }
@@ -258,13 +257,13 @@ impl Material for PbrMaterial {
         &self.config.name
     }
     fn transparent(&self) -> Option<bool> {
-        self.config.transparent
+        Some(self.config.transparent)
     }
     fn double_sided(&self) -> Option<bool> {
-        self.config.double_sided
+        Some(self.config.double_sided)
     }
     fn depth_write_enabled(&self) -> Option<bool> {
-        self.config.depth_write_enabled
+        Some(self.config.depth_write_enabled)
     }
 }
 
@@ -441,6 +440,8 @@ impl AsyncAssetKey<Result<Arc<PbrMaterial>, AssetError>> for PbrMaterialDesc {
             PixelTextureViewKey::white().get(&assets)
         };
 
+        let sampler = DefaultSamplerKey.get(&assets);
+
         let params = PbrMaterialParams {
             base_color_factor: self.base_color_factor.unwrap_or(Vec4::ONE),
             emissive_factor: self.emissive_factor.unwrap_or(Vec4::ZERO),
@@ -463,9 +464,10 @@ impl AsyncAssetKey<Result<Arc<PbrMaterial>, AssetError>> for PbrMaterialDesc {
                 base_color: color_view.clone(),
                 normalmap,
                 metallic_roughness,
-                transparent: self.transparent,
-                double_sided: self.double_sided,
-                depth_write_enabled: None,
+                sampler,
+                transparent: self.transparent.unwrap_or_default(),
+                double_sided: self.double_sided.unwrap_or_default(),
+                depth_write_enabled: false,
             },
         )))
     }

crates/ui_native/src/image.rs
@@ -3,7 +3,7 @@ use std::{borrow::Cow, sync::Arc};
 use ambient_core::{asset_cache, mesh, transform::*, ui_scene};
 use ambient_element::{Element, ElementComponent, ElementComponentExt, Hooks};
 use ambient_gpu::{
-    std_assets::{DefaultNormalMapViewKey, PixelTextureViewKey},
+    std_assets::{DefaultNormalMapViewKey, PixelTextureViewKey, DefaultSamplerKey},
     texture::TextureView,
     texture_loaders::{TextureFromBytes, TextureFromUrl},
 };
@@ -45,9 +45,10 @@ impl ElementComponent for Image {
                         base_color: texture,
                         normalmap: DefaultNormalMapViewKey.get(&assets),
                         metallic_roughness: PixelTextureViewKey::white().get(&assets),
-                        transparent: None,
-                        double_sided: None,
-                        depth_write_enabled: None,
+                        sampler: DefaultSamplerKey.get(&assets),
+                        transparent: false,
+                        double_sided: false,
+                        depth_write_enabled: false,
                     },
                 ))
             })

crates/wasm/src/client/implementation/specific.rs
@@ -2,6 +2,8 @@
 //!
 //! If implementing a trait that is also available on the server, it should go in [super].
 
+use std::sync::Arc;
+
 use ambient_audio::AudioFromUrl;
 use ambient_core::{
     asset_cache,
@@ -10,11 +12,23 @@ use ambient_core::{
     runtime,
     window::{window_ctl, WindowCtl},
 };
+use ambient_gpu::{gpu::GpuKey, texture::Texture};
 use ambient_input::{player_prev_raw_input, player_raw_input};
 use ambient_network::client::game_client;
-use ambient_std::{asset_cache::AsyncAssetKeyExt, asset_url::AbsAssetUrl};
+use ambient_procedurals::{
+    new_material_handle, new_mesh_handle, new_sampler_handle, new_texture_handle,
+    procedural_storage,
+};
+use ambient_renderer::pbr_material::{PbrMaterialConfig, PbrMaterialParams};
+use ambient_std::{
+    asset_cache::{AsyncAssetKeyExt, SyncAssetKeyExt},
+    asset_url::AbsAssetUrl,
+    mesh::MeshBuilder,
+};
 use ambient_world_audio::{audio_sender, AudioMessage};
 use anyhow::Context;
+use glam::Vec4;
+use wgpu::TextureViewDescriptor;
 use winit::window::CursorGrabMode;
 
 use super::Bindings;
@@ -258,6 +272,156 @@ impl wit::client_window::Host for Bindings {
         self.world_mut()
             .resource(window_ctl())
             .send(WindowCtl::SetFullscreen(fullscreen))?;
+        Ok(())
+    }
+}
+impl wit::client_mesh::Host for Bindings {
+    fn create(
+        &mut self,
+        desc: wit::client_mesh::Descriptor,
+    ) -> anyhow::Result<wit::client_mesh::Handle> {
+        let wit::client_mesh::Descriptor { vertices, indices } = desc;
+        let mut positions = Vec::with_capacity(vertices.len());
+        let mut normals = Vec::with_capacity(vertices.len());
+        let mut tangents = Vec::with_capacity(vertices.len());
+        let mut texcoords = Vec::with_capacity(vertices.len());
+        for v in &vertices {
+            positions.push(v.position.from_bindgen());
+            normals.push(v.normal.from_bindgen());
+            tangents.push(v.tangent.from_bindgen());
+            texcoords.push(v.texcoord0.from_bindgen());
+        }
+        let mesh = MeshBuilder {
+            positions,
+            normals,
+            tangents,
+            texcoords: vec![texcoords],
+            indices,
+            ..MeshBuilder::default()
+        }
+        .build()?;
+
+        let world = self.world_mut();
+        let storage = world.resource_mut(procedural_storage());
+        let mesh_handle = new_mesh_handle();
+        storage.meshes.insert(mesh_handle, mesh);
+        Ok(mesh_handle.into_bindgen())
+    }
+    fn destroy(&mut self, handle: wit::client_mesh::Handle) -> anyhow::Result<()> {
+        let world = self.world_mut();
+        let storage = world.resource_mut(procedural_storage());
+        storage.meshes.remove(handle.from_bindgen());
+        Ok(())
+    }
+}
+impl wit::client_texture::Host for Bindings {
+    fn create2d(
+        &mut self,
+        desc: wit::client_texture::Descriptor2d,
+    ) -> anyhow::Result<wit::client_texture::Handle> {
+        let world = self.world_mut();
+        let assets = world.resource(asset_cache());
+        let gpu = GpuKey.get(assets);
+        let texture = Texture::new_with_data(
+            gpu,
+            &wgpu::TextureDescriptor {
+                label: None,
+                size: wgpu::Extent3d {
+                    width: desc.width,
+                    height: desc.height,
+                    depth_or_array_layers: 1,
+                },
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: desc.format.from_bindgen(),
+                usage: wgpu::TextureUsages::TEXTURE_BINDING,
+                view_formats: &[],
+            },
+            &desc.data,
+        );
+        let texture = Arc::new(texture);
+        let texture_view = Arc::new(texture.create_view(&TextureViewDescriptor::default()));
+        let storage = world.resource_mut(procedural_storage());
+        let texture_handle = new_texture_handle();
+        storage.textures.insert(texture_handle, texture_view);
+        Ok(texture_handle.into_bindgen())
+    }
+    fn destroy(&mut self, handle: wit::client_texture::Handle) -> anyhow::Result<()> {
+        let world = self.world_mut();
+        let storage = world.resource_mut(procedural_storage());
+        storage.textures.remove(handle.from_bindgen());
+        Ok(())
+    }
+}
+impl wit::client_sampler::Host for Bindings {
+    fn create(
+        &mut self,
+        desc: wit::client_sampler::Descriptor,
+    ) -> anyhow::Result<wit::client_sampler::Handle> {
+        let world = self.world_mut();
+        let assets = world.resource(asset_cache());
+        let gpu = GpuKey.get(assets);
+        let sampler = gpu.device.create_sampler(&wgpu::SamplerDescriptor {
+            address_mode_u: desc.address_mode_u.from_bindgen(),
+            address_mode_v: desc.address_mode_v.from_bindgen(),
+            address_mode_w: desc.address_mode_w.from_bindgen(),
+            mag_filter: desc.mag_filter.from_bindgen(),
+            min_filter: desc.min_filter.from_bindgen(),
+            mipmap_filter: desc.mipmap_filter.from_bindgen(),
+            ..wgpu::SamplerDescriptor::default()
+        });
+        let sampler = Arc::new(sampler);
+        let storage = world.resource_mut(procedural_storage());
+        let sampler_handle = new_sampler_handle();
+        storage.samplers.insert(sampler_handle, sampler);
+        Ok(sampler_handle.into_bindgen())
+    }
+    fn destroy(&mut self, handle: wit::client_sampler::Handle) -> anyhow::Result<()> {
+        let world = self.world_mut();
+        let storage = world.resource_mut(procedural_storage());
+        storage.samplers.remove(handle.from_bindgen());
+        Ok(())
+    }
+}
+impl wit::client_material::Host for Bindings {
+    fn create(
+        &mut self,
+        desc: wit::client_material::Descriptor,
+    ) -> anyhow::Result<wit::client_material::Handle> {
+        let world = self.world_mut();
+        let storage = world.resource_mut(procedural_storage());
+        let material = PbrMaterialConfig {
+            source: "Procedural Material".to_string(),
+            name: "Procedural Material".to_string(),
+            params: PbrMaterialParams {
+                base_color_factor: Vec4::ONE,
+                emissive_factor: Vec4::ZERO,
+                alpha_cutoff: 0.0,
+                metallic: 1.0,
+                roughness: 1.0,
+                ..PbrMaterialParams::default()
+            },
+            base_color: Arc::clone(storage.textures.get(desc.base_color_map.from_bindgen())),
+            normalmap: Arc::clone(storage.textures.get(desc.normal_map.from_bindgen())),
+            metallic_roughness: Arc::clone(
+                storage
+                    .textures
+                    .get(desc.metallic_roughness_map.from_bindgen()),
+            ),
+            sampler: Arc::clone(storage.samplers.get(desc.sampler.from_bindgen())),
+            transparent: desc.transparent,
+            double_sided: false,
+            depth_write_enabled: false,
+        };
+        let material_handle = new_material_handle();
+        storage.materials.insert(material_handle, material);
+        Ok(material_handle.into_bindgen())
+    }
+    fn destroy(&mut self, handle: wit::client_material::Handle) -> anyhow::Result<()> {
+        let world = self.world_mut();
+        let storage = world.resource_mut(procedural_storage());
+        storage.materials.remove(handle.from_bindgen());
         Ok(())
     }
 }

guest/rust/api_core/src/client/texture.rs
@@ -1,0 +1,129 @@
+use crate::global::ProceduralTextureHandle;
+use crate::internal::conversion::*;
+use crate::internal::wit;
+
+#[derive(Clone, Copy)]
+pub enum Format {
+    R8Unorm,
+    R8Snorm,
+    R8Uint,
+    R8Sint,
+    R16Uint,
+    R16Sint,
+    R16Unorm,
+    R16Snorm,
+    R16Float,
+    Rg8Unorm,
+    Rg8Snorm,
+    Rg8Uint,
+    Rg8Sint,
+    R32Uint,
+    R32Sint,
+    R32Float,
+    Rg16Uint,
+    Rg16Sint,
+    Rg16Unorm,
+    Rg16Snorm,
+    Rg16Float,
+    Rgba8Unorm,
+    Rgba8UnormSrgb,
+    Rgba8Snorm,
+    Rgba8Uint,
+    Rgba8Sint,
+    Bgra8Unorm,
+    Bgra8UnormSrgb,
+    Rgb9e5Ufloat,
+    Rgb10a2Unorm,
+    Rg11b10Float,
+    Rg32Uint,
+    Rg32Sint,
+    Rg32Float,
+    Rgba16Uint,
+    Rgba16Sint,
+    Rgba16Unorm,
+    Rgba16Snorm,
+    Rgba16Float,
+    Rgba32Uint,
+    Rgba32Sint,
+    Rgba32Float,
+}
+
+impl IntoBindgen for Format {
+    type Item = wit::client_texture::Format;
+
+    fn into_bindgen(self) -> Self::Item {
+        match self {
+            Format::R8Unorm => Self::Item::R8Unorm,
+            Format::R8Snorm => Self::Item::R8Snorm,
+            Format::R8Uint => Self::Item::R8Uint,
+            Format::R8Sint => Self::Item::R8Sint,
+            Format::R16Uint => Self::Item::R16Uint,
+            Format::R16Sint => Self::Item::R16Sint,
+            Format::R16Unorm => Self::Item::R16Unorm,
+            Format::R16Snorm => Self::Item::R16Snorm,
+            Format::R16Float => Self::Item::R16Float,
+            Format::Rg8Unorm => Self::Item::Rg8Unorm,
+            Format::Rg8Snorm => Self::Item::Rg8Snorm,
+            Format::Rg8Uint => Self::Item::Rg8Uint,
+            Format::Rg8Sint => Self::Item::Rg8Sint,
+            Format::R32Uint => Self::Item::R32Uint,
+            Format::R32Sint => Self::Item::R32Sint,
+            Format::R32Float => Self::Item::R32Float,
+            Format::Rg16Uint => Self::Item::Rg16Uint,
+            Format::Rg16Sint => Self::Item::Rg16Sint,
+            Format::Rg16Unorm => Self::Item::Rg16Unorm,
+            Format::Rg16Snorm => Self::Item::Rg16Snorm,
+            Format::Rg16Float => Self::Item::Rg16Float,
+            Format::Rgba8Unorm => Self::Item::Rgba8Unorm,
+            Format::Rgba8UnormSrgb => Self::Item::Rgba8UnormSrgb,
+            Format::Rgba8Snorm => Self::Item::Rgba8Snorm,
+            Format::Rgba8Uint => Self::Item::Rgba8Uint,
+            Format::Rgba8Sint => Self::Item::Rgba8Sint,
+            Format::Bgra8Unorm => Self::Item::Bgra8Unorm,
+            Format::Bgra8UnormSrgb => Self::Item::Bgra8UnormSrgb,
+            Format::Rgb9e5Ufloat => Self::Item::Rgb9e5Ufloat,
+            Format::Rgb10a2Unorm => Self::Item::Rgb10a2Unorm,
+            Format::Rg11b10Float => Self::Item::Rg11b10Float,
+            Format::Rg32Uint => Self::Item::Rg32Uint,
+            Format::Rg32Sint => Self::Item::Rg32Sint,
+            Format::Rg32Float => Self::Item::Rg32Float,
+            Format::Rgba16Uint => Self::Item::Rgba16Uint,
+            Format::Rgba16Sint => Self::Item::Rgba16Sint,
+            Format::Rgba16Unorm => Self::Item::Rgba16Unorm,
+            Format::Rgba16Snorm => Self::Item::Rgba16Snorm,
+            Format::Rgba16Float => Self::Item::Rgba16Float,
+            Format::Rgba32Uint => Self::Item::Rgba32Uint,
+            Format::Rgba32Sint => Self::Item::Rgba32Sint,
+            Format::Rgba32Float => Self::Item::Rgba32Float,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Descriptor2D<'a> {
+    pub width: u32,
+    pub height: u32,
+    pub format: Format,
+    pub data: &'a [u8],
+}
+
+impl<'a> IntoBindgen for Descriptor2D<'a> {
+    type Item = wit::client_texture::Descriptor2d<'a>;
+
+    fn into_bindgen(self) -> Self::Item {
+        Self::Item {
+            width: self.width,
+            height: self.height,
+            format: self.format.into_bindgen(),
+            data: self.data,
+        }
+    }
+}
+
+pub fn create_2d(desc: &Descriptor2D) -> ProceduralTextureHandle {
+    wit::client_texture::create2d(desc.clone().into_bindgen()).from_bindgen()
+}
+
+pub fn destroy(handle: ProceduralTextureHandle) {
+    wit::client_texture::destroy(handle.into_bindgen());
+}

.github/workflows/ci.yml
@@ -90,7 +90,11 @@ jobs:
         run: tree --du -h target
 
   golden-image-tests:
-    runs-on: ubuntu-20.04
+    # Note: we must use a newer Ubuntu for golden image tests, because older
+    # llvmpipe versions can randomly fail.
+    runs-on: ubuntu-latest
+    env:
+      RUST_LOG: ambient_gpu=debug
     steps:
       - name: Free up disk space
         run: |

crates/app/Cargo.toml
@@ -22,6 +22,7 @@ ambient_ui_native = { path = "../ui_native" , version = "0.2.1" }
 ambient_input = { path = "../input" , version = "0.2.1" }
 ambient_model = { path = "../model" , version = "0.2.1" }
 ambient_animation = { path = "../animation" , version = "0.2.1" }
+ambient_procedurals = { path = "../procedurals" , version = "0.2.1" }
 
 ambient_element = { path = "../../shared_crates/element" , version = "0.2.1" }
 

commit 12530780cc030e4b75631aaeadd9c941f0c368c8

Fix decals
Reverts some changes into PbrMaterialConfig caused by PR #391.

crates/renderer/examples/pbr.rs
@@ -37,9 +37,9 @@ async fn init(app: &mut App) {
                     }
                     .get(&assets),
                     sampler: DefaultSamplerKey.get(&assets),
-                    transparent: false,
-                    double_sided: false,
-                    depth_write_enabled: false,
+                    transparent: None,
+                    double_sided: None,
+                    depth_write_enabled: None,
                 },
             ));
 

crates/renderer/examples/texture.rs
@@ -39,9 +39,9 @@ async fn init(app: &mut App) {
             normalmap: DefaultNormalMapViewKey.get(&assets),
             metallic_roughness: PixelTextureViewKey::white().get(&assets),
             sampler: DefaultSamplerKey.get(&assets),
-            transparent: false,
-            double_sided: false,
-            depth_write_enabled: false,
+            transparent: None,
+            double_sided: None,
+            depth_write_enabled: None,
         },
     ));
 

crates/renderer/src/materials/pbr_material.rs
@@ -139,9 +139,9 @@ pub struct PbrMaterialConfig {
     /// r: Metallic, g: Roughness
     pub metallic_roughness: Arc<TextureView>,
     pub sampler: Arc<wgpu::Sampler>,
-    pub transparent: bool,
-    pub double_sided: bool,
-    pub depth_write_enabled: bool,
+    pub transparent: Option<bool>,
+    pub double_sided: Option<bool>,
+    pub depth_write_enabled: Option<bool>,
 }
 
 pub struct PbrMaterial {
@@ -219,9 +219,9 @@ impl PbrMaterial {
                 normalmap: DefaultNormalMapViewKey.get(assets),
                 metallic_roughness: PixelTextureViewKey::white().get(assets),
                 sampler: DefaultSamplerKey.get(assets),
-                transparent: false,
-                double_sided: false,
-                depth_write_enabled: false,
+                transparent: None,
+                double_sided: None,
+                depth_write_enabled: None,
             },
         )
     }
@@ -257,13 +257,13 @@ impl Material for PbrMaterial {
         &self.config.name
     }
     fn transparent(&self) -> Option<bool> {
-        Some(self.config.transparent)
+        self.config.transparent
     }
     fn double_sided(&self) -> Option<bool> {
-        Some(self.config.double_sided)
+        self.config.double_sided
     }
     fn depth_write_enabled(&self) -> Option<bool> {
-        Some(self.config.depth_write_enabled)
+        self.config.depth_write_enabled
     }
 }
 
@@ -465,9 +465,9 @@ impl AsyncAssetKey<Result<Arc<PbrMaterial>, AssetError>> for PbrMaterialDesc {
                 normalmap,
                 metallic_roughness,
                 sampler,
-                transparent: self.transparent.unwrap_or_default(),
-                double_sided: self.double_sided.unwrap_or_default(),
-                depth_write_enabled: false,
+                transparent: self.transparent,
+                double_sided: self.double_sided,
+                depth_write_enabled: None,
             },
         )))
     }

crates/ui_native/src/image.rs
@@ -3,7 +3,7 @@ use std::{borrow::Cow, sync::Arc};
 use ambient_core::{asset_cache, mesh, transform::*, ui_scene};
 use ambient_element::{Element, ElementComponent, ElementComponentExt, Hooks};
 use ambient_gpu::{
-    std_assets::{DefaultNormalMapViewKey, PixelTextureViewKey, DefaultSamplerKey},
+    std_assets::{DefaultNormalMapViewKey, DefaultSamplerKey, PixelTextureViewKey},
     texture::TextureView,
     texture_loaders::{TextureFromBytes, TextureFromUrl},
 };
@@ -46,9 +46,9 @@ impl ElementComponent for Image {
                         normalmap: DefaultNormalMapViewKey.get(&assets),
                         metallic_roughness: PixelTextureViewKey::white().get(&assets),
                         sampler: DefaultSamplerKey.get(&assets),
-                        transparent: false,
-                        double_sided: false,
-                        depth_write_enabled: false,
+                        transparent: None,
+                        double_sided: None,
+                        depth_write_enabled: None,
                     },
                 ))
             })

crates/wasm/src/client/implementation/specific.rs
@@ -418,9 +418,9 @@ impl wit::client_material::Host for Bindings {
                     .get(desc.metallic_roughness_map.from_bindgen()),
             ),
             sampler: Arc::clone(storage.samplers.get(desc.sampler.from_bindgen())),
-            transparent: desc.transparent,
-            double_sided: false,
-            depth_write_enabled: false,
+            transparent: Some(desc.transparent),
+            double_sided: None,
+            depth_write_enabled: None,
         };
         let material_handle = new_material_handle();
         storage.materials.insert(material_handle, material);