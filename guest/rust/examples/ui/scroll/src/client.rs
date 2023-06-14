24c994612baee4c999ca0ebd670422aaca839c47
chaosprint <chaosprint@gmail.com>
2023-06-14 17:46:28 +0200

    merge to scroll_area

--- guest/rust/examples/ui/scroll/src/client.rs
+++ guest/rust/examples/ui/scroll/src/client.rs
@@ -2,74 +2,45 @@ use ambient_api::prelude::*;
 
 #[element_component]
 fn App(hooks: &mut Hooks) -> Element {
-    let size_info = hooks.use_query(window_logical_size());
-    let x = size_info[0].1.x as f32;
-    let y = size_info[0].1.y as f32;
-    let f = FlowColumn::el([
+    WindowSized::el(vec![ScrollArea::el(
+        ScrollAreaSizing::FitParentWidth,
         FlowColumn::el([
-            Text::el("ScrollBoxView")
-            .header_style(),
-            ScrollBoxView {
-                min_width: 150.0,
-                min_height: 150.0,
-                scroll_height: 100.0,
-                inner: FlowColumn::el([
-                    Text::el("1111111111111111")
-                    .header_style(),
-                    Text::el("2222222222222222")
-                    .header_style(),
-                    Text::el("3333333333333333")
-                    .header_style(),
-                    Text::el("4444444444444444")
-                    .header_style(),
-                    Text::el("5555555555555555")
-                    .header_style(),
-                    Text::el("6666666666666666")
-                    .header_style(),
-                    Text::el("7777777777777777")
-                    .header_style(),
-                    Text::el("8888888888888888")
-                    .header_style(),
-                    Text::el("9999999999999999")
-                    .header_style(),
-                ])
-            }.el()
-        ]),
-        Text::el("Shall I compare thee to a summer's day?")
-        .header_style(),
-        Text::el("Thou art more lovely and more temperate:")
-        .header_style(),
-        Text::el("Rough winds do shake the darling buds of May,")
-        .header_style(),
-        Text::el("And summer's lease hath all too short a date:")
-        .header_style(),
-        Text::el("Sometime too hot the eye of heaven shines,")
-        .header_style(),
-        Text::el("And often is his gold complexion dimm'd;")
-        .header_style(),
-        Text::el("And every fair from fair sometime declines,")
-        .header_style(),
-        Text::el("By chance, or nature's changing course, untrimm'd;")
-        .header_style(),
-        Text::el("But thy eternal summer shall not fade,")
-        .header_style(),
-        Text::el("Nor lose possession of that fair thou ow'st;")
-        .header_style(),
-        Text::el("Nor shall Death brag thou wander'st in his shade,")
-        .header_style(),
-        Text::el("When in eternal lines to time thou grow'st;")
-        .header_style(),
-
-    ])
-    .with_padding_even(STREET)
-    .with(space_between_items(), 10.);
-
-    ScrollBoxView {
-        min_width: x,
-        min_height: y,
-        scroll_height: y/2.0,
-        inner: f
-    }.el()
+            Text::el("1 Test Test Test").header_style(),
+            Text::el("2 Test Test Test").header_style(),
+            Text::el("3 Test Test Test").header_style(),
+            // ScrollArea::el(
+            //     ScrollAreaSizing::FitChildrenWidth,
+            //     FlowColumn::el([
+            //         Text::el("ScrollAreaSizing::FitChildrenWidth"),
+            //         Text::el("ScrollAreaSizing::FitChildrenWidth"),
+            //         Text::el("ScrollAreaSizing::FitChildrenWidth"),
+            //         Text::el("ScrollAreaSizing::FitChildrenWidth"),
+            //         Text::el("ScrollAreaSizing::FitChildrenWidth"),
+            //         Text::el("ScrollAreaSizing::FitChildrenWidth"),
+            //         Text::el("ScrollAreaSizing::FitChildrenWidth"),
+            //         Text::el("ScrollAreaSizing::FitChildrenWidth"),
+            //     ]),
+            // ),
+            Text::el("4 Test Test Test").header_style(),
+            Text::el("5 Test Test Test").header_style(),
+            Text::el("6 Test Test Test").header_style(),
+            Text::el("7 Test Test Test").header_style(),
+            Text::el("8 Test Test Test").header_style(),
+            Text::el("9 Test Test Test").header_style(),
+            Text::el("10 Test Test Test").header_style(),
+            Text::el("11 Test Test Test").header_style(),
+            Text::el("12 Test Test Test").header_style(),
+            Text::el("13 Test Test Test").header_style(),
+            Text::el("14 Test Test Test").header_style(),
+            Text::el("15 Test Test Test").header_style(),
+            Text::el("16 Test Test Test").header_style(),
+            Text::el("17 Test Test Test").header_style(),
+            Text::el("18 Test Test Test").header_style(),
+            Text::el("19 Test Test Test").header_style(),
+            Text::el("20 Test Test Test").header_style(),
+        ])
+        .with(space_between_items(), STREET),
+    )])
 }
 
 #[main]
--- shared_crates/ui/src/scroll_area.rs
+++ shared_crates/ui/src/scroll_area.rs
@@ -1,22 +1,25 @@
 //! Defines a scroll area.
-use ambient_element::{to_owned, element_component, Element, ElementComponentExt, Hooks};
+use ambient_element::{element_component, to_owned, Element, ElementComponentExt, Group, Hooks};
 use ambient_guest_bridge::{
     components::{
-        app::{window_scale_factor},
-        rect::{background_color, border_radius},
-        ecs::{children},
+        app::window_scale_factor,
+        ecs::{children, parent},
         input::{mouse_over, mouse_pickable_max, mouse_pickable_min},
-        layout::{fit_horizontal_children, fit_horizontal_parent, layout_width_to_children, width, height},
-        transform::{translation, local_to_world, local_to_parent},
-        rendering::{scissors_recursive}
+        layout::{
+            fit_horizontal_children, fit_horizontal_parent, fit_vertical_children, height,
+            layout_width_to_children, width,
+        },
+        rect::{background_color, border_radius},
+        rendering::scissors_recursive,
+        transform::{local_to_parent, local_to_world, translation},
     },
     messages,
 };
-use glam::{vec2, Vec2, vec3, Vec3, vec4, Vec4, Mat4, uvec4};
+use glam::{uvec4, vec2, vec3, vec4, Mat4, Vec2, Vec3, Vec4};
 
 use crate::{
     layout::{Flow, MeasureSize},
-    UIBase, Rectangle,
+    Rectangle, UIBase,
 };
 use ambient_cb::cb;
 
@@ -39,67 +42,62 @@ pub fn ScrollArea(
     inner: Element,
 ) -> Element {
     let (scroll, set_scroll) = hooks.use_state(0.);
-    let (inner_size, set_inner_size) = hooks.use_state(Vec2::ZERO);
-    hooks.use_runtime_message::<messages::WindowMouseWheel>(move |_world, event| {
-        let delta = event.delta;
-        set_scroll(scroll + if event.pixels { delta.y } else { delta.y * 20. });
-    });
-    match sizing {
-        ScrollAreaSizing::FitChildrenWidth => {
-            UIBase
-                .el()
-                .init_default(children())
-                .children(vec![
-                    // TODO: For some reason it didn't work to set the translation on self.0 directly, so had to introduce a Flow in between
-                    MeasureSize::el(
-                        Flow(vec![inner])
-                            .el()
-                            .with_default(fit_horizontal_children())
-                            .with(translation(), vec3(0., scroll, 0.)),
-                        cb(move |size| {
-                            set_inner_size(size);
-                        }),
-                    ),
-                ])
-                .with(width(), inner_size.x)
-        }
-        ScrollAreaSizing::FitParentWidth => {
-            UIBase
-                .el()
-                .init_default(children())
-                .children(vec![
-                    // TODO: For some reason it didn't work to set the translation on self.0 directly, so had to introduce a Flow in between
-                    Flow(vec![inner])
-                        .el()
-                        .with_default(fit_horizontal_parent())
-                        .with(translation(), vec3(0., scroll, 0.)),
-                ])
-                .with_default(layout_width_to_children())
-        }
-    }
-}
-
-
-/// A scroll box view that can be used to scroll its child with a scroll bar.
-#[element_component]
-pub fn ScrollBoxView(
-    hooks: &mut Hooks,
-    /// Width of the scroll area
-    min_width: f32,
-    /// Height of the scroll area
-    min_height: f32,
-    /// Distance of scrolling in y axis
-    scroll_height: f32,
-    /// The child element
-    inner: Element,
-) -> Element {
-    let (scroll, set_scroll) = hooks.use_state(0.);
+    let (_inner_size, set_inner_size) = hooks.use_state(Vec2::ZERO);
+    let (_outer_size, _set_outer_size) = hooks.use_state(Vec2::ZERO);
     let (ratio, _set_ratio) = hooks.use_state_with(|world| {
         let r = world.resource(window_scale_factor()).clone();
         r as f32
     });
+    let (min_width, set_min_width) = hooks.use_state(200.);
+    let (min_height, set_min_height) = hooks.use_state(200.);
+    let (scroll_height, set_scroll_height) = hooks.use_state(500.);
 
     let mouse_over_count = hooks.use_ref_with(|_| 0);
+    let bar_height = min_height / (min_height + scroll_height) * min_height;
+    let offset = scroll / scroll_height * (min_height - bar_height);
+    let id = hooks.use_ref_with(|_| None);
+    let inner_flow_id = hooks.use_ref_with(|_| None);
+    let (canvas_offset, set_canvas_offset) = hooks.use_state(Vec2::ZERO);
+
+    hooks.use_frame({
+        to_owned![id, mouse_over_count, set_min_width];
+        move |world| {
+            if let Some(id) = *id.lock() {
+                let number = world.get(id, mouse_over()).unwrap_or(0);
+                *mouse_over_count.lock() = number;
+                let canvas_local_to_world = world.get(id, local_to_world()).unwrap();
+                let (_, _, pos_world) = Mat4::to_scale_rotation_translation(&canvas_local_to_world);
+                set_canvas_offset(vec2(pos_world.x, pos_world.y));
+
+                let p = world.get(id, parent());
+                // println!("scrollarea id {:?} ", id);
+                // println!("scrollarea parent id {:?}", parent_id);
+                if let Ok(parent_id) = p {
+                    // let parent_id = p.unwrap();
+                    let w = world.get(parent_id, width());
+                    let h = world.get(parent_id, height());
+                    // println!("parent id {:?} w: {:?}", parent_id, w);
+                    if let Ok(w) = w {
+                        set_min_width(w);
+                    } else {
+                        println!("no width");
+                    }
+                    if let Ok(h) = h {
+                        set_min_height(h);
+                    } else {
+                        println!("no height");
+                    }
+
+                    // let canvas_w = world.get(id, width());
+                    // let canvas_h = world.get(id, height());
+                    // println!("canvas id {:?} w: {:?} h {:?}", id, canvas_w, canvas_h);
+                } else {
+                    // scroll area is in root
+                    // use WindowSized
+                }
+            }
+        }
+    });
     hooks.use_runtime_message::<messages::WindowMouseWheel>({
         to_owned![mouse_over_count];
         move |_world, event| {
@@ -108,81 +106,165 @@ pub fn ScrollBoxView(
             };
             let delta = event.delta;
             let mouse_pos = scroll + if event.pixels { delta.y } else { delta.y * 20. };
-            set_scroll(mouse_pos.clamp(-scroll_height, 0.));
+
+            set_scroll(mouse_pos.clamp(-scroll_height, 0.0));
+            // println!("scroll: {:?}", scroll);
         }
     });
+    match sizing {
+        ScrollAreaSizing::FitChildrenWidth => {
+            let canvas = UIBase
+                .el()
+                .init_default(children())
+                .init(mouse_pickable_min(), Vec3::ZERO)
+                .init(mouse_pickable_max(), Vec3::ZERO)
+                .with(background_color(), vec4(0.3, 0.0, 0.2, 1.0))
+                .children(vec![
+                    // TODO: For some reason it didn't work to set the translation on self.0 directly
+                    // so had to introduce a Flow in between
+                    MeasureSize::el(
+                        Flow(vec![inner])
+                            .el()
+                            .with_default(fit_vertical_children())
+                            .with_default(fit_horizontal_children())
+                            .with(scissors_recursive(), {
+                                let (y, h) = if canvas_offset.y > 0.0 {
+                                    (
+                                        (canvas_offset.y * ratio) as u32,
+                                        (min_height * ratio) as u32,
+                                    )
+                                } else {
+                                    let h_check = ((min_height + canvas_offset.y) * ratio) as u32;
+                                    // h <= 0.0 will panic
+                                    if h_check != 0 {
+                                        (0, h_check)
+                                    } else {
+                                        (0, 1)
+                                    }
+                                };
 
-    let bar_height = min_height / (min_height + scroll_height) * min_height;
-    let offset = scroll / scroll_height * (min_height - bar_height);
-    let id = hooks.use_ref_with(|_| None);
-    let (canvas_offset, set_canvas_offset) = hooks.use_state(Vec2::ZERO);
-
-    hooks.use_frame({
-        to_owned![id, mouse_over_count];
-        move |world| {
-            if let Some(id) = *id.lock() {
-                let number = world.get(id, mouse_over()).unwrap_or(0);
-                *mouse_over_count.lock() = number;
-                let canvas_local_to_world = world.get(id, local_to_world()).unwrap();
-                let (_, _, pos_world) = Mat4::to_scale_rotation_translation(&canvas_local_to_world);
-                set_canvas_offset(vec2( pos_world.x, pos_world.y));
-            }
-        }
-    });
-
-    let canvas =
-        UIBase
-        .el()
-        .with(width(), min_width)
-        .with(height(), min_height)
-        .init(mouse_pickable_min(), Vec3::ZERO).init(mouse_pickable_max(), Vec3::ZERO)
-        // .with(background_color(), vec4(0.1, 0.6, 0.1, 0.4))
-        .on_spawned({
-            to_owned![id];
-            move |_world, new_id, _| {
-            *id.lock() = Some(new_id);
-        }})
-        .init_default(children())
-        .children(vec![
-            Flow(vec![inner]).el()
-                .with_default(fit_horizontal_children())
-                .with(scissors_recursive(), {
-                    let (y, h) = if canvas_offset.y > 0.0 {
-                        ((canvas_offset.y * ratio) as u32,( min_height * ratio) as u32)
-                    } else {
-                        let h_check = ((min_height + canvas_offset.y) * ratio) as u32;
-                        // h <= 0.0 will panic
-                        if h_check != 0 {
-                            (0, h_check)
-                        } else {
-                            (0, 1)
-                        }
-                    };
-
-                    let (x, w) = if canvas_offset.x > 0.0 {
-                        ((canvas_offset.x * ratio) as u32, (min_width * ratio) as u32)
-                    } else {
-                        let w_check = ((min_width + canvas_offset.x) * ratio) as u32;
-                        // w <= 0.0 will panic
-                        if w_check != 0 {
-                            (0, w_check)
-                        } else {
-                            (0, 1)
-                        }
-                    };
-                    // println!("x: {}, y: {}, w: {}, h: {}", x, y, w, h);
-                    uvec4(x, y, w, h)
+                                let (x, w) = if canvas_offset.x > 0.0 {
+                                    ((canvas_offset.x * ratio) as u32, (min_width * ratio) as u32)
+                                } else {
+                                    let w_check = ((min_width + canvas_offset.x) * ratio) as u32;
+                                    // w <= 0.0 will panic
+                                    if w_check != 0 {
+                                        (0, w_check)
+                                    } else {
+                                        (0, 1)
+                                    }
+                                };
+                                // println!("x: {}, y: {}, w: {}, h: {}", x, y, w, h);
+                                uvec4(x, y, w, h)
+                            })
+                            .with(translation(), vec3(0., scroll, 0.)),
+                        cb(move |size| {
+                            // println!("messured inner size: {:?}", size);
+                            set_inner_size(size);
+                        }),
+                    ),
+                ])
+                .on_spawned({
+                    to_owned![id];
+                    move |_world, canvas_id, _| {
+                        *id.lock() = Some(canvas_id);
+                    }
                 })
-                .with(translation(), vec3(0., scroll, 0.)),
+                .with(height(), min_height)
+                .with(width(), min_width);
+            let scroll_bar = Rectangle::el()
+                .with(width(), 5.)
+                .with(height(), bar_height)
+                .with(border_radius(), Vec4::ONE * 4.0)
+                .with(background_color(), vec4(0.6, 0.6, 0.6, 1.0))
+                .with_default(local_to_parent())
+                .with_default(local_to_world())
+                .with(translation(), vec3(min_width - 5.0, -offset, 0.1));
+            Group::el([canvas, scroll_bar])
+        }
+        ScrollAreaSizing::FitParentWidth => {
+            let canvas = UIBase
+                .el()
+                .on_spawned({
+                    to_owned![id];
+                    move |_world, canvas_id, _| {
+                        *id.lock() = Some(canvas_id);
+                    }
+                })
+                .with(height(), min_height)
+                .with(width(), min_width)
+                .init(mouse_pickable_min(), Vec3::ZERO)
+                .init(mouse_pickable_max(), Vec3::ZERO)
+                .init_default(children())
+                .children(vec![
+                    // TODO: For some reason it didn't work to set the translation on self.0 directly, so had to introduce a Flow in between
+                    MeasureSize::el(
+                        Flow(vec![inner])
+                            .el()
+                            .with_default(fit_vertical_children())
+                            .with_default(fit_horizontal_parent())
+                            .with(scissors_recursive(), {
+                                let (y, h) = if canvas_offset.y > 0.0 {
+                                    (
+                                        (canvas_offset.y * ratio) as u32,
+                                        (min_height * ratio) as u32,
+                                    )
+                                } else {
+                                    let h_check = ((min_height + canvas_offset.y) * ratio) as u32;
+                                    // h <= 0.0 will panic
+                                    if h_check != 0 {
+                                        (0, h_check)
+                                    } else {
+                                        (0, 1)
+                                    }
+                                };
 
-            Rectangle::el()
-            .with(width(), 5.)
-            .with(height(), bar_height)
-            .with(border_radius(), Vec4::ONE * 4.0)
-            .with(background_color(), vec4(0.6, 0.6, 0.6, 1.0))
-            .with_default(local_to_parent())
-            .with_default(local_to_world())
-            .with(translation(), vec3(min_width-5.0, -offset, 0.)),
-        ]);
-    canvas
+                                let (x, w) = if canvas_offset.x > 0.0 {
+                                    ((canvas_offset.x * ratio) as u32, (min_width * ratio) as u32)
+                                } else {
+                                    let w_check = ((min_width + canvas_offset.x) * ratio) as u32;
+                                    // w <= 0.0 will panic
+                                    if w_check != 0 {
+                                        (0, w_check)
+                                    } else {
+                                        (0, 1)
+                                    }
+                                };
+                                // println!("x: {}, y: {}, w: {}, h: {}", x, y, w, h);
+                                uvec4(x, y, w, h)
+                            })
+                            .on_spawned({
+                                to_owned![inner_flow_id];
+                                move |_world, flow_id, _| {
+                                    *inner_flow_id.lock() = Some(flow_id);
+                                }
+                            })
+                            .with(translation(), vec3(0., scroll, 0.)),
+                        cb(move |size| {
+                            // println!("messured inner size: {:?}", size);
+                            set_inner_size(size);
+                            if size.y - min_height > 0.0 {
+                                set_scroll_height(size.y - min_height);
+                            } else {
+                                set_scroll_height(0.0);
+                            }
+                        }),
+                    ),
+                    if scroll_height > 0.0 {
+                        Rectangle::el()
+                            .with(width(), 5.)
+                            .with(height(), bar_height)
+                            .with(border_radius(), Vec4::ONE * 4.0)
+                            .with(background_color(), vec4(0.6, 0.6, 0.6, 1.0))
+                            .with_default(local_to_parent())
+                            .with_default(local_to_world())
+                            .with(translation(), vec3(min_width - 5.0, -offset, 0.1))
+                    } else {
+                        UIBase.el()
+                    },
+                ])
+                .with_default(layout_width_to_children());
+            canvas
+        }
+    }
 }
