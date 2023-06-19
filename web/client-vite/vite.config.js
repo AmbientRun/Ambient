87786581b88e4f414a811ab3fef8e351cced4b85
Tei Roberts <ten3roberts@gmail.com>
2023-06-19 15:42:58 +0200

    feat: port audio processor to vite client

--- web/client-vite/vite.config.js
+++ web/client-vite/vite.config.js
@@ -5,4 +5,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [wasm()],
+    server: {
+        headers: {
+            // Enables shared array buffers
+            "Access-Control-Allow-Origin": "*",
+            "Cross-Origin-Opener-Policy": "same-origin",
+            "Cross-Origin-Embedder-Policy": "require-corp",
+        },
+    },
 });
