fb433ce8601a25bc691c3cb31f42e4f07bda1e57
Tei Roberts <ten3roberts@gmail.com>
2023-06-19 17:25:51 +0200

    chore: cleanup comments

--- web/www/src/main.ts
+++ web/www/src/main.ts
@@ -1,7 +1,5 @@
 import "./style.css";
 
-console.info("Importing ambient web assembly module");
-
 declare global {
   interface Window {
     audio_ctx: AudioContext | null;
@@ -76,9 +74,6 @@ window.audioStop = () => {
   }
 };
 
-// A dependency graph that contains any wasm must all be imported
-// asynchronously. This `bootstrap.js` file does the single async import, so
-// that no one else needs to worry about it again.
 import("ambient_web")
   .catch((e) => console.error("Error importing `ambient`:", e))
   .then((ambient) => {
