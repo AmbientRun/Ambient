b0c18667977b309c7bde2bc624a2baf5b51127b9
Philpax <me@philpax.me>
2023-06-30 16:58:10 +0200

    feat(examples): document messaging example

--- guest/rust/examples/basics/messaging/src/server_two.rs
+++ guest/rust/examples/basics/messaging/src/server_two.rs
@@ -2,6 +2,7 @@ use ambient_api::prelude::*;
 
 #[main]
 pub fn main() {
+    // This module will receive messages from `server.rs`, and respond to them.
     messages::Local::subscribe(move |source, data| {
         println!("{source:?}: {data:?}");
         if let Some(id) = source.local() {
