7c5d1a63e7aea80dd3b148b953181736e06551fd
Philpax <me@philpax.me>
2023-07-24 10:31:45 +0200

    feat(wasmman): show bytecode urls

--- src/ember_reloader/client/wasm_manager.rs
+++ src/ember_reloader/client/wasm_manager.rs
@@ -1,15 +1,14 @@
 use ambient_api::{
     components::core::{
-        layout::{align_vertical_center, space_between_items},
-        rendering::color,
+        layout::space_between_items,
         text::font_style,
-        wasm::{module, module_enabled, module_name, module_on_server},
+        wasm::{bytecode_from_url, module, module_enabled, module_name, module_on_server},
     },
     prelude::*,
 };
 
 use super::{use_hotkey_toggle, use_input_request, Window};
-use crate::messages::{self, WasmSetEnabled};
+use crate::messages::{WasmReload, WasmSetEnabled};
 
 #[element_component]
 pub fn WasmManager(hooks: &mut Hooks) -> Element {
@@ -28,37 +27,56 @@ fn WasmManagerInner(hooks: &mut Hooks) -> Element {
     use_input_request(hooks);
 
     let mut modules: Vec<_> = hooks
-        .use_query((module(), module_name(), module_enabled()))
+        .use_query((
+            module(),
+            module_name(),
+            module_enabled(),
+            bytecode_from_url(),
+        ))
         .into_iter()
-        .map(|(id, (_, name, enabled))| {
+        .map(|(id, (_, name, enabled, url))| {
             (
                 id,
-                (entity::has_component(id, module_on_server()), name, enabled),
+                (
+                    entity::has_component(id, module_on_server()),
+                    name,
+                    enabled,
+                    url,
+                ),
             )
         })
         .collect();
-    modules.sort_by_key(|(_, (is_server, name, _))| (*is_server, name.clone()));
+    modules.sort_by_key(|(_, (is_server, name, _, _))| (*is_server, name.clone()));
 
     let (server_modules, client_modules): (Vec<_>, Vec<_>) = modules
         .into_iter()
-        .partition(|(_, (is_server, _, _))| *is_server);
+        .partition(|(_, (is_server, _, _, _))| *is_server);
 
-    fn render_modules(heading: String, modules: Vec<(EntityId, (bool, String, bool))>) -> Element {
+    fn render_modules(
+        heading: String,
+        modules: Vec<(EntityId, (bool, String, bool, String))>,
+    ) -> Element {
         FlowColumn::el(Iterator::chain(
             [Text::el(heading).with(font_style(), "Bold".to_string())].into_iter(),
-            modules.into_iter().map(|(id, (_, name, enabled))| {
+            modules.into_iter().map(|(id, (_, name, enabled, url))| {
                 FlowRow::el([
                     Checkbox::new(enabled, move |value| {
                         WasmSetEnabled::new(value, id).send_server_reliable();
                     })
                     .el(),
-                    Text::el(name).with_default(align_vertical_center()),
                     Button::new(FontAwesomeIcon::el(0xf2f1, true), move |_| {
-                        messages::WasmReload::new(id).send_server_reliable();
+                        WasmReload::new(id).send_server_reliable();
                     })
                     .style(ButtonStyle::Flat)
                     .el(),
+                    FlowColumn::el([
+                        Text::el(name),
+                        Text::el(url)
+                            .small_style()
+                            .with(font_style(), "Italic".to_string()),
+                    ]),
                 ])
+                .with(space_between_items(), 4.0)
             }),
         ))
         .with(space_between_items(), 4.0)
