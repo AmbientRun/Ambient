e972623b2f8f2cc16288c07831db622e5ada4a36
Tei Leelo Roberts <ten3roberts@gmail.com>
2023-07-25 10:27:29 +0200

    feat: allow gizmos to be attached to entities
    This will bring us closer for "line debugging" as you can now attach
    arbitrary lines and shapes to an entity, such as a player to visualize
    viewing direction.

--- crates/std/src/line_hash.rs
+++ crates/std/src/line_hash.rs
@@ -2,23 +2,10 @@
 #[macro_export]
 #[doc(hidden)]
 macro_rules! line_hash {
-    ($s:expr) => {{
-        use std::collections::hash_map::DefaultHasher;
-        use std::hash::{Hash, Hasher};
-
-        let id = $s;
-
-        let mut s = DefaultHasher::new();
-        id.hash(&mut s);
-        s.finish()
+    (($s:expr),*) => {{
+        format!("{:?}", $($s),*)
     }};
     () => {{
-        let id = concat!(file!(), line!(), column!());
-        line_hash!(id)
-    }};
-    ($($s:expr),*) => {{
-        let mut s: u128 = 0;
-        $(s += $crate::line_hash!($s) as u128;)*
-        $crate::line_hash!(s)
-    }};
+        concat!(file!(), line!(), column!())
+    }}
 }
