9fd4f7a61c4377eda0054e6a2ca27d917d44490f
chaosprint <chaosprint@gmail.com>
2023-07-26 13:57:08 +0200

    move anim is back but with issues

--- src/fpsanim/anim.rs
+++ src/fpsanim/anim.rs
@@ -7,8 +7,41 @@ use ambient_api::{
 use crate::components as c;
 pub fn register_anim() {
     spawn_query(player()).bind(move |info| {
-        println!("\n\nregistering anim spawn_query____\n\n");
         for (id, ()) in info {
+            let walk_fd = PlayClipFromUrlNode::new(
+                asset::url("assets/anim/Walk Forward.fbx/animations/mixamo.com.anim").unwrap(),
+            );
+
+            let walk_bk = PlayClipFromUrlNode::new(
+                asset::url("assets/anim/Walk Backward.fbx/animations/mixamo.com.anim").unwrap(),
+            );
+
+            let walk_lt = PlayClipFromUrlNode::new(
+                asset::url("assets/anim/Walk Left.fbx/animations/mixamo.com.anim").unwrap(),
+            );
+
+            let walk_rt = PlayClipFromUrlNode::new(
+                asset::url("assets/anim/Walk Right.fbx/animations/mixamo.com.anim").unwrap(),
+            );
+
+            let walk_fd_lt = PlayClipFromUrlNode::new(
+                asset::url("assets/anim/Walk Forward Left.fbx/animations/mixamo.com.anim").unwrap(),
+            );
+
+            let walk_fd_rt = PlayClipFromUrlNode::new(
+                asset::url("assets/anim/Walk Forward Right.fbx/animations/mixamo.com.anim")
+                    .unwrap(),
+            );
+
+            let walk_bk_lt = PlayClipFromUrlNode::new(
+                asset::url("assets/anim/Walk Backward Left.fbx/animations/mixamo.com.anim")
+                    .unwrap(),
+            );
+
+            let walk_bk_rt = PlayClipFromUrlNode::new(
+                asset::url("assets/anim/Walk Backward Right.fbx/animations/mixamo.com.anim")
+                    .unwrap(),
+            );
             let run_fd = PlayClipFromUrlNode::new(
                 asset::url("assets/anim/Run Forward.fbx/animations/mixamo.com.anim").unwrap(),
             );
@@ -42,34 +75,34 @@ pub fn register_anim() {
                     .unwrap(),
             );
 
-            let run = PlayClipFromUrlNode::new(
-                asset::url("assets/anim/Rifle Run.fbx/animations/mixamo.com.anim").unwrap(),
-            );
-            let jump = PlayClipFromUrlNode::new(
-                asset::url("assets/anim/Rifle Jump.fbx/animations/mixamo.com.anim").unwrap(),
-            );
+            // let run = PlayClipFromUrlNode::new(
+            //     asset::url("assets/anim/Rifle Run.fbx/animations/mixamo.com.anim").unwrap(),
+            // );
+            // let jump = PlayClipFromUrlNode::new(
+            //     asset::url("assets/anim/Rifle Jump.fbx/animations/mixamo.com.anim").unwrap(),
+            // );
 
-            // Looping is buggy
-            jump.looping(false);
+            // // Looping is buggy
+            // jump.looping(false);
 
-            let hit = PlayClipFromUrlNode::new(
-                asset::url("assets/anim/Rifle Hit Reaction.fbx/animations/mixamo.com.anim")
-                    .unwrap(),
-            );
+            // let hit = PlayClipFromUrlNode::new(
+            //     asset::url("assets/anim/Rifle Hit Reaction.fbx/animations/mixamo.com.anim")
+            //         .unwrap(),
+            // );
 
-            let death = PlayClipFromUrlNode::new(
-                asset::url("assets/anim/Rifle Death.fbx/animations/mixamo.com.anim").unwrap(),
-            );
+            // let death = PlayClipFromUrlNode::new(
+            //     asset::url("assets/anim/Rifle Death.fbx/animations/mixamo.com.anim").unwrap(),
+            // );
 
-            // TODO: buggy!!!!!
-            death.looping(false);
-            // death.freeze_at_percentage(100.0);
-            let fire = PlayClipFromUrlNode::new(
-                asset::url("assets/anim/Firing Rifle.fbx/animations/mixamo.com.anim").unwrap(),
-            );
-            fire.set_retargeting(AnimationRetargeting::Skeleton {
-                model_url: "assets/model/X Bot.fbx".to_string(),
-            });
+            // // TODO: buggy!!!!!
+            // death.looping(false);
+            // // death.freeze_at_percentage(100.0);
+            // let fire = PlayClipFromUrlNode::new(
+            //     asset::url("assets/anim/Firing Rifle.fbx/animations/mixamo.com.anim").unwrap(),
+            // );
+            // fire.set_retargeting(AnimationRetargeting::Skeleton {
+            //     model_url: "assets/model/X Bot.fbx".to_string(),
+            // });
             let idle = PlayClipFromUrlNode::new(
                 asset::url("assets/anim/Rifle Aiming Idle.fbx/animations/mixamo.com.anim").unwrap(),
             );
@@ -104,8 +137,14 @@ pub fn register_anim() {
             let idle_bk_lt = BlendNode::new(&idle, &bk_lt, 0.5);
             let idle_bk_rt = BlendNode::new(&idle, &bk_rt, 0.5);
 
-            // we should have only one player
-            let solo_player = AnimationPlayer::new(&idle);
+            let walk_fd_player = AnimationPlayer::new(&walk_fd);
+            let walk_bk_player = AnimationPlayer::new(&walk_bk);
+            let walk_rt_player = AnimationPlayer::new(&walk_rt);
+            let walk_lt_player = AnimationPlayer::new(&walk_lt);
+            let walk_fd_lt_player = AnimationPlayer::new(&walk_fd_lt);
+            let walk_fd_rt_player = AnimationPlayer::new(&walk_fd_rt);
+            let walk_bk_lt_player = AnimationPlayer::new(&walk_bk_lt);
+            let walk_bk_rt_player = AnimationPlayer::new(&walk_bk_rt);
 
             let run_fd_player = AnimationPlayer::new(&run_fd);
             let run_bk_player = AnimationPlayer::new(&run_bk);
@@ -116,11 +155,11 @@ pub fn register_anim() {
             let run_bk_lt_player = AnimationPlayer::new(&run_bk_lt);
             let run_bk_rt_player = AnimationPlayer::new(&run_bk_rt);
 
-            let run_player = AnimationPlayer::new(&run);
-            let jump_player = AnimationPlayer::new(&jump);
-            let hit_player = AnimationPlayer::new(&hit);
-            let death_player = AnimationPlayer::new(&death);
-            let fire_player = AnimationPlayer::new(&fire);
+            // let run_player = AnimationPlayer::new(&run);
+            // let jump_player = AnimationPlayer::new(&jump);
+            // let hit_player = AnimationPlayer::new(&hit);
+            // let death_player = AnimationPlayer::new(&death);
+            // let fire_player = AnimationPlayer::new(&fire);
             let fd_lt_player = AnimationPlayer::new(&fd_lt);
             let fd_rt_player = AnimationPlayer::new(&fd_rt);
             let bk_lt_player = AnimationPlayer::new(&bk_lt);
@@ -136,38 +175,41 @@ pub fn register_anim() {
 
             entity::add_components(
                 id,
-                Entity::new().with(c::jump(), vec![jump.0 .0, jump_player.0]),
+                Entity::new()
+                    .with(c::walk_fd(), vec![walk_fd.0 .0, walk_fd_player.0])
+                    .with(c::walk_bk(), vec![walk_bk.0 .0, walk_bk_player.0])
+                    .with(c::walk_rt(), vec![walk_rt.0 .0, walk_rt_player.0])
+                    .with(c::walk_lt(), vec![walk_lt.0 .0, walk_lt_player.0])
+                    .with(c::walk_fd_rt(), vec![walk_fd_rt.0 .0, walk_fd_rt_player.0])
+                    .with(c::walk_fd_lt(), vec![walk_fd_lt.0 .0, walk_fd_lt_player.0])
+                    .with(c::walk_bk_rt(), vec![walk_bk_rt.0 .0, walk_bk_rt_player.0])
+                    .with(c::walk_bk_lt(), vec![walk_bk_lt.0 .0, walk_bk_lt_player.0])
+                    .with(c::run_fd(), vec![run_fd.0 .0, run_fd_player.0])
+                    .with(c::run_bk(), vec![run_bk.0 .0, run_bk_player.0])
+                    .with(c::run_rt(), vec![run_rt.0 .0, run_rt_player.0])
+                    .with(c::run_lt(), vec![run_lt.0 .0, run_lt_player.0])
+                    .with(c::run_fd_rt(), vec![run_fd_rt.0 .0, run_fd_rt_player.0])
+                    .with(c::run_fd_lt(), vec![run_fd_lt.0 .0, run_fd_lt_player.0])
+                    .with(c::run_bk_rt(), vec![run_bk_rt.0 .0, run_bk_rt_player.0])
+                    .with(c::run_bk_lt(), vec![run_bk_lt.0 .0, run_bk_lt_player.0])
+                    // .with(c::run(), vec![run.0 .0, run_player.0])
+                    // .with(c::jump(), vec![jump.0 .0, jump_player.0])
+                    // .with(c::hit(), vec![hit.0 .0, hit_player.0])
+                    // .with(c::death(), vec![death.0 .0, death_player.0])
+                    // .with(c::fire(), vec![fire.0 .0, fire_player.0])
+                    .with(c::fd_lt(), vec![fd_lt.0 .0, fd_lt_player.0])
+                    .with(c::fd_rt(), vec![fd_rt.0 .0, fd_rt_player.0])
+                    .with(c::bk_lt(), vec![bk_lt.0 .0, bk_lt_player.0])
+                    .with(c::bk_rt(), vec![bk_rt.0 .0, bk_rt_player.0])
+                    .with(c::idle_fd(), vec![idle_fd.0 .0, idle_fd_player.0])
+                    .with(c::idle_bk(), vec![idle_bk.0 .0, idle_bk_player.0])
+                    .with(c::idle_lt(), vec![idle_lt.0 .0, idle_lt_player.0])
+                    .with(c::idle_rt(), vec![idle_rt.0 .0, idle_rt_player.0])
+                    .with(c::idle_fd_lt(), vec![idle_fd_lt.0 .0, idle_fd_lt_player.0])
+                    .with(c::idle_fd_rt(), vec![idle_fd_rt.0 .0, idle_fd_rt_player.0])
+                    .with(c::idle_bk_lt(), vec![idle_bk_lt.0 .0, idle_bk_lt_player.0])
+                    .with(c::idle_bk_rt(), vec![idle_bk_rt.0 .0, idle_bk_rt_player.0]),
             );
-
-            // entity::add_components(
-            //     id,
-            //     Entity::new()
-            //         .with(c::run_fd(), vec![run_fd.0 .0, run_fd_player.0])
-            //         .with(c::run_bk(), vec![run_bk.0 .0, run_bk_player.0])
-            //         .with(c::run_rt(), vec![run_rt.0 .0, run_rt_player.0])
-            //         .with(c::run_lt(), vec![run_lt.0 .0, run_lt_player.0])
-            //         .with(c::run_fd_rt(), vec![run_fd_rt.0 .0, run_fd_rt_player.0])
-            //         .with(c::run_fd_lt(), vec![run_fd_lt.0 .0, run_fd_lt_player.0])
-            //         .with(c::run_bk_rt(), vec![run_bk_rt.0 .0, run_bk_rt_player.0])
-            //         .with(c::run_bk_lt(), vec![run_bk_lt.0 .0, run_bk_lt_player.0])
-            //         .with(c::run(), vec![run.0 .0, run_player.0])
-            //         .with(c::jump(), vec![jump.0 .0, jump_player.0])
-            //         .with(c::hit(), vec![hit.0 .0, hit_player.0])
-            //         .with(c::death(), vec![death.0 .0, death_player.0])
-            //         .with(c::fire(), vec![fire.0 .0, fire_player.0])
-            //         .with(c::fd_lt(), vec![fd_lt.0 .0, fd_lt_player.0])
-            //         .with(c::fd_rt(), vec![fd_rt.0 .0, fd_rt_player.0])
-            //         .with(c::bk_lt(), vec![bk_lt.0 .0, bk_lt_player.0])
-            //         .with(c::bk_rt(), vec![bk_rt.0 .0, bk_rt_player.0])
-            //         .with(c::idle_fd(), vec![idle_fd.0 .0, idle_fd_player.0])
-            //         .with(c::idle_bk(), vec![idle_bk.0 .0, idle_bk_player.0])
-            //         .with(c::idle_lt(), vec![idle_lt.0 .0, idle_lt_player.0])
-            //         .with(c::idle_rt(), vec![idle_rt.0 .0, idle_rt_player.0])
-            //         .with(c::idle_fd_lt(), vec![idle_fd_lt.0 .0, idle_fd_lt_player.0])
-            //         .with(c::idle_fd_rt(), vec![idle_fd_rt.0 .0, idle_fd_rt_player.0])
-            //         .with(c::idle_bk_lt(), vec![idle_bk_lt.0 .0, idle_bk_lt_player.0])
-            //         .with(c::idle_bk_rt(), vec![idle_bk_rt.0 .0, idle_bk_rt_player.0]),
-            // );
         }
     });
 }
--- src/fpsanim/server.rs
+++ src/fpsanim/server.rs
@@ -12,6 +12,7 @@ mod anim;
 
 #[main]
 pub fn main() {
+    anim::register_anim();
     spawn_query((player(), components::player_model_ref())).bind(|v| {
         for (id, (_, model)) in v {
             let jump = PlayClipFromUrlNode::new(
@@ -41,99 +42,160 @@ pub fn main() {
                     asset::url("assets/anim/Rifle Jump.fbx/animations/mixamo.com.anim").unwrap(),
                 );
                 jump.looping(false);
-                play_clip_on_model(model, jump);
+                let anim_player =
+                    entity::get_component(model, apply_animation_player()).unwrap_or_default();
+                let p = AnimationPlayer(anim_player);
+                p.play(jump);
                 entity::add_component(player_id, components::player_jumping(), false);
                 continue;
             }
 
-            if is_shooting {
-                let shoot = PlayClipFromUrlNode::new(
-                    asset::url("assets/anim/Rifle Firing.fbx/animations/mixamo.com.anim").unwrap(),
-                );
-                shoot.looping(false);
-                play_clip_on_model(model, shoot);
-                continue;
-            }
-
+            // if is_shooting {
+            //     let shoot = PlayClipFromUrlNode::new(
+            //         asset::url("assets/anim/Rifle Firing.fbx/animations/mixamo.com.anim").unwrap(),
+            //     );
+            //     shoot.looping(false);
+            //     play_clip_on_model(model, shoot);
+            //     continue;
+            // }
             let fd = dir.y == -1.0;
             let bk = dir.y == 1.0;
             let lt = dir.x == -1.0;
             let rt = dir.x == 1.0;
 
-            if fd && !lt && !rt {
-                play_clip_on_model(
-                    model,
-                    PlayClipFromUrlNode::new(
-                        asset::url("assets/anim/Walk Forward.fbx/animations/mixamo.com.anim")
-                            .unwrap(),
-                    ),
-                );
-            } else if bk && !lt && !rt {
-                play_clip_on_model(
-                    model,
-                    PlayClipFromUrlNode::new(
-                        asset::url("assets/anim/Walk Backward.fbx/animations/mixamo.com.anim")
-                            .unwrap(),
-                    ),
-                );
-            } else if lt && !fd && !bk {
-                play_clip_on_model(
-                    model,
-                    PlayClipFromUrlNode::new(
-                        asset::url("assets/anim/Walk Left.fbx/animations/mixamo.com.anim").unwrap(),
-                    ),
-                );
-            } else if rt && !fd && !bk {
-                play_clip_on_model(
-                    model,
-                    PlayClipFromUrlNode::new(
-                        asset::url("assets/anim/Walk Right.fbx/animations/mixamo.com.anim")
-                            .unwrap(),
-                    ),
-                );
-            } else if fd && lt {
-                play_clip_on_model(
-                    model,
-                    PlayClipFromUrlNode::new(
-                        asset::url("assets/anim/Walk Forward Left.fbx/animations/mixamo.com.anim")
-                            .unwrap(),
-                    ),
-                );
-            } else if fd && rt {
-                play_clip_on_model(
-                    model,
-                    PlayClipFromUrlNode::new(
-                        asset::url("assets/anim/Walk Forward Right.fbx/animations/mixamo.com.anim")
-                            .unwrap(),
-                    ),
-                );
-            } else if bk && lt {
-                play_clip_on_model(
-                    model,
-                    PlayClipFromUrlNode::new(
-                        asset::url("assets/anim/Walk Backward Left.fbx/animations/mixamo.com.anim")
-                            .unwrap(),
-                    ),
-                );
-            } else if bk && rt {
-                play_clip_on_model(
-                    model,
-                    PlayClipFromUrlNode::new(
-                        asset::url(
-                            "assets/anim/Walk Backward Right.fbx/animations/mixamo.com.anim",
-                        )
-                        .unwrap(),
-                    ),
-                );
+            if is_running {
+                if fd && !lt && !rt {
+                    apply_animation(player_id, components::run_fd());
+                } else if bk && !lt && !rt {
+                    apply_animation(player_id, components::run_bk());
+                } else if lt && !fd && !bk {
+                    apply_animation(player_id, components::run_lt());
+                } else if rt && !fd && !bk {
+                    apply_animation(player_id, components::run_rt());
+                } else if fd && lt {
+                    apply_animation(player_id, components::run_fd_lt());
+                } else if fd && rt {
+                    apply_animation(player_id, components::run_fd_rt());
+                } else if bk && lt {
+                    apply_animation(player_id, components::run_bk_lt());
+                } else if bk && rt {
+                    apply_animation(player_id, components::run_bk_rt());
+                } else {
+                    // TODO: there is a bug on multiple animations playing at the same time
+                    // I cannot use this commented line
+                    // there is a "hijack" bug on the animation player
+                    // have to create anim for each player
+                    apply_anim_blend(player_id, components::idle_fd(), 0.0);
+                    // apply_anim(player_id, components::idle_fd_lt(), 0.0);
+                }
+                continue;
             } else {
-                play_clip_on_model(
-                    model,
-                    PlayClipFromUrlNode::new(
-                        asset::url("assets/anim/Rifle Aiming Idle.fbx/animations/mixamo.com.anim")
-                            .unwrap(),
-                    ),
-                );
+                if fd && !lt && !rt {
+                    apply_animation(player_id, components::walk_fd());
+                } else if bk && !lt && !rt {
+                    apply_animation(player_id, components::walk_bk());
+                } else if lt && !fd && !bk {
+                    apply_animation(player_id, components::walk_lt());
+                } else if rt && !fd && !bk {
+                    apply_animation(player_id, components::walk_rt());
+                } else if fd && lt {
+                    apply_animation(player_id, components::walk_fd_lt());
+                } else if fd && rt {
+                    apply_animation(player_id, components::walk_fd_rt());
+                } else if bk && lt {
+                    apply_animation(player_id, components::walk_bk_lt());
+                } else if bk && rt {
+                    apply_animation(player_id, components::walk_bk_rt());
+                } else {
+                    // TODO: there is a bug on multiple animations playing at the same time
+                    // I cannot use this commented line
+                    // there is a "hijack" bug on the animation player
+                    // have to create anim for each player
+                    apply_anim_blend(player_id, components::idle_fd(), 0.0);
+                    // apply_anim(player_id, components::idle_fd_lt(), 0.0);
+                }
+                continue;
             }
+
+            // let fd = dir.y == -1.0;
+            // let bk = dir.y == 1.0;
+            // let lt = dir.x == -1.0;
+            // let rt = dir.x == 1.0;
+
+            // if fd && !lt && !rt {
+            //     play_clip_on_model(
+            //         model,
+            //         PlayClipFromUrlNode::new(
+            //             asset::url("assets/anim/Walk Forward.fbx/animations/mixamo.com.anim")
+            //                 .unwrap(),
+            //         ),
+            //     );
+            // } else if bk && !lt && !rt {
+            //     play_clip_on_model(
+            //         model,
+            //         PlayClipFromUrlNode::new(
+            //             asset::url("assets/anim/Walk Backward.fbx/animations/mixamo.com.anim")
+            //                 .unwrap(),
+            //         ),
+            //     );
+            // } else if lt && !fd && !bk {
+            //     play_clip_on_model(
+            //         model,
+            //         PlayClipFromUrlNode::new(
+            //             asset::url("assets/anim/Walk Left.fbx/animations/mixamo.com.anim").unwrap(),
+            //         ),
+            //     );
+            // } else if rt && !fd && !bk {
+            //     play_clip_on_model(
+            //         model,
+            //         PlayClipFromUrlNode::new(
+            //             asset::url("assets/anim/Walk Right.fbx/animations/mixamo.com.anim")
+            //                 .unwrap(),
+            //         ),
+            //     );
+            // } else if fd && lt {
+            //     play_clip_on_model(
+            //         model,
+            //         PlayClipFromUrlNode::new(
+            //             asset::url("assets/anim/Walk Forward Left.fbx/animations/mixamo.com.anim")
+            //                 .unwrap(),
+            //         ),
+            //     );
+            // } else if fd && rt {
+            //     play_clip_on_model(
+            //         model,
+            //         PlayClipFromUrlNode::new(
+            //             asset::url("assets/anim/Walk Forward Right.fbx/animations/mixamo.com.anim")
+            //                 .unwrap(),
+            //         ),
+            //     );
+            // } else if bk && lt {
+            //     play_clip_on_model(
+            //         model,
+            //         PlayClipFromUrlNode::new(
+            //             asset::url("assets/anim/Walk Backward Left.fbx/animations/mixamo.com.anim")
+            //                 .unwrap(),
+            //         ),
+            //     );
+            // } else if bk && rt {
+            //     play_clip_on_model(
+            //         model,
+            //         PlayClipFromUrlNode::new(
+            //             asset::url(
+            //                 "assets/anim/Walk Backward Right.fbx/animations/mixamo.com.anim",
+            //             )
+            //             .unwrap(),
+            //         ),
+            //     );
+            // } else {
+            //     play_clip_on_model(
+            //         model,
+            //         PlayClipFromUrlNode::new(
+            //             asset::url("assets/anim/Rifle Aiming Idle.fbx/animations/mixamo.com.anim")
+            //                 .unwrap(),
+            //         ),
+            //     );
+            // }
         }
     });
 
@@ -168,4 +230,33 @@ fn play_clip_on_model(model: EntityId, clip: PlayClipFromUrlNode) {
     let anim_player = entity::get_component(model, apply_animation_player()).unwrap_or_default();
     let p = AnimationPlayer(anim_player);
     p.play(clip);
+}
+
+pub fn apply_animation(player_id: EntityId, comp: Component<Vec<EntityId>>) {
+    let model = entity::get_component(player_id, components::player_model_ref());
+    if model.is_none() {
+        return;
+    }
+    let model = model.unwrap();
+    let anim_player = entity::get_component(player_id, comp);
+    if anim_player.is_none() {
+        return;
+    }
+    let anim_player = anim_player.unwrap();
+    entity::add_component(model, apply_animation_player(), anim_player[1]);
+}
+
+pub fn apply_anim_blend(player_id: EntityId, comp: Component<Vec<EntityId>>, blend_value: f32) {
+    let model = entity::get_component(player_id, components::player_model_ref());
+    if model.is_none() {
+        return;
+    }
+    let model = model.unwrap();
+    let blend_player = entity::get_component(player_id, comp);
+    if blend_player.is_none() {
+        return;
+    }
+    let blend_player = blend_player.unwrap();
+    entity::set_component(blend_player[0], blend(), blend_value);
+    entity::add_component(model, apply_animation_player(), blend_player[1]);
 }
