5c2216baa4e9b56b2bdc557551524cb055a89200
chaosprint <chaosprint@gmail.com>
2023-07-27 15:44:40 +0200

    async anim mechanism

--- src/fpsanim/server.rs
+++ src/fpsanim/server.rs
@@ -15,15 +15,15 @@ fn calculate_blend_from_weight(weights: &[f32]) -> Vec<f32> {
     for i in 0..weights.len() {
         total += weights[i];
     }
-    let mut left_weight = weights[0];
+    // left weight is used to compare left and right
+    let mut left_weight = weights[0] / total;
     for i in 0..weights.len() - 1 {
-        left_weight += weights[i + 1];
+        left_weight += weights[i + 1] / total;
         let b: f32 = if left_weight != 0.0 {
             weights[i + 1] / left_weight
         } else {
             0.0
         };
-
         blend.push(b);
     }
     blend
@@ -41,22 +41,82 @@ impl FPSAnimBlend {
         let walk_fd = PlayClipFromUrlNode::new(
             asset::url("assets/anim/Walk Forward.fbx/animations/mixamo.com.anim").unwrap(),
         );
-        let jump = PlayClipFromUrlNode::new(
-            asset::url("assets/anim/Rifle Jump.fbx/animations/mixamo.com.anim").unwrap(),
+        let walk_bk = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Walk Backward.fbx/animations/mixamo.com.anim").unwrap(),
         );
-        jump.looping(false);
+        let walk_lt = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Walk Left.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let walk_rt = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Walk Right.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let walk_fd_lt = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Walk Forward Left.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let walk_fd_rt = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Walk Forward Right.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let walk_bk_lt = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Walk Backward Left.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let walk_bk_rt = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Walk Backward Right.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let run_fd = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Run Forward.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let run_bk = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Run Backward.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let run_lt = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Run Left.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let run_rt = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Run Right.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let run_fd_lt = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Run Forward Left.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let run_fd_rt = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Run Forward Right.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let run_bk_lt = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Run Backward Left.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+        let run_bk_rt = PlayClipFromUrlNode::new(
+            asset::url("assets/anim/Run Backward Right.fbx/animations/mixamo.com.anim").unwrap(),
+        );
+
         let idle = PlayClipFromUrlNode::new(
             asset::url("assets/anim/Rifle Aiming Idle.fbx/animations/mixamo.com.anim").unwrap(),
         );
-        let blend1 = BlendNode::new(&walk_fd, &jump, 0.5);
-        let blend2 = BlendNode::new(&blend1, &idle, 0.5);
+        let blend1 = BlendNode::new(&walk_fd, &walk_bk, 0.0);
+        let blend2 = BlendNode::new(&blend1, &walk_lt, 0.0);
+        let blend3 = BlendNode::new(&blend2, &walk_rt, 0.0);
+        let blend4 = BlendNode::new(&blend3, &walk_fd_lt, 0.0);
+        let blend5 = BlendNode::new(&blend4, &walk_fd_rt, 0.0);
+        let blend6 = BlendNode::new(&blend5, &walk_bk_lt, 0.0);
+        let blend7 = BlendNode::new(&blend6, &walk_bk_rt, 0.0);
+        let blend8 = BlendNode::new(&blend7, &run_fd, 0.0);
+        let blend9 = BlendNode::new(&blend8, &run_bk, 0.0);
+        let blend10 = BlendNode::new(&blend9, &run_lt, 0.0);
+        let blend11 = BlendNode::new(&blend10, &run_rt, 0.0);
+        let blend12 = BlendNode::new(&blend11, &run_fd_lt, 0.0);
+        let blend13 = BlendNode::new(&blend12, &run_fd_rt, 0.0);
+        let blend14 = BlendNode::new(&blend13, &run_bk_lt, 0.0);
+        let blend15 = BlendNode::new(&blend14, &run_bk_rt, 0.0);
+        let blend16 = BlendNode::new(&blend15, &idle, 0.0);
 
         Self {
-            nodes: vec![blend1, blend2],
+            nodes: vec![
+                blend1, blend2, blend3, blend4, blend5, blend6, blend7, blend8, blend9, blend10,
+                blend11, blend12, blend13, blend14, blend15, blend16,
+            ],
         }
     }
     pub fn update_weights(&mut self, weights: &[f32]) {
         let blend = calculate_blend_from_weight(weights);
+        println!("current frame blend{:?}", blend);
         for i in 0..self.nodes.len() {
             self.nodes[i].set_weight(blend[i]);
         }
@@ -83,39 +143,95 @@ pub fn main() {
         .bind(move |res| {
             for (player_id, (_, is_jumping)) in res {
                 let anim_lib = anim_lib_once.borrow_mut();
-                let (mut blend, anim_player) = anim_lib.get(&player_id).unwrap().clone();
+                let (blend, anim_player) = anim_lib.get(&player_id).unwrap().clone();
                 if is_jumping {
                     let clip = PlayClipFromUrlNode::new(
                         asset::url("assets/anim/Rifle Jump.fbx/animations/mixamo.com.anim")
                             .unwrap(),
                     );
                     clip.looping(false);
+
                     anim_player.play(clip);
-                } else {
-                    anim_player.play(blend.nodes.last().unwrap());
-                }
+
+                    run_async(async move {
+                        let clip = PlayClipFromUrlNode::new(
+                            asset::url("assets/anim/Rifle Jump.fbx/animations/mixamo.com.anim")
+                                .unwrap(),
+                        );
+                        clip.looping(false);
+                        let dur = clip.clip_duration().await;
+                        std::thread::sleep(std::time::Duration::from_secs_f32(dur));
+                        let another_clip = PlayClipFromUrlNode::new(
+                            asset::url("assets/anim/Rifle Death.fbx/animations/mixamo.com.anim")
+                                .unwrap(),
+                        );
+                        another_clip.looping(false);
+                        anim_player.play(another_clip);
+                    });
+                };
+
+                //  else {
+                //     anim_player.play(blend.nodes.last().unwrap());
+                // }
             }
         });
     query((
         player(),
         components::player_model_ref(),
-        // components::player_jumping(),
         components::player_direction(),
+        components::player_running(),
     ))
     .each_frame(move |res| {
-        for (player_id, (_, _model, dir)) in res {
-            let mut weights = vec![1.0, 0.0, 0.0];
+        for (player_id, (_, _model, dir, is_running)) in res {
+            let mut weights = vec![0.0; 17];
             let anim_lib = anim_lib_clone.borrow_mut();
             let (mut blend, _anim_player) = anim_lib.get(&player_id).unwrap().clone();
+            let fd = dir.y == -1.0;
+            let bk = dir.y == 1.0;
+            let lt = dir.x == -1.0;
+            let rt = dir.x == 1.0;
 
-            if dir.x == 0.0 && dir.y == 0.0 {
-                //idle
-                weights = vec![0.0, 0.0, 1.0];
+            if !is_running {
+                if fd && !lt && !rt {
+                    weights[0] = 1.0;
+                } else if bk && !lt && !rt {
+                    weights[1] = 1.0;
+                } else if lt && !fd && !bk {
+                    weights[2] = 1.0;
+                } else if rt && !fd && !bk {
+                    weights[3] = 1.0;
+                } else if fd && lt {
+                    weights[4] = 1.0;
+                } else if fd && rt {
+                    weights[5] = 1.0;
+                } else if bk && lt {
+                    weights[6] = 1.0;
+                } else if bk && rt {
+                    weights[7] = 1.0;
+                } else {
+                    weights[16] = 1.0;
+                }
             } else {
-                // walk
-                weights = vec![1.0, 0.0, 0.0];
+                if fd && !lt && !rt {
+                    weights[8] = 1.0;
+                } else if bk && !lt && !rt {
+                    weights[9] = 1.0;
+                } else if lt && !fd && !bk {
+                    weights[10] = 1.0;
+                } else if rt && !fd && !bk {
+                    weights[11] = 1.0;
+                } else if fd && lt {
+                    weights[12] = 1.0;
+                } else if fd && rt {
+                    weights[13] = 1.0;
+                } else if bk && lt {
+                    weights[14] = 1.0;
+                } else if bk && rt {
+                    weights[15] = 1.0;
+                } else {
+                    weights[16] = 1.0;
+                }
             }
-            // }
             blend.update_weights(&weights);
             println!("current frame weight{:?}", weights);
         }
