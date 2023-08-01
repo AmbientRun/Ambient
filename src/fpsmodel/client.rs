7cdf54513264915eabdc92b0c21251339e0c7132
chaosprint <chaosprint@gmail.com>
2023-08-01 10:00:32 +0200

    new gun

--- src/fpsmodel/client.rs
+++ src/fpsmodel/client.rs
@@ -26,17 +26,14 @@ pub fn main() {
                 let hand = hand.unwrap();
                 let gun = Entity::new()
                     .with_merge(make_transformable())
-                    .with(
-                        prefab_from_url(),
-                        asset::url("assets/gun/m4a1_carbine.glb").unwrap(),
-                    )
-                    // y => far from body; need more tuning
-                    .with(translation(), vec3(0.0, 0.2, 0.0))
+                    .with(prefab_from_url(), asset::url("assets/gun/red.glb").unwrap())
+                    .with(translation(), vec3(-0.1, 0.2, 0.0))
                     .with(
                         rotation(),
-                        Quat::from_rotation_y(-std::f32::consts::FRAC_PI_2),
+                        Quat::from_rotation_y(-std::f32::consts::FRAC_PI_2)
+                            * Quat::from_rotation_z(std::f32::consts::FRAC_PI_2),
                     )
-                    .with(scale(), Vec3::ONE * 0.01)
+                    .with(scale(), Vec3::ONE * 0.3)
                     .with(color(), vec4(1.0, 1.0, 0.0, 1.0))
                     .with_default(local_to_parent())
                     .with_default(reset_scale())
