bd3e5032953a7f45953babca40aaaa2cb7ea5cdb
Philpax <me@philpax.me>
2023-08-11 02:20:51 +0200

    feat(editor): add selected outline+gizmo

--- mods/editor/src/client.rs
+++ mods/editor/src/client.rs
@@ -1,7 +1,10 @@
 use ambient_api::{
     core::{
-        app::components::name, messages::Frame, rendering::components::color,
-        transform::components::translation,
+        app::components::{main_scene, name},
+        messages::Frame,
+        rect::components::{line_from, line_to, line_width, rect},
+        rendering::components::{color, double_sided},
+        transform::components::{rotation, translation},
     },
     input::CursorLockGuard,
     prelude::*,
@@ -102,7 +105,7 @@ pub fn App(hooks: &mut Hooks) -> Element {
     });
 
     if in_editor {
-        Group::el([MenuBar::el(), MouseoverDisplay::el()])
+        Group::el([MenuBar::el(), MouseoverDisplay::el(), SelectedDisplay::el()])
     } else {
         Element::new()
     }
@@ -145,6 +148,139 @@ fn MouseoverDisplay(hooks: &mut Hooks) -> Element {
     Text::el(text)
         .with(translation(), mouseover_position_2d)
         .with(color(), Vec4::ONE)
+}
+
+#[element_component]
+fn SelectedDisplay(hooks: &mut Hooks) -> Element {
+    let player_id = player::get_local();
+    let (selected_entity, _) = hooks.use_entity_component(player_id, selected_entity());
+    let (camera_id, _) = hooks.use_entity_component(player_id, editor_camera());
+
+    // TODO: is there a better way to force this element to re-render every frame?
+    let rerender = hooks.use_rerender_signal();
+    hooks.use_frame(move |_| {
+        rerender();
+    });
+
+    let Some(selected_entity) = selected_entity else { return Element::new(); };
+    let Some(camera_id) = camera_id else { return Element::new(); };
+
+    let position = entity::get_component(selected_entity, translation()).unwrap_or_default();
+    Group::el([
+        Text::el(format!(
+            "{:.02?}\n{}",
+            position.to_array(),
+            display_entity(selected_entity)
+        ))
+        .with(
+            translation(),
+            camera::world_to_screen(camera_id, position).extend(0.0),
+        )
+        .with(color(), Vec4::ONE),
+        Gizmo::el(camera_id, selected_entity),
+    ])
+}
+
+const GIZMO_LENGTH: f32 = 5.;
+const GIZMO_WIDTH: f32 = 0.25;
+
+#[element_component]
+fn Gizmo(_hooks: &mut Hooks, camera_id: EntityId, entity: EntityId) -> Element {
+    Group::el(
+        GizmoLine::for_entity(entity)
+            .into_iter()
+            .map(|l| l.as_element(camera_id)),
+    )
+}
+
+struct GizmoLine {
+    origin: Vec3,
+    direction: Vec3,
+    color: Vec3,
+}
+impl GizmoLine {
+    fn new(origin: Vec3, direction: Vec3, color: Vec3) -> Self {
+        Self {
+            origin,
+            direction,
+            color,
+        }
+    }
+
+    fn for_entity(id: EntityId) -> [Self; 3] {
+        let origin = entity::get_component(id, translation()).unwrap_or_default();
+        let rotation = entity::get_component(id, rotation()).unwrap_or_default();
+
+        [
+            Self::new(origin, rotation * Vec3::X, vec3(1., 0., 0.)),
+            Self::new(origin, rotation * Vec3::Y, vec3(0., 1., 0.)),
+            Self::new(origin, rotation * Vec3::Z, vec3(0., 0., 1.)),
+        ]
+    }
+
+    fn as_element(&self, camera_id: EntityId) -> Element {
+        let our_color = if self.is_moused_over(camera_id, input::get().mouse_position) {
+            self.color
+        } else {
+            self.color * 0.6
+        }
+        .extend(1.0);
+
+        let head_length = 0.1;
+        let line_end = self.origin + self.direction * (GIZMO_LENGTH * (1. - head_length));
+        let head_end = line_end + self.direction * (GIZMO_LENGTH * head_length);
+
+        Group::el([
+            Element::new()
+                .init_default(rect())
+                .with_default(main_scene())
+                .with(line_from(), self.origin)
+                .with(line_to(), line_end)
+                .with(line_width(), GIZMO_WIDTH)
+                .with(double_sided(), true)
+                .with(color(), our_color),
+            Element::new()
+                .init_default(rect())
+                .with_default(main_scene())
+                .with(line_from(), line_end)
+                .with(line_to(), head_end)
+                .with(line_width(), GIZMO_WIDTH * 3.)
+                .with(double_sided(), true)
+                .with(color(), our_color),
+        ])
+    }
+
+    fn is_moused_over(&self, camera_id: EntityId, mouse_position: Vec2) -> bool {
+        is_mouse_in_cylinder(
+            self.origin,
+            self.direction,
+            // such a bodge
+            GIZMO_WIDTH * 10.,
+            GIZMO_LENGTH * 1.5,
+            camera_id,
+            mouse_position,
+        )
+    }
+}
+
+fn is_mouse_in_cylinder(
+    position: Vec3,
+    direction: Vec3,
+    radius: f32,
+    length: f32,
+    camera_id: EntityId,
+    mouse_position: Vec2,
+) -> bool {
+    let screen_pos = camera::world_to_screen(camera_id, position);
+    let screen_dir = camera::world_to_screen(camera_id, position + direction * length) - screen_pos;
+    let screen_mouse_pos = mouse_position - screen_pos;
+
+    let screen_mouse_pos_on_cylinder_axis =
+        screen_mouse_pos.dot(screen_dir) / screen_dir.dot(screen_dir) * screen_dir;
+    let screen_mouse_pos_on_cylinder = screen_mouse_pos - screen_mouse_pos_on_cylinder_axis;
+    let distance_to_cylinder = screen_mouse_pos_on_cylinder.length();
+
+    distance_to_cylinder <= radius
 }
 
 // todo: move to API
