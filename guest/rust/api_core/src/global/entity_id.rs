8dbf53cd27e8f3acbb2cea62fa635a223bb48b41
Philpax <me@philpax.me>
2023-08-23 13:59:41 +0200

    fix #731 - use base64 for EntityId output

--- guest/rust/api_core/src/global/entity_id.rs
+++ guest/rust/api_core/src/global/entity_id.rs
@@ -3,35 +3,48 @@ use crate::internal::{
     wit,
 };
 
+use data_encoding::BASE64URL_NOPAD as BASE64;
+
 /// An identifier for an entity in the world.
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 pub struct EntityId {
     #[doc(hidden)]
-    pub(crate) id0: u64,
+    pub id0: u64,
     #[doc(hidden)]
-    pub(crate) id1: u64,
+    pub id1: u64,
 }
 impl EntityId {
-    #[doc(hidden)]
+    /// Create a new [EntityId] from a Base64 representation.
+    ///
+    /// # Panics
+    /// This will panic if
+    /// - the Base64 string is more than 16 bytes
+    /// - the Base64 string is not a valid EntityId.
     pub fn from_base64(encoded: &str) -> Self {
-        let len = data_encoding::BASE64URL_NOPAD
-            .decode_len(encoded.len())
-            .unwrap();
+        let len = BASE64.decode_len(encoded.len()).unwrap();
 
-        if len >= 16 {
-            panic!("base64 entityid is more than 16 bytes");
+        if len > 16 {
+            panic!("base64 EntityId length {len} > 16");
         }
 
         let mut bytes = [0u8; 16];
-        data_encoding::BASE64URL_NOPAD
-            .decode_mut(encoded.as_bytes(), &mut bytes)
-            .unwrap();
+        BASE64.decode_mut(encoded.as_bytes(), &mut bytes).unwrap();
 
         Self {
             id0: u64::from_le_bytes(bytes[0..8].try_into().unwrap()),
             id1: u64::from_le_bytes(bytes[8..].try_into().unwrap()),
         }
     }
+
+    /// Convert this [EntityId] to a Base64 representation.
+    pub fn to_base64(&self) -> String {
+        let mut bytes = [0u8; 16];
+        bytes[0..8].copy_from_slice(&self.id0.to_le_bytes());
+        bytes[8..].copy_from_slice(&self.id1.to_le_bytes());
+
+        BASE64.encode(&bytes[..])
+    }
+
     /// Return a null [EntityId]
     pub const fn null() -> Self {
         Self { id0: 0, id1: 0 }
@@ -56,7 +69,7 @@ impl Default for EntityId {
 }
 impl std::fmt::Display for EntityId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", ((self.id0 as u128) << 64) + self.id1 as u128)
+        write!(f, "{}", self.to_base64())
     }
 }
 impl std::fmt::Debug for EntityId {
