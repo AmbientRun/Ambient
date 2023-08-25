bffebbebe099386d7f79a45eed3791454a16a57a
Philpax <me@philpax.me>
2023-08-25 23:10:57 +0200

    feat: move tools out, update deployments

--- tools/package_manager/src/client/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod package_load;
-pub mod package_manager;
-pub mod util;
-pub mod wasm_manager;
-pub mod window;
-
-pub use util::*;
-pub use window::Window;
--- tools/package_manager/src/server/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod package_load;
-pub mod package_manager;
-pub mod wasm_manager;
