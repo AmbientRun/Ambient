5abeb5b4a917999826f800d1f9829d53387ba416
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-09-05 17:48:19 +0200

    Use basic_character_animations in zombie mod

--- guest/rust/packages/std/basic_character_animation/src/server.rs
+++ guest/rust/packages/std/basic_character_animation/src/server.rs
@@ -8,6 +8,7 @@ use ambient_api::{
 };
 use packages::{
     this::assets,
+    this::components::*,
     unit_schema::components::{health, jumping, run_direction, running},
 };
 use std::{
@@ -16,12 +17,21 @@ use std::{
 };
 
 fn anim_url(name: &str) -> String {
-    assets::url(&format!("{name}/animations/mixamo.com.anim"))
+    assets::url(&format!("{name}.fbx/animations/mixamo.com.anim"))
+}
+
+fn clip(owner: EntityId, component: Component<String>, fallback: &str) -> String {
+    if let Some(url) = get_component(owner, component) {
+        url
+    } else {
+        anim_url(fallback)
+    }
 }
 
 #[element_component(without_el)]
 fn UnitAnimation(
     _hooks: &mut Hooks,
+    owner_id: EntityId,
     direction: Vec2,
     running: bool,
     jumping: bool,
@@ -31,18 +41,23 @@ fn UnitAnimation(
         root: Transition {
             animations: vec![
                 PlayClipFromUrl {
-                    url: anim_url("Rifle Death.fbx"),
+                    url: clip(owner_id, death(), "Rifle Death"),
                     looping: false,
                 }
                 .el()
                 .key("death"),
                 PlayClipFromUrl {
-                    url: anim_url("Rifle Jump.fbx"),
+                    url: clip(owner_id, jump(), "Rifle Jump"),
                     looping: false,
                 }
                 .el()
                 .key("jump"),
-                Walk { direction, running }.el(),
+                Walk {
+                    owner_id,
+                    direction,
+                    running,
+                }
+                .el(),
             ],
             active: if health <= 0. {
                 0
@@ -60,6 +75,7 @@ fn UnitAnimation(
 impl UnitAnimation {
     fn from_entity(entity: EntityId) -> Self {
         Self {
+            owner_id: entity,
             direction: get_component(entity, run_direction()).unwrap_or_default(),
             running: get_component(entity, running()).unwrap_or_default(),
             jumping: get_component(entity, jumping()).unwrap_or_default(),
@@ -69,19 +85,19 @@ impl UnitAnimation {
 }
 
 #[element_component]
-fn Walk(hooks: &mut Hooks, running: bool, direction: Vec2) -> Element {
+fn Walk(hooks: &mut Hooks, owner_id: EntityId, running: bool, direction: Vec2) -> Element {
     let lagging_direction = hooks.use_ref_with(|_| Vec3::ZERO);
     let mut lag_dir = lagging_direction.lock();
     *lag_dir = lag_dir.lerp(direction.extend(if running { 1. } else { 0. }), 0.1);
 
-    fn walkblend(items: &[(&str, f32)]) -> Element {
+    fn walkblend(items: &[(String, f32)]) -> Element {
         BlendNode::normalize_multiblend(
             items
                 .iter()
                 .map(|(a, w)| {
                     (
                         PlayClipFromUrl {
-                            url: anim_url(a),
+                            url: a.clone(),
                             looping: true,
                         }
                         .el()
@@ -93,35 +109,74 @@ fn Walk(hooks: &mut Hooks, running: bool, direction: Vec2) -> Element {
         )
     }
     walkblend(&[
-        ("Rifle Aiming Idle.fbx", lag_dir.xy().distance(Vec2::ZERO)),
-        ("Walk Forward.fbx", lag_dir.distance(vec3(0., -1., 0.))),
         (
-            "Walk Forward Left.fbx",
+            clip(owner_id, idle(), "Rifle Aiming Idle"),
+            lag_dir.xy().distance(Vec2::ZERO),
+        ),
+        (
+            clip(owner_id, walk_forward(), "Walk Forward"),
+            lag_dir.distance(vec3(0., -1., 0.)),
+        ),
+        (
+            clip(owner_id, walk_forward_left(), "Walk Forward Left"),
             lag_dir.distance(vec3(-1., -1., 0.)),
         ),
         (
-            "Walk Forward Right.fbx",
+            clip(owner_id, walk_forward_right(), "Walk Forward Right"),
             lag_dir.distance(vec3(1., -1., 0.)),
         ),
-        ("Walk Backward.fbx", lag_dir.distance(vec3(0., 1., 0.))),
         (
-            "Walk Backward Left.fbx",
+            clip(owner_id, walk_backward(), "Walk Backward"),
+            lag_dir.distance(vec3(0., 1., 0.)),
+        ),
+        (
+            clip(owner_id, walk_backward_left(), "Walk Backward Left"),
             lag_dir.distance(vec3(-1., 1., 0.)),
         ),
         (
-            "Walk Backward Right.fbx",
+            clip(owner_id, walk_backward_right(), "Walk Backward Right"),
             lag_dir.distance(vec3(1., 1., 0.)),
         ),
-        ("Walk Left.fbx", lag_dir.distance(vec3(-1., 0., 0.))),
-        ("Walk Right.fbx", lag_dir.distance(vec3(1., 0., 0.))),
-        ("Run Forward.fbx", lag_dir.distance(vec3(0., -1., 1.))),
-        ("Run Forward Left.fbx", lag_dir.distance(vec3(-1., -1., 1.))),
-        ("Run Forward Right.fbx", lag_dir.distance(vec3(1., -1., 1.))),
-        ("Run Backward.fbx", lag_dir.distance(vec3(0., 1., 1.))),
-        ("Run Backward Left.fbx", lag_dir.distance(vec3(-1., 1., 1.))),
-        ("Run Backward Right.fbx", lag_dir.distance(vec3(1., 1., 1.))),
-        ("Run Left.fbx", lag_dir.distance(vec3(-1., 0., 1.))),
-        ("Run Right.fbx", lag_dir.distance(vec3(1., 0., 1.))),
+        (
+            clip(owner_id, walk_left(), "Walk Left"),
+            lag_dir.distance(vec3(-1., 0., 0.)),
+        ),
+        (
+            clip(owner_id, walk_right(), "Walk Right"),
+            lag_dir.distance(vec3(1., 0., 0.)),
+        ),
+        (
+            clip(owner_id, run_forward(), "Run Forward"),
+            lag_dir.distance(vec3(0., -1., 1.)),
+        ),
+        (
+            clip(owner_id, run_forward_left(), "Run Forward Left"),
+            lag_dir.distance(vec3(-1., -1., 1.)),
+        ),
+        (
+            clip(owner_id, run_forward_right(), "Run Forward Right"),
+            lag_dir.distance(vec3(1., -1., 1.)),
+        ),
+        (
+            clip(owner_id, run_backward(), "Run Backward"),
+            lag_dir.distance(vec3(0., 1., 1.)),
+        ),
+        (
+            clip(owner_id, run_backward_left(), "Run Backward Left"),
+            lag_dir.distance(vec3(-1., 1., 1.)),
+        ),
+        (
+            clip(owner_id, run_backward_right(), "Run Backward Right"),
+            lag_dir.distance(vec3(1., 1., 1.)),
+        ),
+        (
+            clip(owner_id, run_left(), "Run Left"),
+            lag_dir.distance(vec3(-1., 0., 1.)),
+        ),
+        (
+            clip(owner_id, run_right(), "Run Right"),
+            lag_dir.distance(vec3(1., 0., 1.)),
+        ),
     ])
 }
 
@@ -130,23 +185,23 @@ pub fn main() {
     fn preload(name: &str) {
         PlayClipFromUrlNodeRef::new(anim_url(name));
     }
-    preload("Walk Forward.fbx");
-    preload("Walk Backward.fbx");
-    preload("Walk Left.fbx");
-    preload("Walk Right.fbx");
-    preload("Walk Forward Left.fbx");
-    preload("Walk Forward Right.fbx");
-    preload("Walk Backward Left.fbx");
-    preload("Walk Backward Right.fbx");
-    preload("Run Forward.fbx");
-    preload("Run Backward.fbx");
-    preload("Run Left.fbx");
-    preload("Run Right.fbx");
-    preload("Run Forward Left.fbx");
-    preload("Run Forward Right.fbx");
-    preload("Run Backward Left.fbx");
-    preload("Run Backward Right.fbx");
-    preload("Rifle Aiming Idle.fbx");
+    preload("Walk Forward");
+    preload("Walk Backward");
+    preload("Walk Left");
+    preload("Walk Right");
+    preload("Walk Forward Left");
+    preload("Walk Forward Right");
+    preload("Walk Backward Left");
+    preload("Walk Backward Right");
+    preload("Run Forward");
+    preload("Run Backward");
+    preload("Run Left");
+    preload("Run Right");
+    preload("Run Forward Left");
+    preload("Run Forward Right");
+    preload("Run Backward Left");
+    preload("Run Backward Right");
+    preload("Rifle Aiming Idle");
 
     let anims = Arc::new(Mutex::new(HashMap::<EntityId, ElementTree>::new()));
 
