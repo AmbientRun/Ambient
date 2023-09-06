10cab4f0cadf21c9bb933cb527f5da227d0a4970
Tei Leelo Roberts <ten3roberts@gmail.com>
2023-09-06 18:01:15 +0200

    fix: show module name

--- web/client/src/wasm.rs
+++ web/client/src/wasm.rs
@@ -2,7 +2,6 @@ use ambient_core::asset_cache;
 // use ambient_audio::AudioMixer;
 use ambient_ecs::{EntityId, SystemGroup, World};
 use ambient_wasm::shared::{module_name, MessageType};
-use tracing::Level;
 
 use std::sync::Arc;
 
@@ -16,15 +15,15 @@ pub fn initialize(world: &mut World) -> anyhow::Result<()> {
 
     let messenger = Arc::new(
         |world: &World, id: EntityId, type_: MessageType, message: &str| {
-            let name = world.get_cloned(id, module_name()).unwrap_or_default();
+            let module_name = world.get_cloned(id, module_name()).unwrap_or_default();
             let message = message.trim();
 
             match type_ {
-                MessageType::Info => tracing::info!("{}", message),
-                MessageType::Warn => tracing::warn!("{}", message),
-                MessageType::Error => tracing::error!("{}", message),
-                MessageType::Stdout => tracing::info!("stdout {}", message),
-                MessageType::Stderr => tracing::info!("stderr: {}", message),
+                MessageType::Info => tracing::info!(%module_name, "{}", message),
+                MessageType::Warn => tracing::warn!(%module_name, "{}", message),
+                MessageType::Error => tracing::error!(%module_name, "{}", message),
+                MessageType::Stdout => tracing::info!(%module_name, "stdout {}", message),
+                MessageType::Stderr => tracing::info!(%module_name, "stderr: {}", message),
             };
         },
     );
