17c898ffc635cfaf0b6ff25682ee17a3d7956334
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-09-08 16:58:22 +0200

    More cleanup

--- guest/rust/packages/std/hide_cursor/src/client.rs
+++ guest/rust/packages/std/hide_cursor/src/client.rs
@@ -1,23 +1,15 @@
 use ambient_api::{
     core::{
-        app::components::{main_scene, name},
-        camera::{
-            components::aspect_ratio_from_window,
-            concepts::make_perspective_infinite_reverse_camera,
-        },
         messages::Frame,
-        player::components::is_player,
-        transform::components::{local_to_parent, translation},
         ui::{components::focus, messages::FocusChanged},
     },
-    entity::{add_child, get_component, mutate_component_with_default, resources, set_component},
+    entity::{get_component, resources, set_component},
     input::{set_cursor_lock, set_cursor_visible},
     prelude::*,
 };
 
 fn update() {
     let current_focus = get_component(resources(), focus()).unwrap_or_default();
-    println!("FOCUS: {}", current_focus);
     if current_focus.is_empty() {
         set_cursor_lock(true);
         set_cursor_visible(false);
