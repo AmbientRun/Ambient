3f0cec6d8f738ac9aff39c1faa92f2b6ef4bf125
Philpax <me@philpax.me>
2023-09-14 11:42:50 +0200

    refactor(package_manager): split out package list

--- guest/rust/packages/tools/package_manager/src/client/package_manager.rs
+++ guest/rust/packages/tools/package_manager/src/client/package_manager.rs
@@ -1,21 +1,16 @@
-use crate::ambient_element::use_module_message;
-use crate::ambient_element::use_spawn;
 use ambient_api::{
     core::{
-        package::components::{
-            authors, client_modules, description, enabled, is_package, name, server_modules,
-            version,
-        },
+        package::{components::description, concepts::Package},
         text::{components::font_style, types::FontStyle},
     },
-    element::use_query,
+    element::{use_module_message, use_query, use_spawn},
     prelude::*,
     ui::ImageFromUrl,
 };
 
 use crate::packages::this::{
     assets,
-    messages::{PackageLoadShow, PackageSetEnabled, PackageShow, WasmReload},
+    messages::{PackageLoadShow, PackageSetEnabled, PackageShow},
 };
 
 use super::use_hotkey_toggle;
@@ -38,114 +33,104 @@ pub fn PackageManager(hooks: &mut Hooks) -> Element {
 
 #[element_component]
 fn PackageManagerInner(hooks: &mut Hooks) -> Element {
-    let packages = use_query(
-        hooks,
-        (
-            is_package(),
-            enabled(),
-            name(),
-            version(),
-            authors(),
-            client_modules(),
-            server_modules(),
-        ),
-    );
+    let packages = use_query(hooks, Package::as_query());
 
-    struct Package {
-        enabled: bool,
-        name: String,
-        version: String,
-        authors: Vec<String>,
-        description: Option<String>,
-        client_modules: Vec<EntityId>,
-        server_modules: Vec<EntityId>,
-    }
-
-    let mut packages: Vec<_> = packages
+    let display_packages: Vec<_> = packages
         .into_iter()
-        .map(
-            |(id, (_, enabled, name, version, authors, client_modules, server_modules))| {
-                let description = entity::get_component(id, description());
+        .map(|(id, package)| {
+            let description = entity::get_component(id, description());
 
-                (
-                    id,
-                    Package {
-                        enabled,
-                        name,
-                        version,
-                        authors,
-                        description,
-                        client_modules,
-                        server_modules,
-                    },
-                )
-            },
-        )
+            DisplayPackage {
+                source: DisplayPackageSource::Local { id },
+                enabled: package.enabled,
+                name: package.name,
+                version: package.version,
+                authors: package.authors,
+                description,
+            }
+        })
         .collect();
-    packages.sort_by_key(|(_, package)| package.name.clone());
 
-    FlowColumn::el(Iterator::chain(
-        [Button::new("Load package", |_| {
+    FlowColumn::el([
+        Button::new("Load package", |_| {
             PackageLoadShow.send_local(crate::packages::this::entity())
         })
-        .el()]
-        .into_iter(),
-        packages.into_iter().map(|(id, package)| {
-            FlowRow::el([
-                ImageFromUrl {
-                    url: assets::url("construction.png"),
-                }
-                .el()
-                .with(width(), 48.0)
-                .with(height(), 48.0),
-                FlowColumn::el([
-                    Checkbox::new(package.enabled, move |value| {
-                        PackageSetEnabled { id, enabled: value }.send_server_reliable();
-                    })
-                    .el(),
-                    Button::new(FontAwesomeIcon::el(0xf2f1, true), {
-                        let modules: Vec<_> = package
-                            .client_modules
-                            .iter()
-                            .chain(package.server_modules.iter())
-                            .copied()
-                            .collect();
-                        move |_| {
-                            for &id in &modules {
-                                WasmReload::new(id).send_server_reliable();
-                            }
-                        }
-                    })
-                    .style(ButtonStyle::Flat)
-                    .el(),
-                ]),
-                FlowColumn::el([
-                    FlowRow::el([
-                        Text::el(package.name).with(font_style(), FontStyle::Bold),
-                        Text::el(package.version),
-                        Text::el("by"),
-                        Text::el(if package.authors.is_empty() {
-                            "No authors specified".to_string()
-                        } else {
-                            package.authors.join(", ")
-                        })
-                        .with(font_style(), FontStyle::Italic),
-                    ])
-                    .with(space_between_items(), 4.0),
-                    Text::el(package.description.as_deref().unwrap_or("No description")),
-                    Button::new("View", move |_| {
-                        PackageShow { id }.send_local(crate::packages::this::entity())
-                    })
-                    .style(ButtonStyle::Flat)
-                    .el(),
-                ])
-                .with(space_between_items(), 8.0),
-            ])
-            .with(space_between_items(), 8.0)
-        }),
-    ))
+        .el(),
+        PackageList::el(display_packages),
+    ])
     .with(space_between_items(), 4.0)
     .with_margin_even(STREET)
+}
+
+#[derive(Clone, Debug)]
+struct DisplayPackage {
+    source: DisplayPackageSource,
+    enabled: bool,
+    name: String,
+    version: String,
+    authors: Vec<String>,
+    description: Option<String>,
+}
+
+#[derive(Clone, Debug)]
+enum DisplayPackageSource {
+    Local { id: EntityId },
+    Remote { url: String },
+}
+
+#[element_component]
+fn PackageList(_hooks: &mut Hooks, packages: Vec<DisplayPackage>) -> Element {
+    let mut packages = packages;
+    packages.sort_by_key(|package| package.name.clone());
+
+    FlowColumn::el(packages.into_iter().map(|package| {
+        FlowRow::el([
+            ImageFromUrl {
+                url: assets::url("construction.png"),
+            }
+            .el()
+            .with(width(), 48.0)
+            .with(height(), 48.0),
+            match &package.source {
+                DisplayPackageSource::Local { id } => {
+                    let id = *id;
+                    FlowColumn::el([Checkbox::new(package.enabled, move |value| {
+                        PackageSetEnabled { id, enabled: value }.send_server_reliable();
+                    })
+                    .el()])
+                }
+                _ => Element::new(),
+            },
+            FlowColumn::el([
+                FlowRow::el([
+                    Text::el(package.name).with(font_style(), FontStyle::Bold),
+                    Text::el(package.version),
+                    Text::el("by"),
+                    Text::el(if package.authors.is_empty() {
+                        "No authors specified".to_string()
+                    } else {
+                        package.authors.join(", ")
+                    })
+                    .with(font_style(), FontStyle::Italic),
+                ])
+                .with(space_between_items(), 4.0),
+                Text::el(package.description.as_deref().unwrap_or("No description")),
+                match &package.source {
+                    DisplayPackageSource::Local { id } => {
+                        let id = *id;
+                        Button::new("View", move |_| {
+                            PackageShow { id }.send_local(crate::packages::this::entity())
+                        })
+                        .style(ButtonStyle::Flat)
+                        .el()
+                    }
+                    _ => Element::new(),
+                },
+            ])
+            .with(space_between_items(), 8.0),
+        ])
+        .with(space_between_items(), 8.0)
+    }))
 }
 
 // TODO: is there a way to share this?
