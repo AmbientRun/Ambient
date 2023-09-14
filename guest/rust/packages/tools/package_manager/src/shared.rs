f473ca37c5e98c2d3451a484c6a11b559e289fd0
Philpax <me@philpax.me>
2023-09-14 13:05:39 +0200

    feat(package_manager): remote packages list

--- guest/rust/packages/tools/package_manager/src/shared.rs
+++ guest/rust/packages/tools/package_manager/src/shared.rs
@@ -1,1 +1,11 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize)]
+pub struct PackageJson {
+    // TODO: don't let people submit their own custom URLs to the backend
+    pub url: String,
+    pub name: String,
+    pub version: String,
+    pub authors: Vec<String>,
+    pub description: Option<String>,
+}
