b4c7cc55bb141c5641b7a6c5c4bb24118f374cf1
Philpax <me@philpax.me>
2023-09-14 13:05:39 +0200

    feat(package_manager): remote packages list

--- guest/rust/packages/tools/package_manager/src/client/package_manager.rs
+++ guest/rust/packages/tools/package_manager/src/client/package_manager.rs
@@ -2,18 +2,27 @@ use std::fmt;
 
 use ambient_api::{
     core::{
-        package::{components::description, concepts::Package},
+        package::{components::description, concepts::Package as PackageConcept},
         rect::components::background_color,
         text::{components::font_style, types::FontStyle},
     },
-    element::{use_module_message, use_query, use_spawn},
+    element::{use_effect, use_module_message, use_query, use_spawn, use_state},
     prelude::*,
     ui::ImageFromUrl,
 };
 
-use crate::packages::this::{
-    assets,
-    messages::{PackageLoadShow, PackageSetEnabled, PackageShow},
+use crate::{
+    packages::{
+        self,
+        this::{
+            assets,
+            messages::{
+                PackageLoadShow, PackageRemoteRequest, PackageRemoteResponse, PackageSetEnabled,
+                PackageShow,
+            },
+        },
+    },
+    shared::PackageJson,
 };
 
 use super::use_hotkey_toggle;
@@ -57,12 +66,7 @@ fn PackageManagerInner(hooks: &mut Hooks) -> Element {
 
     Tabs::new()
         .with_tab(ListTab::Local, || PackagesLocal::el())
-        .with_tab(ListTab::Remote, || {
-            Button::new("Load package", |_| {
-                PackageLoadShow.send_local(crate::packages::this::entity())
-            })
-            .el()
-        })
+        .with_tab(ListTab::Remote, || PackagesRemote::el())
         .el()
         .with(space_between_items(), 4.0)
         .with_margin_even(STREET)
@@ -70,7 +74,7 @@ fn PackageManagerInner(hooks: &mut Hooks) -> Element {
 
 #[element_component]
 fn PackagesLocal(hooks: &mut Hooks) -> Element {
-    let packages = use_query(hooks, Package::as_query());
+    let packages = use_query(hooks, PackageConcept::as_query());
 
     let display_packages: Vec<_> = packages
         .into_iter()
@@ -78,8 +82,10 @@ fn PackagesLocal(hooks: &mut Hooks) -> Element {
             let description = entity::get_component(id, description());
 
             DisplayPackage {
-                source: DisplayPackageSource::Local { id },
-                enabled: package.enabled,
+                source: DisplayPackageSource::Local {
+                    id,
+                    enabled: package.enabled,
+                },
                 name: package.name,
                 version: package.version,
                 authors: package.authors,
@@ -91,10 +97,79 @@ fn PackagesLocal(hooks: &mut Hooks) -> Element {
     PackageList::el(display_packages)
 }
 
+#[element_component]
+fn PackagesRemote(hooks: &mut Hooks) -> Element {
+    #[derive(Clone, Debug)]
+    enum PackagesState {
+        Loading,
+        Loaded(Vec<DisplayPackage>),
+        Error(String),
+    }
+    let (packages, set_packages) = use_state(hooks, PackagesState::Loading);
+
+    use_effect(hooks, (), move |_, _| {
+        PackageRemoteRequest::default().send_server_reliable();
+        |_| {}
+    });
+
+    use_module_message::<PackageRemoteResponse>(hooks, move |_, ctx, msg| {
+        if !ctx.server() {
+            return;
+        }
+
+        if let Some(error) = &msg.error {
+            set_packages(PackagesState::Error(error.to_string()));
+            return;
+        }
+
+        let packages_json = msg
+            .packages
+            .iter()
+            .map(|p| serde_json::from_str::<PackageJson>(p))
+            .collect::<Result<Vec<_>, _>>();
+
+        match packages_json {
+            Ok(packages_json) => set_packages(PackagesState::Loaded(
+                packages_json
+                    .into_iter()
+                    .map(|package| DisplayPackage {
+                        source: DisplayPackageSource::Remote {
+                            url: package.url.clone(),
+                        },
+                        name: package.name,
+                        version: package.version,
+                        authors: package.authors,
+                        description: package.description,
+                    })
+                    .collect(),
+            )),
+            Err(error) => {
+                set_packages(PackagesState::Error(format!(
+                    "Failed to parse packages: {}",
+                    error
+                )));
+                return;
+            }
+        };
+    });
+
+    FlowColumn::el([
+        match packages {
+            PackagesState::Loading => Text::el("Loading..."),
+            PackagesState::Loaded(packages) => PackageList::el(packages),
+            PackagesState::Error(error) => Text::el(error),
+        },
+        Button::new("Load package from URL", |_| {
+            PackageLoadShow.send_local(crate::packages::this::entity())
+        })
+        .el(),
+    ])
+    .with(space_between_items(), 8.0)
+}
+
 #[derive(Clone, Debug)]
 struct DisplayPackage {
     source: DisplayPackageSource,
-    enabled: bool,
     name: String,
     version: String,
     authors: Vec<String>,
@@ -103,7 +178,7 @@ struct DisplayPackage {
 
 #[derive(Clone, Debug)]
 enum DisplayPackageSource {
-    Local { id: EntityId },
+    Local { id: EntityId, enabled: bool },
     Remote { url: String },
 }
 
@@ -112,64 +187,72 @@ fn PackageList(_hooks: &mut Hooks, packages: Vec<DisplayPackage>) -> Element {
     let mut packages = packages;
     packages.sort_by_key(|package| package.name.clone());
 
-    FlowColumn::el(packages.into_iter().map(|package| {
-        with_rect(FlowRow::el([
-            ImageFromUrl {
-                url: assets::url("construction.png"),
-            }
+    FlowColumn::el(packages.into_iter().map(|package| Package::el(package)))
+        .with(space_between_items(), 8.0)
+        .with(min_width(), 400.0)
+}
+
+#[element_component]
+fn Package(_hooks: &mut Hooks, package: DisplayPackage) -> Element {
+    fn button(text: impl Into<String>, action: impl Fn() + Send + Sync + 'static) -> Element {
+        Button::new(text.into(), move |_| action())
+            .style(ButtonStyle::Inline)
             .el()
-            .with(width(), 64.0)
-            .with(height(), 64.0),
-            FlowColumn::el([
-                FlowRow::el([
-                    Text::el(package.name).with(font_style(), FontStyle::Bold),
-                    Text::el(package.version),
-                    Text::el("by"),
-                    Text::el(if package.authors.is_empty() {
-                        "No authors specified".to_string()
-                    } else {
-                        package.authors.join(", ")
-                    })
-                    .with(font_style(), FontStyle::Italic),
-                ])
-                .with(space_between_items(), 4.0),
-                Text::el(package.description.as_deref().unwrap_or("No description")),
-                match &package.source {
-                    DisplayPackageSource::Local { id } => {
-                        let id = *id;
-                        FlowRow::el([
-                            Button::new(
-                                if package.enabled { "Disable" } else { "Enable" },
-                                move |_| {
-                                    PackageSetEnabled {
-                                        id,
-                                        enabled: !package.enabled,
-                                    }
-                                    .send_server_reliable();
-                                },
-                            )
-                            .style(ButtonStyle::Inline)
-                            .el(),
-                            Button::new("View", move |_| {
-                                PackageShow { id }.send_local(crate::packages::this::entity())
-                            })
-                            .style(ButtonStyle::Inline)
-                            .el(),
-                        ])
-                        .with(space_between_items(), 8.0)
-                    }
-                    _ => Element::new(),
-                },
+    }
+
+    with_rect(FlowRow::el([
+        // Image (ideally, this would be the package icon)
+        ImageFromUrl {
+            url: assets::url("construction.png"),
+        }
+        .el()
+        .with(width(), 64.0)
+        .with(height(), 64.0),
+        // Contents
+        FlowColumn::el([
+            // Header
+            FlowRow::el([
+                Text::el(package.name).with(font_style(), FontStyle::Bold),
+                Text::el(package.version),
+                Text::el("by"),
+                Text::el(if package.authors.is_empty() {
+                    "No authors specified".to_string()
+                } else {
+                    package.authors.join(", ")
+                })
+                .with(font_style(), FontStyle::Italic),
             ])
             .with(space_between_items(), 4.0),
-        ]))
-        .with(space_between_items(), 8.0)
-        .with(background_color(), vec4(0., 0., 0., 0.5))
-        .with(fit_horizontal(), Fit::Parent)
-        .with_padding_even(8.0)
-    }))
+            // Description
+            Text::el(package.description.as_deref().unwrap_or("No description")),
+            // Buttons
+            match &package.source {
+                DisplayPackageSource::Local { id, enabled } => {
+                    let id = *id;
+                    let enabled = *enabled;
+                    FlowRow::el([
+                        button(if enabled { "Disable" } else { "Enable" }, move || {
+                            PackageSetEnabled {
+                                id,
+                                enabled: !enabled,
+                            }
+                            .send_server_reliable();
+                        }),
+                        button("View", move || {
+                            PackageShow { id }.send_local(crate::packages::this::entity())
+                        }),
+                    ])
+                    .with(space_between_items(), 8.0)
+                }
+                _ => Element::new(),
+            },
+        ])
+        .with(space_between_items(), 4.0),
+    ]))
     .with(space_between_items(), 8.0)
-    .with(min_width(), 400.0)
+    .with(background_color(), vec4(0., 0., 0., 0.5))
+    .with(fit_horizontal(), Fit::Parent)
+    .with_padding_even(8.0)
 }
 
 // TODO: is there a way to share this?
--- guest/rust/packages/tools/package_manager/src/server/package_manager.rs
+++ guest/rust/packages/tools/package_manager/src/server/package_manager.rs
@@ -1,9 +1,74 @@
-use ambient_api::{core::package::components::enabled, prelude::*};
+use std::collections::HashSet;
 
-use crate::packages::this::messages::PackageSetEnabled;
+use ambient_api::{anyhow, core::package::components::enabled, prelude::*};
+use ambient_package::Manifest;
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    packages::this::messages::{PackageRemoteRequest, PackageRemoteResponse, PackageSetEnabled},
+    shared::PackageJson,
+};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct PackageListApiJson {
+    name: String,
+    latest_deployment: String,
+    content: Vec<String>,
+}
 
 pub fn main() {
     PackageSetEnabled::subscribe(|_, msg| {
         entity::set_component(msg.id, enabled(), msg.enabled);
     });
+
+    PackageRemoteRequest::subscribe(|ctx, _| {
+        let Some(user_id) = ctx.client_user_id() else {
+            return;
+        };
+
+        run_async(async move {
+            match process_request().await {
+                Ok(msg) => msg,
+                Err(err) => PackageRemoteResponse {
+                    packages: vec![],
+                    error: Some(err.to_string()),
+                },
+            }
+            .send_client_targeted_reliable(user_id)
+        });
+    });
+}
+
+async fn process_request() -> anyhow::Result<PackageRemoteResponse> {
+    let mut api_packages: Vec<PackageListApiJson> =
+        serde_json::from_slice(&http::get("https://api.ambient.run/packages/list").await?)?;
+
+    let ignore_content_types: HashSet<&str> = HashSet::from_iter(["Schema", "Playable"]);
+    api_packages.retain(|pkg| {
+        HashSet::from_iter(pkg.content.iter().map(|s| s.as_str()))
+            .is_disjoint(&ignore_content_types)
+    });
+
+    let mut packages_json = vec![];
+    for api_package in api_packages {
+        let url = format!(
+            "https://assets.ambient.run/{}/ambient.toml",
+            api_package.latest_deployment
+        );
+
+        let manifest: Manifest = toml::from_str(std::str::from_utf8(&http::get(&url).await?)?)?;
+
+        packages_json.push(serde_json::to_string(&PackageJson {
+            name: manifest.package.name,
+            url,
+            version: manifest.package.version.to_string(),
+            authors: manifest.package.authors,
+            description: manifest.package.description,
+        })?);
+    }
+
+    Ok(PackageRemoteResponse {
+        packages: packages_json,
+        error: None,
+    })
 }
