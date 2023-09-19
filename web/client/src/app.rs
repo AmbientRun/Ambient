5240419d639af224c5b1db9ec74e3b88983802fc
Tei Leelo Roberts <ten3roberts@gmail.com>
2023-09-19 10:22:50 +0200

    feat(web): ambient_prefab

--- web/client/src/app.rs
+++ web/client/src/app.rs
@@ -58,12 +58,13 @@ fn systems() -> SystemGroup {
     SystemGroup::new(
         "client",
         vec![
-            // Box::new(ambient_prefab::systems()),
+            Box::new(ambient_prefab::systems()),
             // Box::new(ambient_decals::client_systems()),
             Box::new(ambient_primitives::systems()),
             // Box::new(ambient_sky::systems()),
             // Box::new(ambient_water::systems()),
             // Box::new(ambient_physics::client_systems()),
+            // Box::new(ambient_gizmos::client_systems()),
             Box::new(wasm::systems()),
             Box::new(player::systems_final()),
         ],
