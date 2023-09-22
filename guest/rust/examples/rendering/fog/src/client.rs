f0560ac15acced63024f65b517bd4f94dd3895df
Alexander Martin <hi@droqen.com>
2023-09-22 11:09:58 -0400

    Revert "turntable sliders added to 'fog' (for now)"
    This reverts commit 735b219a63041323d0dc05e0ef9de8c87ce96612.

--- guest/rust/examples/rendering/fog/src/client.rs
+++ guest/rust/examples/rendering/fog/src/client.rs
@@ -2,7 +2,6 @@ use ambient_api::{
     core::{
         app::components::main_scene,
         camera::components::fog,
-        model::components::model_from_url,
         primitives::components::{cube, quad},
         rendering::components::{
             cast_shadows, color, fog_color, fog_density, fog_height_falloff, light_diffuse, sky,
@@ -12,18 +11,11 @@ use ambient_api::{
     },
     prelude::*,
 };
-use packages::orbit_camera::{
-    components::{camera_angle, camera_distance},
-    concepts::{OrbitCamera, OrbitCameraOptional},
-};
-use packages::this::components::{
-    fog_b, fog_g, fog_r, orbit_turn, orbit_zoom, sun_b, sun_g, sun_r, sun_rotx, sun_roty, sun_rotz,
-    turntable,
-};
+use packages::orbit_camera::concepts::{OrbitCamera, OrbitCameraOptional};
 
 #[main]
 fn main() {
-    let orbitcamera = OrbitCamera {
+    OrbitCamera {
         is_orbit_camera: (),
         optional: OrbitCameraOptional {
             camera_angle: Some(vec2(0.0, 15f32.to_radians())),
@@ -33,27 +25,7 @@ fn main() {
     }
     .make()
     .with(fog(), ())
-    .with(orbit_turn(), 0.0)
-    .with(orbit_zoom(), 0.0)
     .spawn();
-
-    query(orbit_turn()).each_frame(|orbitcams| {
-        for (cam, turn) in orbitcams {
-            entity::add_component(cam, camera_angle(), vec2(6.28 * turn, 15f32.to_radians()));
-        }
-    });
-
-    query(orbit_zoom()).each_frame(|orbitcams| {
-        for (cam, zoom) in orbitcams {
-            entity::add_component(cam, camera_distance(), (20f32).powf(1. + zoom) * 0.25);
-        }
-    });
-
-    query(turntable()).each_frame(|turners| {
-        for (turner, turn) in turners {
-            entity::add_component(turner, rotation(), Quat::from_rotation_z(turn * 6.28));
-        }
-    });
 
     let sun = Entity::new()
         .with(sun(), 0.0)
@@ -63,39 +35,7 @@ fn main() {
         .with(fog_color(), vec3(1., 1., 1.))
         .with(fog_density(), 0.1)
         .with(fog_height_falloff(), 0.01)
-        .with(fog_b(), 1.)
-        .with(fog_g(), 1.)
-        .with(fog_r(), 1.)
-        .with(sun_b(), 1.)
-        .with(sun_g(), 1.)
-        .with(sun_r(), 1.)
-        .with(sun_rotx(), 0.)
-        .with(sun_roty(), 1.)
-        .with(sun_rotz(), 0.)
         .spawn();
-
-    query((
-        fog_r(),
-        fog_g(),
-        fog_b(),
-        sun_r(),
-        sun_g(),
-        sun_b(),
-        sun_rotx(),
-        sun_roty(),
-        sun_rotz(),
-    ))
-    .each_frame(|suns| {
-        for (sun, (fr, fg, fb, sr, sg, sb, lx, ly, lz)) in suns {
-            entity::add_component(sun, fog_color(), vec3(fr, fg, fb));
-            entity::add_component(sun, light_diffuse(), vec3(sr, sg, sb));
-            entity::add_component(
-                sun,
-                rotation(),
-                Quat::from_euler(glam::EulerRot::XYZ, lx * 6.28, ly * 6.28, lz * 6.28),
-            );
-        }
-    });
 
     Entity::new().with(sky(), ()).spawn();
 
@@ -105,7 +45,7 @@ fn main() {
         .with(color(), vec4(1., 0., 0., 1.))
         .spawn();
 
-    for i in 1..10 {
+    for i in 0..10 {
         Entity::new()
             .with(cube(), ())
             .with(translation(), vec3(0., 1. * (2f32).powi(i), 1.))
@@ -115,21 +55,11 @@ fn main() {
             .spawn();
     }
 
-    let statue = Entity::new()
-        .with(
-            model_from_url(),
-            packages::this::assets::url("hermaneubis.glb"),
-        )
-        .with(translation(), vec3(0., 0., 0.))
-        .with(cast_shadows(), ())
-        .with(turntable(), 0.0)
-        .spawn();
-
-    App::el(sun, statue, orbitcamera).spawn_interactive();
+    App::el(sun).spawn_interactive();
 }
 
 #[element_component]
-fn App(hooks: &mut Hooks, sun: EntityId, statue: EntityId, ocam: EntityId) -> Element {
+fn App(hooks: &mut Hooks, sun: EntityId) -> Element {
     FlowColumn::el([
         FlowRow::el([
             Text::el("Fog density: "),
@@ -138,54 +68,6 @@ fn App(hooks: &mut Hooks, sun: EntityId, statue: EntityId, ocam: EntityId) -> El
         FlowRow::el([
             Text::el("Fog height falloff: "),
             Slider::new_for_entity_component(hooks, sun, fog_height_falloff()).el(),
-        ]),
-        FlowRow::el([
-            Text::el("Fog colour (fog_r): "),
-            Slider::new_for_entity_component(hooks, sun, fog_r()).el(),
-        ]),
-        FlowRow::el([
-            Text::el("Fog colour (fog_g): "),
-            Slider::new_for_entity_component(hooks, sun, fog_g()).el(),
-        ]),
-        FlowRow::el([
-            Text::el("Fog colour (fog_b): "),
-            Slider::new_for_entity_component(hooks, sun, fog_b()).el(),
-        ]),
-        FlowRow::el([
-            Text::el("Sunlight colour (sun_r): "),
-            Slider::new_for_entity_component(hooks, sun, sun_r()).el(),
-        ]),
-        FlowRow::el([
-            Text::el("Sunlight colour (sun_g): "),
-            Slider::new_for_entity_component(hooks, sun, sun_g()).el(),
-        ]),
-        FlowRow::el([
-            Text::el("Sunlight colour (sun_b): "),
-            Slider::new_for_entity_component(hooks, sun, sun_b()).el(),
-        ]),
-        FlowRow::el([
-            Text::el("Sun angle (sun_rotx): "),
-            Slider::new_for_entity_component(hooks, sun, sun_rotx()).el(),
-        ]),
-        FlowRow::el([
-            Text::el("Sun angle (sun_roty): "),
-            Slider::new_for_entity_component(hooks, sun, sun_roty()).el(),
-        ]),
-        FlowRow::el([
-            Text::el("Sun angle (sun_rotz): "),
-            Slider::new_for_entity_component(hooks, sun, sun_rotz()).el(),
-        ]),
-        FlowRow::el([
-            Text::el("Orbit camera turn: "),
-            Slider::new_for_entity_component(hooks, ocam, orbit_turn()).el(),
-        ]),
-        FlowRow::el([
-            Text::el("Orbit camera dist: "),
-            Slider::new_for_entity_component(hooks, ocam, orbit_zoom()).el(),
-        ]),
-        FlowRow::el([
-            Text::el("Model turn: "),
-            Slider::new_for_entity_component(hooks, statue, turntable()).el(),
         ]),
     ])
     .with_background(vec4(0., 0., 0., 0.9))
