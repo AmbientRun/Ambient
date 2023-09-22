9a26babea6d44433788d54c2b3aa06735820d48a
Philpax <me@philpax.me>
2023-09-22 21:50:46 +0200

    fix: user feedback

--- app/src/cli/deploy.rs
+++ app/src/cli/deploy.rs
@@ -41,7 +41,7 @@ pub async fn handle(
                 .api_token
                 .as_ref()
                 .with_context(|| format!(
-                    "No API token provided.\n\nYou can provide one with `--token` or by specifying `general.api_token` in {:?}.\n\nCreate your API token on the Ambient website: '{}'.",
+                    "No API token provided.\n\nYou can provide one with `--token` or by specifying `general.api_token` in {:?}.\n\nSign up for an account on the Ambient website, then go to your profile page to generate an API token: '{}'.",
                     Settings::path().unwrap_or_default(),
                     ambient_shared_types::urls::AMBIENT_WEB_APP_URL
                 ))?
