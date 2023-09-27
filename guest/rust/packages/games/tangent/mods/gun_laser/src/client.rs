a08172028eebb50e844657c0eea4d982419f4beb
Philpax <me@philpax.me>
2023-09-27 10:28:39 +0200

    feat(tangent): fix laser sound

--- guest/rust/packages/games/tangent/mods/gun_laser/src/client.rs
+++ guest/rust/packages/games/tangent/mods/gun_laser/src/client.rs
@@ -1,4 +1,7 @@
-use ambient_api::{core::transform::components::translation, prelude::*};
+use ambient_api::{
+    core::transform::components::{local_to_world, translation},
+    prelude::*,
+};
 use packages::this::messages::Fire;
 
 #[main]
@@ -8,9 +11,11 @@ pub fn main() {
             return;
         }
 
-        let Some(translation) = entity::get_component(msg.weapon_id, translation()) else {
+        let Some(local_to_world) = entity::get_component(msg.weapon_id, local_to_world()) else {
             return;
         };
+
+        let (_, _, translation) = local_to_world.to_scale_rotation_translation();
 
         audio::SpatialAudioPlayer::oneshot(
             translation,
