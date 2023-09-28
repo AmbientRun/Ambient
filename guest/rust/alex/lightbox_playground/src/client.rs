4fb4c0dd61bf9cf346c9cd44c201a32f65e5df24
Alexander Martin <hi@droqen.com>
2023-09-27 20:48:06 -0400

    added a 1-colour chicken

--- guest/rust/alex/lightbox_playground/src/client.rs
+++ guest/rust/alex/lightbox_playground/src/client.rs
@@ -135,7 +135,8 @@ fn main() {
             // packages::this::assets::url("hermaneubis_yellow.glb"),
             // packages::this::assets::url("cliffmesh01_fredrik.glb"),
             // packages::this::assets::url("cliffmesh01.gltf"),
-            packages::this::assets::url("cliffmesh_fredrik_then_alex_2.glb"),
+            // packages::this::assets::url("cliffmesh_fredrik_then_alex_2.glb"),
+            packages::this::assets::url("colour_chicken.glb"),
         )
         .with(translation(), vec3(0., 0., 0.))
         .with(cast_shadows(), ())
