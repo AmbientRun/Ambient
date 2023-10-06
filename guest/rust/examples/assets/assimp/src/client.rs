88faafad5fa4b9372e3fdca658486cdebfcdfcc0
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-10-06 18:13:14 +0200

    Cleanup assimp example

--- guest/rust/examples/assets/assimp/src/client.rs
+++ guest/rust/examples/assets/assimp/src/client.rs
@@ -1,13 +1,9 @@
 use ambient_api::{
-    animation::{self, AnimationPlayerRef, BindId, PlayClipFromUrlNodeRef},
+    animation::{AnimationPlayerRef, PlayClipFromUrlNodeRef},
     core::{
-        animation::components::apply_animation_player,
-        messages::Frame,
-        model::components::model_loaded,
-        prefab::components::prefab_from_url,
-        transform::{components::rotation, concepts::Transformable},
+        animation::components::apply_animation_player, prefab::components::prefab_from_url,
+        transform::concepts::Transformable,
     },
-    entity::mutate_component,
     prelude::*,
 };
 use packages::this::assets;
@@ -25,13 +21,4 @@ pub async fn main() {
     let idle = PlayClipFromUrlNodeRef::new(assets::url("Zombie1.x/animations/Run1.anim"));
     let anim_player = AnimationPlayerRef::new(idle);
     entity::add_component(zombie, apply_animation_player(), anim_player.0);
-
-    // Joint3
-
-    // let _ = entity::wait_for_component(zombie, model_loaded()).await;
-    // let bone =
-    //     animation::get_bone_by_bind_id(zombie, &BindId::Custom("Joint3".to_string())).unwrap();
-    // Frame::subscribe(move |_| {
-    //     mutate_component(bone, rotation(), |x| *x *= Quat::from_rotation_z(0.1));
-    // });
 }
--- guest/rust/examples/assets/assimp/src/server.rs
+++ guest/rust/examples/assets/assimp/src/server.rs
@@ -1,21 +1,7 @@
-use ambient_api::{
-    core::{
-        camera::concepts::{
-            PerspectiveInfiniteReverseCamera, PerspectiveInfiniteReverseCameraOptional,
-        },
-        prefab::components::prefab_from_url,
-        primitives::components::quad,
-        transform::{
-            components::{lookat_target, translation},
-            concepts::Transformable,
-        },
-    },
-    prelude::*,
-};
-use packages::{orbit_camera::concepts::OrbitCamera, this::assets};
+use ambient_api::prelude::*;
+use packages::orbit_camera::concepts::OrbitCamera;
 
 #[main]
 pub fn main() {
-    // Camera
     OrbitCamera::suggested().spawn();
 }
