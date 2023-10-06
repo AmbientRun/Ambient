06e4d2f869795d327eb7df4f653d230e4e40a41d
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-10-06 15:39:18 +0200

    Start on skinmesh support for assimp

---
+++ guest/rust/examples/assets/assimp/src/client.rs
@@ -0,0 +1,32 @@
+use ambient_api::{
+    animation::{self, BindId},
+    core::{
+        messages::Frame,
+        model::components::model_loaded,
+        prefab::components::prefab_from_url,
+        transform::{components::rotation, concepts::Transformable},
+    },
+    entity::mutate_component,
+    prelude::*,
+};
+use packages::this::assets;
+#[main]
+pub async fn main() {
+    // Model
+    let zombie = Entity::new()
+        .with_merge(Transformable {
+            local_to_world: Default::default(),
+            optional: Default::default(),
+        })
+        .with(prefab_from_url(), assets::url("Zombie1.x"))
+        .spawn();
+
+    // Joint3
+
+    let _ = entity::wait_for_component(zombie, model_loaded()).await;
+    let bone =
+        animation::get_bone_by_bind_id(zombie, &BindId::Custom("Joint3".to_string())).unwrap();
+    Frame::subscribe(move |_| {
+        mutate_component(bone, rotation(), |x| *x *= Quat::from_rotation_z(0.1));
+    });
+}
