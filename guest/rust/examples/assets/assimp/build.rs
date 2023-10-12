1db714f538b49e51e31abc652cb4cdd2f33f5373
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-10-12 12:13:08 +0200

    Fix assimp example

--- guest/rust/examples/assets/assimp/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let fname = std::path::Path::new("assets/Zombie1.x.zip");
-    let file = std::fs::File::open(fname).unwrap();
-    let mut archive = zip::ZipArchive::new(file).unwrap();
-    archive.extract("assets/").unwrap();
-}
